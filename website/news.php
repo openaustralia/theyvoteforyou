b549de0dd33e378c05e76705a172632e2d2d062d
frabcus <frabcus>
2003-09-09 14:36:23 +0000

    Yadadh

--- website/news.php
+++ website/news.php
@@ -1,5 +1,5 @@
 <?  $title = "News"; include "header.inc";
-# $Id: news.php,v 1.1 2003/09/09 14:26:08 frabcus Exp $
+# $Id: news.php,v 1.2 2003/09/09 14:36:23 frabcus Exp $
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -9,8 +9,9 @@
 
 <h2>9 September 2003 by Francis</h2>
 <p>
-Parliament has reconvened after the summer, and so have we.  Yesterday there
-were <a href="search.php?query=water+bill&button=Search">three divisions
+Parliament has reconvened now summer is over.  However, under this new
+system, after only two weeks it will adjourn for the party conferences. 
+Yesterday there were <a href="search.php?query=water+bill&button=Search">three divisions
 votes</a> on the Water Bill.  I'm doing these updates semi-manually at
 the moment, so new divisions will only be available a few days after
 they happen.
