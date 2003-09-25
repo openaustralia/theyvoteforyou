4317823d80af9ec5337dceaff846bb99bf53f7df
frabcus <frabcus>
2003-09-25 21:19:43 +0000

    More consistent labelling

--- website/boths.php
+++ website/boths.php
@@ -1,5 +1,5 @@
 <?php 
-    # $Id: boths.php,v 1.1 2003/09/25 20:29:17 uid37249 Exp $
+    # $Id: boths.php,v 1.2 2003/09/25 21:19:43 frabcus Exp $
 
     # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
     # This is free software, and you are welcome to redistribute it under
@@ -15,15 +15,15 @@
     $sort = mysql_escape_string($_GET["sort"]);
     if ($sort == "")
     {
-        $sort = "division";
+        $sort = "date";
     }
-    if ($sort == "division")
+    if ($sort == "date")
     {
-        $order = "division_date desc, division_number desc, last_name, first_name, constituency";
+        $order = "order by division_date desc, division_number desc, last_name, first_name, constituency";
     }
-    elseif ($sort == "name")
+    elseif ($sort == "lastname")
     {
-        $order = "last_name, first_name, constituency, division_date desc, division_number desc";
+        $order = "order by last_name, first_name, constituency, division_date desc, division_number desc";
     }
 
     $db->query("select first_name, last_name, constituency, party, 
@@ -31,7 +31,7 @@
         division_number, division_date, division_name from pw_mp,
         pw_division, pw_vote where pw_mp.mp_id = pw_vote.mp_id and
         pw_division.division_id = pw_vote.division_id and vote =
-        'both' order by $order");
+        'both' $order");
     $count = $db->rows();
 
 ?>
@@ -49,15 +49,15 @@ do so.  Unless they just followed each other blindly into the
 wrong lobby...
 
 <p>This table lists all instances of double voting.  You can select the column
-headings to sort it by MP name or by division.
+headings to sort it by MP name or by division date.
 
 <?php
     $url = "boths.php?";
     print "<table class=\"mix\"><tr class=\"headings\">\n";
     print "<tr class=\"headings\"><td>No.</td>";
-    head_cell($url, $sort, "Division", "division", "Sort by division date");
-    print "<td>Subject</td>";
-    head_cell($url, $sort, "MP", "name", "Sort by surname");
+    head_cell($url, $sort, "Date", "date", "Sort by division date");
+    print "<td>Division</td>";
+    head_cell($url, $sort, "MP", "lastname", "Sort by surname");
     print "<td>Constituency</td><td>Party</td>";
     print "</tr>";
 
@@ -65,9 +65,8 @@ headings to sort it by MP name or by division.
     while ($row = $db->fetch_row())
     {
         $prettyrow = pretty_row_start($prettyrow);
-        print "<td>$row[6]</td><td><a href=\"division.php?date=" . urlencode($row[7]) .
-        "&number=" . urlencode($row[6]) . "\">$row[7]</a></td> 
-               <td>$row[8]</td>";
+        print "<td>$row[6]</td><td>$row[7]</td><td><a href=\"division.php?date=" . urlencode($row[7]) .
+        "&number=" . urlencode($row[6]) . "\">$row[8]</a></td>";
         print "<td><a href=\"mp.php?firstname=" . urlencode($row[0]) .
             "&lastname=" . urlencode($row[1]) . "&constituency=" .
             urlencode($row[2]) . "\">
