a8a9866b51c09bb39fc6b63a2806f07d86557d9e
frabcus <frabcus>
2003-10-11 11:37:33 +0000

    Credit

--- website/faq.php
+++ website/faq.php
@@ -1,5 +1,5 @@
 <?php $title = "Frequently Asked Questions"; include "header.inc" 
-# $Id: faq.php,v 1.6 2003/10/03 21:46:10 frabcus Exp $
+# $Id: faq.php,v 1.7 2003/10/11 11:37:33 frabcus Exp $
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -101,7 +101,9 @@ would retain all the power but would have slightly better software
 
 <h2>What organisation is behind the Public Whip?</h2>
 <p>None.  Just two guys <a href="http://www.flourish.org">Francis</a>
-and Julian who had an idea and made it happen.
+and Julian who had an idea and made it happen.  Giles designed the look
+of the website.  We're hosted by the ever helpful and encouraging
+<a href="http://www.mythic-beasts.com/">Mythic Beasts</a>.
 
 <h2>Where can I email?</h2>
 <p>If you have any problems, comments, queries, suggestions or offers of help email <a
