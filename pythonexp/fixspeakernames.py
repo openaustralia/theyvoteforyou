60c1c3b189a2f9112c2a8f57391d42e490354a52
goatchurch <goatchurch>
2003-11-17 10:46:49 +0000

    this script added into the system

--- pythonexp/fixspeakernames.py
+++ pythonexp/fixspeakernames.py
@@ -1,90 +1,123 @@
+#! /usr/bin/python2.3
+
 import sys
 import re
+import os
+import string
 
 # this filter finds the speakers and replaces with full itendifiers
-# <speaker name="Eric Martlew  (Carlisle)"/>
+# <speaker name="Eric Martlew  (Carlisle)"><p>Eric Martlew  (Carlisle)</p></speaker>
 
 # in and out files for this filter
-filein = "f3hocdaydebate2000-11-07.htm"
-fileout = "f4hocdaydebate2000-11-07.htm"
+dirin = "c2daydebatefixcolumnnumbers"
+dirout = "c3daydebatematchspeakers"
+dtemp = "daydebtemp.htm"
+
 
 # <B> Mr. Eric Martlew  (Carlisle):</B>
 # <B> Mr. Grieve: </B>
-lspeakerregexp = '(<b>.*?</b>):?(?i)'
+lspeakerregexp = '(<b>.*?</b>\s*?:|<b>.*?</b>)(?i)'
 speakerregexp = '<b>\s*([^:]*?):?\s*</b>(?i)'
 
+#print re.findall(lspeakerregexp, '<b> hi there </b> \n: dkdk')
+#sys.exit()
 
-fin = open(filein);
-fr = fin.read()
-fs = re.split(lspeakerregexp, fr)
-fin.close()
+# scan through directory
+fdirin = os.listdir(dirin)
 
-fout = open(fileout, "w");
-
-# the map which will expand the names from the common abbreviations
-nameexp = {  }
-
-for fss in fs:
-	if not fss:
-		continue
-	speakergroup = re.match(speakerregexp, fss)
-	if not speakergroup:
-		fout.write(fss)
+for fin in fdirin:
+	jfin = os.path.join(dirin, fin)
+	jfout = os.path.join(dirout, fin)
+	if os.path.isfile(jfout):
+		print "skipping " + fin
 		continue
 
-	# we have a string in bold
-	boldnamestring = speakergroup.group(1)
+	print fin
+	fin = open(jfin);
+	fr = fin.read()
+	fin.close()
 
-	# discard divisions from consideration
-	if (re.search('Division', boldnamestring)):
-		fout.write(fss)
-		continue
+	tempfile = open(dtemp, "w")
 
-	# full name type : Mr. Eric Martlew  (Carlisle)
-	# oofficial name type: The Secretary of State for Foreign and Commonwealth Affairs   (Mr. Robin Cook)
-	bracketgroup = re.match('(.*?)\s*[(](.*?)[)].*?', boldnamestring)
-	if bracketgroup:
-		# first determin which of side is the name
-		if re.search('president|minister|secretary(?i)', bracketgroup.group(1)):
-			realname = bracketgroup.group(2)
+	# setup for scanning through the file.
+	# we should have a name matching module which gets the unique ids, and
+	# takes the full speaker name and date to find a match.
+	fs = re.split(lspeakerregexp, fr)
+
+	# hard code one value
+	nameexp = { 'The Prime Minister':'Mr. Tony Blair ((The Prime Minister))'}
+
+	# the map which will expand the names from the common abbreviations
+	for fss in fs:
+		if not fss:
+			continue
+		speakergroup = re.findall(speakerregexp, fss)
+		if len(speakergroup) == 0:
+			tempfile.write(fss)
+			continue
+
+		# we have a string in bold
+		boldnamestring = speakergroup[0]
+
+		# discard divisions from consideration
+		if (re.search('Division', boldnamestring)):
+			tempfile.write(fss)
+			continue
+
+		# full name type : Mr. Eric Martlew  (Carlisle)
+		# official name type: The Secretary of State for Foreign and Commonwealth Affairs   (Mr. Robin Cook)
+		bracketgroup = re.match('(.*?)\s*[(](.*?)[)].*?', boldnamestring)
+		if bracketgroup:
+			# first determin which of side is the name
+			if re.search('president|minister|secretary|chancellor|leader|general(?i)', bracketgroup.group(1)):
+				realname = bracketgroup.group(2)
+				# rearrange the name so that the title is in place of constituency in more brackets
+				office = bracketgroup.group(1)
+				boldnamestring = '%s ((%s))' % (realname, office)
+
+				# let the office refer to the person as well
+				# only seen with Prime Minister, which is now hardcoded
+				#if not nameexp.has_key(office):
+				#	nameexp[office] = boldnamestring
+			else:
+				realname = bracketgroup.group(1) # the other is the constituency
+
+			# take out the dots
+			realname = re.sub('[.]', '', realname)
+
+			# add in realname and its abbreviations into the mapping
+			if not nameexp.has_key(realname):
+				nameexp[realname] = boldnamestring
+				realnamegroup = re.match('(Mr|Mrs|Miss|Ms|Sir|Dr)\s+(\w+?)\s+(.+?)$', realname)
+				if realnamegroup:
+					shortrealname = realnamegroup.group(1) + ' ' + realnamegroup.group(3)
+					if nameexp.has_key(shortrealname):
+						print "*** short realname ambiguous " + shortrealname + "  " + nameexp[shortrealname] + boldnamestring
+						nameexp[shortrealname] = "ambiguous"
+					else:
+						nameexp[shortrealname] = boldnamestring
+
+
 		else:
-			realname = bracketgroup.group(1) # the other is the constituency
+			shortname = re.sub('[.]', '', boldnamestring)
+			if re.search('hon member(?i)', shortname):
+				boldnamestring = shortname
+			elif re.search('mr speaker(?i)', shortname):
+				boldnamestring = shortname
 
-		# take out the dots
-		realname = re.sub('[.]', '', realname)
+			elif nameexp.has_key(shortname):
+				boldnamestring = nameexp[shortname]
+			else:
+				boldnamestring = 'ambiguous'
+				print "long name not found for " + shortname
 
-		# add in realname and its abbreviations
-		if not nameexp.has_key(realname):
-			nameexp[realname] = boldnamestring
-			realnamegroup = re.match('(Mr|Mrs|Ms|Sir|Dr)\s+(\w+?)\s+(.+?)$', realname)
-			if realnamegroup:
-				shortrealname = realnamegroup.group(1) + ' ' + realnamegroup.group(3)
-				if nameexp.has_key(shortrealname):
-					print "*** short realname ambiguous " + shortrealname + "  " + nameexp[shortrealname] + boldnamestring
-					nameexp[shortrealname] = "ambiguous"
-				else:
-					nameexp[shortrealname] = boldnamestring
+			if boldnamestring == 'ambiguous':
+				boldnamestring = shortname
 
+		# now output what we've decided
+		#print boldnamestring
+		tempfile.write('<p><speaker name="%s"><font color="#003fcf">%s</font></speaker></p>\n' % (boldnamestring, boldnamestring))
 
-	else:
-		shortname = re.sub('[.]', '', boldnamestring)
-		if re.search('hon member(?i)', shortname):
-			boldnamestring = shortname
-		elif re.search('mr speaker(?i)', shortname):
-			boldnamestring = shortname
-
-		elif nameexp.has_key(shortname):
-			boldnamestring = nameexp[shortname]
-		else:
-			boldnamestring = 'ambiguous'
-			print "long name not found for " + shortname
-
-		if boldnamestring == 'ambiguous':
-			boldnamestring = shortname
-
-	print boldnamestring
-	fout.write('<speaker name="%s">\n' % boldnamestring)
-
-
-fout.close()
+	tempfile.close()
+	os.rename(dtemp, jfout)
 
