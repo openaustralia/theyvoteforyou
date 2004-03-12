c0688648ce3c217ebc42040b39eef52eea72ed08
frabcus <frabcus>
2004-03-12 17:27:41 +0000

    FAQ on free votes

--- website/faq.php
+++ website/faq.php
@@ -1,5 +1,5 @@
 <?php $title = "Frequently Asked Questions"; include "header.inc" 
-# $Id: faq.php,v 1.19 2004/02/14 20:31:55 frabcus Exp $
+# $Id: faq.php,v 1.20 2004/03/12 17:27:41 frabcus Exp $
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -10,12 +10,28 @@
 <a href="http://www.newstatesman.com/newmedia">
 <img align=right src="http://www.newstatesman.com/nma/nma2004/Graphics/buttons/nominatedbutton.gif" border="0"></a>
 
-<h2>What is the Public Whip?</h2>
+<ul>
+<li><a href="#whatis">What is the Public Whip?</a> <br/>
+<li><a href="#jargon">First, can you explain "division" and other political jargon?</a> <br/>
+<li><a href="#how">How does the Public Whip work?</a> <br/>
+<li><a href="#timeperiod">What time period does it cover?</a> <br/>
+<li><a href="#clarify">What do the "rebellion" and "attendance" figures mean exactly?</a> <br/>
+<li><a href="#freevotes">Why do you incorrectly say people are rebels in free votes?</a> <br/>
+<li><a href="#spreadsheet">Where is the data in spreadsheet file format or in XML?</a> <br/>
+<li><a href="#legal">Legal question, what can I use this information for?</a> <br/>
+<li><a href="#playwith">Can I play with the software?</a> <br/>
+<li><a href="#whyfree">Why are you giving everything away for free?</a> <br/>
+<li><a href="#organisation">What organisation is behind the Public Whip?</a> <br/>
+<li><a href="#keepup">How can I keep up with what you are doing?</a> <br/>
+<li><a href="#email">Where can I email?</a> <br/>
+</ul>
+
+<h2><a name="whatis">What is the Public Whip?</a></h2>
 <p>The Public Whip is a project to watch Members of the United Kingdom
 Parliament, so that the public (people like us) can better influence their
 voting patterns.
 
-<h2>First, can you explain "division" and other political jargon?</h2>
+<h2><a name="jargon">First, can you explain "division" and other political jargon?</a></h2>
 <p>The House of Commons <i>divides</i> many times each week into those who
 vote "aye" ("yes", for the motion) and those who vote "no" (against the
 motion).  Each political party has <i>whips</i> who try to make their
@@ -25,14 +41,14 @@ MPs (Members of Parliament) vote for the party line.  Sometimes an MP
 <p>A <i>Written Answer</i> is a Government reply to a question from an MP, 
 usually containing detailed factual information.
 
-<h2>How does the Public Whip work?</h2>
+<h2><a name="how">How does the Public Whip work?</a></h2>
 <p>All the House of Commons debate transcripts (<a href="http://www.parliament.the-stationery-office.co.uk/pa/cm/cmhansrd.htm">Hansard</a>) back to
 1988 are published electronically on the World Wide Web.  We've written
 a program to read it for you and separate out all the records of
 voting and the written answers.  This information has been added into an online
 database which you can access.
 
-<h2>What time period does it cover?</h2>
+<h2><a name="timeperiod">What time period does it cover?</a></h2>
 <p>Voting data extends back across two parliaments to the May 1997 General
 Election, although there are a few divisions missing in the 1997
 parliament.  New divisions are added semi-manually, so will not appear
@@ -64,8 +80,7 @@ in each division.  In total <?=$vote_count?> votes were cast, of which
 That's an overall <?=$attendance?>% attendance rate and <?=$rebelocity?>%
 rebellion rate.  A total of 46878 written answers are indexed.
 
-<h2><a name="clarify">What do the "rebellion"
-and "attendance" figures mean exactly?</a></h2>
+<h2><a name="clarify">What do the "rebellion" and "attendance" figures mean exactly?</a></h2>
 <p>The apparent meaning of the data can be misleading, so try not to
 jump to conclusions about your MP.
 
@@ -80,16 +95,36 @@ House of Commons library research paper</a>.  Note also that the Public
 Whip does not currently record if a member spoke in the debate but did
 not vote.
 
-<p>"Rebellion" on this website means a vote against the
-majority vote by members of the MP's party.  Unfortunately this will
-indicate that many members have rebelled in a free vote.  Until
-precise data on when and how strongly each party has whipped is made
-available, there is no true way of identifying a "rebellion".  Perhaps
-you might like to ask your party whip's office to post this information
-on their website.  We know of no heuristics which can reliably detect
-free votes, perhaps you can think of some and let us know.
+<p>"Rebellion" on this website means a vote against the majority vote by
+members of the MP's party.  Unfortunately this will indicate that many members
+have rebelled in a free vote.  Until precise data on when and how strongly each
+party has whipped is made available, there is no true way of identifying a
+"rebellion".  We know of no heuristics which can reliably detect free votes,
+perhaps you can think of some and let us know.  See also the next question.
 
-<h2>Where is the data in spreadsheet file format or in XML?</h2>
+<h2><a name="freevotes">Why do you incorrectly say people are rebels in free votes?</a></h2>
+
+<p>There is no official, recorded data about the party whip.  At the moment
+we guess based on the majority vote by MPs for each party.  In order to
+correctly identify rebels, we need to know each party's whip in each division.
+There are two ways this could be officially recorded.
+
+<ol>
+<li>Hansard clerks could record the whip.  They could either be officially told
+the whip by each party's whips' office, or they could deduce it from the
+presence of offical whips.  The whip would then be written in Hansard next to
+the division listing.
+<li>Each whips' office could publish their official whip on their website after
+each vote.  If you are a member of a political party, and want to fix the Public
+Whip site, lobby them to do this, then let us know.
+</ol>
+
+<p>Parties can't have it both ways - complain that we don't know what the whip is,
+and at the same time not tell us.  There is no contradiction in admitting the whip
+exists and recording it officially - after all some whips <a href="http://www.cabinet-office.gov.uk/civilservice/min-mp-pay/min_sal_tables.htm">are paid a salary by the taxpayer</a> so
+there is a precedent for admitting they exist.
+
+<h2><a name="spreadsheet">Where is the data in spreadsheet file format or in XML?</a></h2>
 
 <p> Take a look at our <a href="project/data.php">Raw Data</a> page.
 
@@ -113,7 +148,7 @@ inaccuracies.
 results from it, we ask you to refer your readers to
 www.publicwhip.org.uk.  This way they can use and contribute themselves.
 
-<h2>Can I play with the software?</h2>
+<h2><a name="playwith">Can I play with the software?</a></h2>
 
 <p> Sure.  All the software we've written is free (libre and gratuit), protected by the 
 <a href="GPL.php">GNU General Public License</a>.  It's not complicated,
@@ -122,7 +157,7 @@ this if you are going to change it as otherwise you will see the same
 results.  For more details go to the special <a
 href="project/code.php">coding section</a> of this website.
 
-<h2>Why are you giving everything away for free?</h2>
+<h2><a name="whyfree">Why are you giving everything away for free?</a></h2>
 
 <p>We're not; we're letting you take copies.  Whatever you do, we still 
 have our computers, all the programs, and our domain name.  The more 
@@ -136,19 +171,19 @@ would happen is that the people who are already organized influentially
 would retain all the power but would have slightly better software
 (which they probably have already). 
 
-<h2>What organisation is behind the Public Whip?</h2>
+<h2><a name="organisation">What organisation is behind the Public Whip?</a></h2>
 <p>None.  Just two guys <a href="http://www.flourish.org">Francis</a>
 and Julian who had an idea and made it happen.  <a
 href="http://www.knownoffender.net/">Giles</a> designed the look of the
 website.  We're hosted by the ever helpful and encouraging
 <a href="http://www.mythic-beasts.com/">Mythic Beasts</a>.
 
-<h2>How can I keep up with what you are doing?</h2>
+<h2><a name="keepup">How can I keep up with what you are doing?</a></h2>
 <p><a href="account/register.php">Subscribe to our newsletter!</a>  It's
 at most once a month, and has interesting news and articles
 relating to the project.
 
-<h2>Where can I email?</h2>
+<h2><a name="email">Where can I email?</a></h2>
 <p>If you have any problems, comments, queries, suggestions or offers of help email <a
 href="mailto:support@publicwhip.org.uk">support@publicwhip.org.uk</a>.
 
