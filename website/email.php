f6405efa4f6333e6139b0f8e9f8db45776ba6379
frabcus <frabcus>
2004-03-19 11:14:40 +0000

    Better email stuff

---
+++ website/email.php
@@ -0,0 +1,31 @@
+<?php $title = "Emailing Public Whip"; include "header.inc" 
+# $Id: email.php,v 1.1 2004/03/19 11:14:40 frabcus Exp $
+
+# The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
+# This is free software, and you are welcome to redistribute it under
+# certain conditions.  However, it comes with ABSOLUTELY NO WARRANTY.
+# For details see the file LICENSE.html in the top level of the source.
+?>
+
+<p>Public Whip is a project to watch MPs.  If you would like to contact someone
+about a particular issue or problem concerning you, try the following:
+
+<ul>
+
+<li>To contact your MP, please use <a href="http://www.faxyourmp.com">Fax Your MP</a>.
+
+<li>Some MPs have email addresses, you can look them up on
+<a href="http://www.parliament.uk/directories/hciolists/alms.cfm">this page</a>.
+
+<li>To take an issue up with central government, go to <a href="http://www.number-10.gov.uk">number 10</a>.
+
+</ul>
+
+<p>
+Email <a href="mailto:support@publicwhip.org.uk">support@publicwhip.org.uk</a> 
+for anything else (errors, questions, ideas, interviews, talks, grants) about Public Whip.
+
+<p>
+
+<?php include "footer.inc" ?>
+
