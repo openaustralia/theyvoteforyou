835b1b37f343189bc90cf497e2c6a6816ec3f2f4
frabcus <frabcus>
2004-04-16 13:38:56 +0000

    Ooops

---
+++ website/account/editdream.php
@@ -0,0 +1,108 @@
+<?  
+
+# $Id: editdream.php,v 1.1 2004/04/16 13:38:56 frabcus Exp $
+
+# The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
+# This is free software, and you are welcome to redistribute it under
+# certain conditions.  However, it comes with ABSOLUTELY NO WARRANTY.
+# For details see the file LICENSE.html in the top level of the source.
+
+include('database.inc');
+include('user.inc');
+include "../db.inc";
+
+$db = new DB(); 
+$just_logged_in = do_login_screen();
+
+if (user_isloggedin()) # User logged in, show settings screen
+{
+    $title = "Modify Your Dream MP"; 
+    include "../header.inc";
+
+    $id=db_scrub($_GET["id"]);
+    
+    $name=db_scrub($_POST["name"]);
+    $description=db_scrub($_POST["description"]);
+    $submit=db_scrub($_POST["submit"]);
+
+    $query = "select name, description, user_id from pw_dyn_rolliemp where rollie_Id = '$id'";
+    $row = $db->query_one_row($query);
+    if (!$name)
+        $name = $row[0];
+    if (!$description)
+        $description = $row[1];
+    $user_id = $row[2];
+
+    if (user_getid() != $user_id)
+    {
+        print "<p>This is not your Dream MP, so you can't edit their name or description.";
+    }
+    else
+    {
+        $ok = false;
+        if ($submit && (!$just_logged_in)) 
+        {
+            if ($name == "" or $description == "")
+                $feedback = "Please name your dream MP, and give a description.";
+            else
+            {
+                $db = new DB(); 
+                $ret = $db->query_errcheck("update pw_dyn_rolliemp set name='$name', description='$description' where rollie_id='$id'");
+
+                if ($ret)
+                {
+                    $ok = true;
+                    $feedback = "Successfully edited dream MP '" . html_scrub($name) . "'.  
+                     To see the changes, go to <a href=\"../dreammp.php?id=$id\">your
+                     dream MP's page</a>.";
+                    audit_log("Edited description dream MP '" . $name . "'");
+                }
+                else
+                {
+                    $feedback = "Failed to edit dream MP. " . mysql_error();
+                }
+            }
+        }
+
+        if ($feedback && (!$just_logged_in)) {
+            if ($ok)
+            {
+                echo "<p>$feedback</p>";
+            }
+            else
+            {
+                echo "<div class=\"error\"><h2>Modifying your dream MP not complete, please try again
+                    </h2><p>$feedback</div>";
+            }
+        }
+        else
+        {
+            print "<p>Here you change the name or description of your dream
+            MP.";
+
+        }
+
+        if (!$ok)
+        {
+        ?>
+            <P>
+            <FORM ACTION="<?=$PHP_SELF?>" METHOD="POST">
+            <B>Name (the name of your organisation or the issue your dream MP votes on behalf of):</B><BR>
+            <INPUT TYPE="TEXT" NAME="name" VALUE="<?=html_scrub($name)?>" SIZE="40" MAXLENGTH="50">
+            <P>
+            <B>Description (the criteria you will use to choose how your dream MP votes, give as much detail as possible):</B><BR>
+            <textarea name="description" rows="6" cols="80"><?=html_scrub($description)?></textarea></p>
+
+            <p><INPUT TYPE="SUBMIT" NAME="submit" VALUE="Edit Dream MP">
+            </FORM>
+        <?php
+        }
+    }
+}
+else
+{
+    login_screen();
+}
+?>
+
+<?php include "../footer.inc" ?>
