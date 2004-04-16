7343fed95de5b33b5152a4fcd174779c8ccee031
frabcus <frabcus>
2004-04-16 13:43:52 +0000

    PHP_SELF doesn't work the same on mythic

--- website/account/editdream.php
+++ website/account/editdream.php
@@ -1,6 +1,6 @@
 <?  
 
-# $Id: editdream.php,v 1.1 2004/04/16 13:38:56 frabcus Exp $
+# $dreamid: editdream.php,v 1.1 2004/04/16 13:38:56 frabcus Exp $
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -19,13 +19,13 @@ if (user_isloggedin()) # User logged in, show settings screen
     $title = "Modify Your Dream MP"; 
     include "../header.inc";
 
-    $id=db_scrub($_GET["id"]);
+    $dreamid=db_scrub($_GET["id"]);
     
     $name=db_scrub($_POST["name"]);
     $description=db_scrub($_POST["description"]);
     $submit=db_scrub($_POST["submit"]);
 
-    $query = "select name, description, user_id from pw_dyn_rolliemp where rollie_Id = '$id'";
+    $query = "select name, description, user_id from pw_dyn_rolliemp where rollie_Id = '$dreamid'";
     $row = $db->query_one_row($query);
     if (!$name)
         $name = $row[0];
@@ -47,13 +47,13 @@ if (user_isloggedin()) # User logged in, show settings screen
             else
             {
                 $db = new DB(); 
-                $ret = $db->query_errcheck("update pw_dyn_rolliemp set name='$name', description='$description' where rollie_id='$id'");
+                $ret = $db->query_errcheck("update pw_dyn_rolliemp set name='$name', description='$description' where rollie_id='$dreamid'");
 
                 if ($ret)
                 {
                     $ok = true;
                     $feedback = "Successfully edited dream MP '" . html_scrub($name) . "'.  
-                     To see the changes, go to <a href=\"../dreammp.php?id=$id\">your
+                     To see the changes, go to <a href=\"../dreammp.php?id=$dreamid\">your
                      dream MP's page</a>.";
                     audit_log("Edited description dream MP '" . $name . "'");
                 }
@@ -86,7 +86,7 @@ if (user_isloggedin()) # User logged in, show settings screen
         {
         ?>
             <P>
-            <FORM ACTION="<?=$PHP_SELF?>" METHOD="POST">
+            <FORM ACTION="editdream.php?id=<?=$dreamid?>" METHOD="POST">
             <B>Name (the name of your organisation or the issue your dream MP votes on behalf of):</B><BR>
             <INPUT TYPE="TEXT" NAME="name" VALUE="<?=html_scrub($name)?>" SIZE="40" MAXLENGTH="50">
             <P>
