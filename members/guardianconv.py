bcaf4e2ddf45794ce0b6e9134a51c4cf6f077139
frabcus <frabcus>
2004-05-13 13:14:01 +0000

    Now have swing to lose seat

--- members/guardianconv.py
+++ members/guardianconv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2.3
-# $Id: guardianconv.py,v 1.3 2004/05/12 16:56:36 frabcus Exp $
+# $Id: guardianconv.py,v 1.4 2004/05/13 13:14:01 frabcus Exp $
 
-# Converts tab file of Guardian URLs into XML
+# Converts tab file of Guardian URLs into XML.  Also extracts swing/majority
+# from the constituency page on the Guardian.
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -13,6 +14,8 @@ date = '2004-02-25'
 
 import sys
 import string
+import urllib
+import re
 sys.path.append("../pyscraper")
 from resolvemembernames import memberList
 
@@ -25,11 +28,23 @@ c = 0
 for l in ih:
     c = c + 1
     origname, origcons, personurl, consurl = map(string.strip, l.split("\t"))
+
+    # Match the name, and output basic URLs
     id, name, cons =  memberList.matchfullnamecons(origname, origcons, date)
     personid = memberList.membertoperson(id)
     cons = cons.replace("&", "&amp;")
     print '<personinfo id="%s" guardian_mp_summary="%s" />' % (personid, personurl)
     print '<consinfo canonical="%s" guardian_election_results="%s" />' % (cons.encode("latin-1"), consurl)
+
+    # Grab swing from the constituency page
+    ur = urllib.urlopen(consurl)
+    content = ur.read()
+    ur.close()
+    m = re.search("requires a (\d+\.\d+) \&\#037\; swing to gain seat", content)
+    if m:
+        print '<personinfo id="%s" today_swing_to_lose_mp_seat="%s" date="%s"/>' % (personid, m.group(1), date)
+    else:
+        print >>sys.stderr, "no match for swing at url %s" % consurl
 
 assert c == 659, "Expected %d MPs" % c
 
