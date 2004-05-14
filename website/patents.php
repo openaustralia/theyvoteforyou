269b3ac3a14b1981a907ad88add603d15eaad8ac
frabcus <frabcus>
2004-05-14 02:31:11 +0000

    Patent page

---
+++ website/patents.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Patent directive will destroy this website - The Public Whip</title>
+<link href="<?=$toppath?>publicwhip.css" type="text/css" rel="stylesheet">
+</head>
+
+<p align=center><img src="thepublicwhip.gif"></p>
+
+<p><b>The Public Whip is threatened by a proposed EU software patents
+directive</b>.  Many software patents are passed even though the "inventions"
+are obvious to anyone in the industry and there is prior art.  The EU directive
+will stifle innovation in software and destroy businesses.  Organisations will
+no longer own the software that they write, instead the corporations who
+collect trivial patents will.
+
+<p><b>On 17-18 May, the Irish Presidency is attempting to push through the
+software patents directive at a meeting of ministers.</b>  The European
+Parliament has voted for a version of the directive which is much better.
+The Council of Ministers is due to overrule the democratic decision of the
+Parliament, and pass the much worse original form of the directive.
+
+<p><b>Please act now to save The Public Whip and other cool software:</b>
+
+<p>
+<ul>
+<li>If you are in parliament, contact ***minister** and them to
+change Britain's vote in the Council of Ministers to be against the
+counter-proposal.
+<li>If you are a voter, <a href="http://www.faxyourmp.com">Fax Your MP</a> (free!) and tell your MP to write to ***minister*** and ask him to change our vote.
+<li>Media queries, please ring ***phonenumber*** of ***organisation***
+<li>Comments, or more information please <a href="mailto:support@publicwhip.org.uk">email us</a>.
+</ul>
+
+<p align=center><a href="index.php">Continue to where you wanted to be</a></p>
+
+<p><b>The Public Whip depends on our freedom to write software</b>
+
+<p><i>Further information:</i>
+<br><a href="http://swpat.ffii.org/journal/04/cons0507/index.en.html">EU Council Plans to Scrap Parliamentary Vote without Discussion</a>
+<br><a href="http://linuxtoday.com/news_story.php3?ltsn=2000-05-26-004-04-OP-LF">The Anatomy of a Trivial Patent</a>
+
+</html>
