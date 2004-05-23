ced57cdc0827911eaf1986fdd5aae9cf51664f6e
frabcus <frabcus>
2004-05-23 17:10:46 +0000

    Updated running code instructions

--- website/project/code.php
+++ website/project/code.php
@@ -1,5 +1,5 @@
 <?php $title = "Source Code"; include "../header.inc" 
-# $Id: code.php,v 1.3 2004/03/26 14:09:32 frabcus Exp $
+# $Id: code.php,v 1.4 2004/05/23 17:10:46 frabcus Exp $
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -18,27 +18,22 @@ software license.
 
 <p>At the moment I've only run this on Linux. It will certainly work on
 other Unixes, and it should run on Windows.  You need to install at
-least Perl and MySQL though, and preferably a web server with PHP also.
-Since so few people have used this code, there are bound to be problems.
-Don't hesitate to <a href="mailto:francis@flourish.org">email me</a> for
-help.
+least Python, preferably preferably MySQL, Perl and a web server with PHP also.
+Since so few people have used this code, there are bound to be problems.  Don't
+hesitate to <a href="mailto:francis@flourish.org">email me</a> for help.
 
 <p>If all this sounds like gobbledygook, then email me and ask for help.
 I can tell you where to go and what to read to learn how to get it up
 and running.  Or if there is a particular good idea that you want to try
 out, I might help more directly by implementing it.
 
-<p><i>This page is quite out of date now, and does not document the new parser.
-You should look at the Python parser which is in pyscraper, rather than the
-old Perl one.  Email to ask us for help, and I'll update this page.</i>
-
 <h2>Roughly how it works</h2>
-<p>Perl code downloads data from the UK parliament
-website, and stores it in a MySQL database. A combination of Perl and
-Octave (an open source mathematics package, compatible with Matlab) code
-perform various calculations on the data to form other database tables.
-The website is written in PHP and makes pages by querying the MySQL
-database.
+<p>Python code downloads data from the UK parliament website, stores it as an
+HTML file for each day, and parses those files into XML files.  A Perl script
+loads that data into a MySQL database.  A combination of Perl and Octave (an
+open source mathematics package, compatible with Matlab) code perform various
+calculations on the data to form other database tables.  The website is written
+in PHP and makes pages by querying the MySQL database.
 
 <h2>Getting the source code</h2>
 <A href="http://sourceforge.net/projects/publicwhip"> <IMG align=right vspace=8 hspace=8
@@ -66,21 +61,32 @@ module you want is called <i>publicwhip</i>.
 <p>There is README.txt file with the source code, explaining what is in
 each directory, and what the various todo and idea list files are.
 
-<h2>Setting up Perl and MySQL</h2>
+<h2>Setting up Python, Unix-utils, Perl and MySQL</h2>
+
+<p><span class="ptitle">Python</span> - Under Windows download <a
+href="http://www.python.org/download/">Python 2.3</a>.  Unix-based operating
+systems probably have Python already installed, but you may need to
+upgrade to Python 2.3.  You also need the <a
+href="http://www.egenix.com/files/python/mxDateTime.html">mxDateTime</a> module
+by eGenix, go to downloads on that page.  Under Debian this is in the package
+python2.3-egenix-mxdatetime.
+
+<p><span class="ptitle">Patch and Diff</span> - 
+The parser has a preprocessor which applies patches to Hansard to fix
+uncommon errors.  This is done using the tools "patch" and "diff",
+which will be installed by default if you are using Unix.  On Windows
+you can download them from 
+<a href="http://unxutils.sourceforge.net/">GNU utilities for win32</a>.
 
 <p><span class="ptitle">Perl</span> - Under Windows download <a
 href="http://www.activestate.com/ActivePerl/">ActivePerl</a>.
 Unix-based operating systems will probably have Perl already installed.
-You also need to install some extra special Perl modules for screen
-scraping (the automated downloading and parsing of Hansard's website).
 
-<p>My favourite of these is WWW::Mechanize, which provides
-an object that can programmatically browse the web - following links
-containing certain text, submitting forms and so on.  My second
-favourite is HTML::TokeParser which makes it a lot easier to extract
-data from an HTML file.
-
-<p>To install them follow the <a
+<p> If you want to run the old Perl scraper, you also need to install some
+extra special Perl modules for screen scraping (the automated downloading and
+parsing of Hansard's website).  You probably don't need these for
+the new Python scraper which you should be using (please let me
+know if you do or don't).  <p>To install them follow the <a
 href="http://www.cpan.org/misc/cpan-faq.html#How_install_Perl_modules">CPAN
 instructions</a> (Comprehensive Perl Archive Network) under Unix or <a
 href="http://aspn.activestate.com/ASPN/docs/ActivePerl-5.6/faq/ActivePerl-faq2.html">PPM instructions</a> (Perl Package Manager) under Windows.  
@@ -103,60 +109,41 @@ Tell me if this list is wrong.
 href="http://www.mysql.com">MySQL.com</a>.  After installing it you need
 to set up the instructions to create a database and a user with
 privileges to access that database. The database can have any name.
-
-<p>You need to set up the database tables, which all begin with
+You need to set up the database tables, which all begin with
 <i>pw_</i>.  To do this read the instructions at the top of the file
 create.sql.  It contains the SQL commands which create the tables.
 
 <h2>Scraping parliamentary transcripts</h2>
 
-<p>Use the command line and change to the <i>scraper</i> directory.
-The script called <i>scraper.pl</i> in there does all of the screen
-scraping from Hansard, and various cached calculations for use on the
-website.
+<p>Use the command line and change to the <i>pyscraper</i> directory.  The
+script called <i>lazyrunall.py</i> in there does all of the screen scraping
+from Hansard.  Run it with no parameters to find out its syntax.  Then
+do something like this 
 
-<p>Run scraper.pl with no parameters to find out its syntax.  Then you
-need to run it once with each of the following commands.  Or you can do
-them all in one go, such as <tt>./scrape.pl mps months sessions content
-divisions clean calc</tt>.
+<p><tt>./lazyrunall.py --from 2001-06-01 scrape parse
+debates wrans</tt>
 
-<p><span class="ptitle">mps</span> - Reads the files from the rawdata
-folder, which contain lists of MPs from the Parliamentary intranet.
-Adds these to the pw_mp database table.
+<p>That will screen scrape back to the start of the 2001 parliament, 
+writing the files in pwdata/cmpages.  Then it will parse these
+files into XML files and put them in pwdata/scrapedxml.  On Unix
+the pwdata folder is in your home directory, on Windows it will
+be in the same folder as the publicwhip folder which contains
+the source code.
 
-<p><span class="ptitle">months sessions</span> - Finds the URLs for the
-first page of each day's transcript, and adds them to the pw_hansard_day
-table.  months does this for recent months not yet in a bound volume.
-sessions does recent sessions, currently back to the start of this
-parliament.  Use just months to get going with a smaller amount of data,
-or both if you want the entire parliament. The script won't add URLs
-which already exist in the database, so you can run it multiple times,
-or run it for a short while and stop it.
+<p>The command above will gather both debates and written answers (wrans).
+You can run a command again and it will lazily make only those files which
+weren't downloaded/parsed last time.  When you are done, you should have
+lots of XML files in the pwdata/scrapedxml/debates folder.  Take a look
+at them.  These are used by the Perl script in the next phase.
 
-<p><span class="ptitle">content</span> - For every URL in the database,
-downloads the complete content of Hansard for that day.  This is
-extracted from multiple web pages, and made into one HTML file stored in
-pw_debate_content.  This takes some time and bandwidth; there are 155Mb
-of data for just the first two years of the 2001- parliament.  Only
-downloads data it doesn't already have. 
+<p>Change to the <i>scraper</i> directory.  The
+script called <i>scraper.pl</i> in there loads the divisons from the XML files
+into the database and does various cached calculations for use on the website.
+Run it with no parameters to find out its syntax.  First you have to tell Perl
+your MySQL username and password.  Copy the file config.pm.incvs to config.pm
+and edit it.  Now do this:
 
-<p><span class="ptitle">divisions</span> - For any days as yet
-unscanned (according to the divisions_extracted column in
-pw_debate_content), searches them for divisions.  Adds the divisions and
-voting record of MPs to the pw_division and pw_vote tables.
-
-<p><span class="ptitle">clean</span> - Having the misfortune to be using
-a database that doesn't support transactions, you need to run this after
-running divisions.  It removes any half-inserted divisions where there
-was an error during parsing.  Also checks for special cases where there
-are two divisions where one is a correction of another, and amalgamates
-them.
-
-<p><span class="ptitle">calc</span> - Fills in all the various pw_cache_
-tables with data calculated from the other tables.  This is stuff like
-guessing the rebellious votes and counting the attendance rates.  Note
-that the website won't work at all until you have done this, as it
-realies on the cached data tables.
+<p><tt>./scrape.pl divsxml check calc
 
 <p>While you're doing this all, you probably want to run a tool like the <a
 href="http://www.mysql.com/products/mysqlcc/">MySQL Control Center</a>
@@ -191,15 +178,16 @@ Alias /publicwhip/ /home/francis/devel/publicwhip/website/
 <p>You also need a .htaccess file to say how to handle .php files.  Two
 example ones htaccess-francis and htaccess-pworg are in the website
 folder.  Rename htaccess-francis to .htaccess as a first go - exactly
-what you need to do depends on the rest of your Apache configuration.
+what you need to do depends on the rest of your Apache configuration.  
+When you're done make sure you restart the webserver so it reloads its
+config files. 
 
-<p>When you're done make sure you restart the webserver so it reloads its
-config files, and browse to http://localhost/publicwhip/.
+<p>Finally you need to tell the PHP scripts about the database.  Copy
+the file pwdb.inc.incvs to pwdb.inc and edit it with your MySQL 
+settings.  You also need to edit db.inc to point to this file (TODO:
+work out a way to remove that need).
 
-<p>There's an extra hidden webpage which only works on your local
-machine, called hansard.php.  This lets you view complete days of
-hansard, extracted from pw_content.  View URLs in this form:
-http://localhost/publicwhip/hansard.php?date=2003-06-05
+<p>Now browse to <a href="http://localhost/publicwhip">http://localhost/publicwhip/</a>.
 
 <h2>Vote map (Clustering, Multi-dimensional scaling)</h2>
 
