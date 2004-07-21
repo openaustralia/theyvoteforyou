ee879d28f0d4540a78afa7e4b06470146c5de572
theyworkforyou <theyworkforyou>
2004-07-21 01:32:34 +0000

    Quieter chg pages Daily stuff

--- pyscraper/miniposts/crongrabpages.py
+++ pyscraper/miniposts/crongrabpages.py
@@ -70,7 +70,7 @@ def GrabWatchCopies(sdate):
 			# build the name for this page and make sure it follows, even when we have the same date
 			wwn = "%s%04d_%s.html" % (ww, lastnum + 1, sdate)
 			wwnj = os.path.join(watchdir, wwn)
-			print "changed page", wwnj
+			# print "changed page", wwnj
 
 			assert not os.path.isfile(wwn)
 			wout = open(wwnj, "w")
