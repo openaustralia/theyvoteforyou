900dcbfd8dfd484ea9c7bf798808d90c9239cb48
theyworkforyou <theyworkforyou>
2004-11-10 22:19:34 +0000

    Expenses Python script now does all years at once, and outputs the files itself and all other manner of nice things like that. Hence new XML files too.

--- members/expenses.py
+++ members/expenses.py
@@ -14,15 +14,18 @@ sys.path.append("../pyscraper/")
 import re
 from resolvemembernames import memberList
 
-date_today = datetime.date.today().isoformat()
-expmembers  = sets.Set() # for storing who we have found links for
+# date_today = datetime.date.today().isoformat()
 
-print '''<?xml version="1.0" encoding="ISO-8859-1"?>
-<publicwhip>'''
+for yearstr in ['200102', '200203', '200304']:
+	expmembers = sets.Set() # for storing who we have found links for
+	year = str( (int(yearstr)+2100)/101 )
+	yeardate = year + '-03-31'
+	xmlstr = 'expenses' + year
+	fout = open('expenses' + yearstr + '.xml', 'w')
+	fout.write('''<?xml version="1.0" encoding="ISO-8859-1"?>
+<publicwhip>\n''')
 
-yearstr = 'expenses2002'
-for year in [ '200102' ]:
-	file = open('../rawdata/mpsexpenses' + year + '.tsv')
+	file = open('../rawdata/mpsexpenses' + yearstr + '.tsv')
 	content = file.readlines()
 	file.close()
 
@@ -32,9 +35,6 @@ for year in [ '200102' ]:
 #    matcher = '<TD ALIGN="LEFT" VALIGN="TOP"><A HREF="(/weblink/html/member.html/.*)/log=\d+/pos=\d+" TARGET="_parent"><font face="arial,helvetica" size=2>(.*)/(.*)</A></TD>\s*<TD ALIGN="LEFT" VALIGN="TOP"><font face="arial,helvetica" size=2>(.*)</TD>'
 #    matches = re.findall(matcher, content)
 
-# Members’ Allowance Expenditure April 2003 – March 2004
-# Member	Constituency	Column 1 - Additional Costs Allowance	Column 2 - London Supplement	Column 3 - IEP	Column 4 - Staff Costs	Column 5 - Members' Travel	Column 6 - Members' Staff Travel	Column 7 - Centrally Purchased Stationery	Column 7a - Stationery: Associated Postage Costs	Column 8 - Central IT Provision	Column 9 - Other Costs
-
 	for line in content:
 		cols = line.split("\t")
 		first = cols[0]
@@ -42,7 +42,8 @@ for year in [ '200102' ]:
 		cons = cols[2]
 		money = cols[3:]
 		money = map(lambda x: re.sub("\xa3","", x), money)
-		id, name, cons =  memberList.matchfullnamecons(first + " " + last, cons, '2002-03-31')
+		money = map(lambda x: re.sub(",","", x), money)
+		id, name, cons =  memberList.matchfullnamecons(first + " " + last, cons, yeardate)
 		if not id:
 			raise Exception, "Failed to find MP %s %s" % (first, last)
 
@@ -51,21 +52,28 @@ for year in [ '200102' ]:
 		if id in expmembers:
 			print >>sys.stderr, "Ignored repeated entry for " , id
 		else:
-			print '<memberinfo id="%s" ' % id
-			for i in [ 0,1,2,3,4,5,6,7,8 ]:
-#				if (i==7):
-#					col = '7a'
-#				elif (i==8 or i==9):
-#					col = i
-#				else:
-				col = i+1
-				print '%s_col%s="%s" ' % (yearstr, col, money[i].strip())
-			print '/>'
+			fout.write('<memberinfo id="%s" ' % id)
+			for i in [ 0,1,2,3,4,5,6,7,8,9 ]:
+				if (year=='2004'):
+					if (i==7):
+						col = '7a'
+					elif (i==8 or i==9):
+						col = i
+					else:
+						col = i+1
+				else:
+					if (i<9):
+						col = i+1
+					else:
+						continue
+				fout.write('%s_col%s="%s" ' % (xmlstr, col, money[i].strip()))
+			fout.write('/>\n')
 		expmembers.add(id)
 
 	sys.stdout.flush()
 
-print '</publicwhip>'
+	fout.write('</publicwhip>\n')
+	fout.close()
 
 # Check we have everybody
 #allmembers = sets.Set(memberList.currentmpslist())
