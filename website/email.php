9b3d9f5d4400f58ff0c8d3ced796ec983dd5e3e6
frabcus <frabcus>
2004-11-22 18:09:48 +0000

    Not part of the government

--- website/email.php
+++ website/email.php
@@ -1,5 +1,5 @@
 <?php $title = "Emailing Public Whip"; include "header.inc" 
-# $Id: email.php,v 1.1 2004/03/19 11:14:40 frabcus Exp $
+# $Id: email.php,v 1.2 2004/11/22 18:09:48 frabcus Exp $
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -7,23 +7,31 @@
 # For details see the file LICENSE.html in the top level of the source.
 ?>
 
-<p>Public Whip is a project to watch MPs.  If you would like to contact someone
-about a particular issue or problem concerning you, try the following:
+<p>Public Whip is an independent project to help the public watch MPs.
+If you would like to contact someone about a particular issue or problem
+concerning you, instead try the following:
 
 <ul>
 
-<li>To contact your MP, please use <a href="http://www.faxyourmp.com">Fax Your MP</a>.
+<li><a href="http://www.faxyourmp.com">Fax Your MP</a> to contact your
+own MP.
 
-<li>Some MPs have email addresses, you can look them up on
-<a href="http://www.parliament.uk/directories/hciolists/alms.cfm">this page</a>.
+<li>
+There are <a href="http://www.parliament.uk/directories/hciolists/alms.cfm">
+email addresses</a> for MPs who make them available.
 
-<li>To take an issue up with central government, go to <a href="http://www.number-10.gov.uk">number 10</a>.
+<li>
+<a href="http://www.number-10.gov.uk">Number 10</a> is a good place to
+start if you would like to take an issue up with central government.
 
 </ul>
 
 <p>
-Email <a href="mailto:support@publicwhip.org.uk">support@publicwhip.org.uk</a> 
-for anything else (errors, questions, ideas, interviews, talks, grants) about Public Whip.
+You can also contact us  with errors, questions, ideas, interviews, talks,
+and offers of grants,  but only about the Public Whip project itself.  We're just a few
+people who run a website, and <b>not part of the government</b>.  Please email
+<a
+href="mailto:support@publicwhip.org.uk">support@publicwhip.org.uk</a>.
 
 <p>
 
