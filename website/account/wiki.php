acae5326c2bc8102340e55c7d8163e03601201c2
frabcus <frabcus>
2005-02-18 13:19:35 +0000

    Recent edits display

--- website/account/wiki.php
+++ website/account/wiki.php
@@ -1,5 +1,5 @@
 <?php require_once "../common.inc";
-# $Id: wiki.php,v 1.4 2005/01/22 14:46:40 frabcus Exp $
+# $Id: wiki.php,v 1.5 2005/02/18 13:19:35 frabcus Exp $
 # vim:sw=4:ts=4:et:nowrap
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
@@ -24,7 +24,7 @@ if (user_isloggedin()) # User logged in, show settings screen
     $r = db_scrub($_GET["r"]);
 
     $title = "Edit Text"; 
-    if (preg_match("/^motion-(\d\d\d\d-\d\d-\d\d)-(\d+)$/",$key, $matches)) {
+    if ($matches = get_motion_from_key($key)) {
         $division_date = $matches[1];
         $division_number = $matches[2];
         $db->query("select * from pw_division where division_date = '$division_date' 
---
+++ website/edits.php
@@ -0,0 +1,56 @@
+<?php require_once "common.inc";
+
+    # $dreamid: dreammp.php,v 1.4 2004/04/16 12:32:42 frabcus Exp $
+
+    # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
+    # This is free software, and you are welcome to redistribute it under
+    # certain conditions.  However, it comes with ABSOLUTELY NO WARRANTY.
+    # For details see the file LICENSE.html in the top level of the source.
+
+    include "db.inc";
+    include "gather.inc";
+    include "pretty.inc";
+    $db = new DB(); 
+
+    $title = "Motion Text Edits";
+    include "header.inc";
+?>
+   <p>Recent changes to motion text.
+<?php
+
+    print "<table>\n";
+    print "<tr class=\"headings\">
+        <td>Object</td>
+        <td>Text</td>
+        <td>Made by</td>
+        <td>Date</td>
+        </tr>";
+
+    $query = "select object_key, text_body, user_name, pw_dyn_user.user_id as user_id, 
+                     edit_date 
+             from pw_dyn_wiki, pw_dyn_user
+             where pw_dyn_wiki.user_id = pw_dyn_user.user_id
+             order by pw_dyn_wiki.edit_date desc";
+    $db->query($query);
+
+    $prettyrow = 0;
+    while ($row = $db->fetch_row_assoc())
+    {
+        $prettyrow = pretty_row_start($prettyrow);
+        if ($matches = get_motion_from_key($row['object_key'])) {
+            $division_date = $matches[1];
+            $division_number = $matches[2];
+            print "<td><a href=\"division.php?date=\"" . $division_date . "&number=" . $division_number . ">" . $division_date . "#" . $division_number . "</a></td>";
+        } else {
+            print "<td>" . $row['object_key'] . "</td>";
+        }
+        print "<td>" . trim_characters(sanitise_wiki_text_for_display($row['text_body']), 0, 200) . "</td>\n";
+        print "<td>" . html_scrub($row['user_name']) . "</td>";
+        print "<td>" . $row['edit_date'] . "</td>\n";
+        print "</td></tr>";
+    }
+    print "</table>\n";
+
+?>
+
+<?php include "footer.inc" ?>
