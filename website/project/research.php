ed165d544d26b50ac1af9e3788550975568d1976
frabcus <frabcus>
2005-03-28 12:03:19 +0000

    Added research project page

---
+++ website/project/research.php
@@ -0,0 +1,60 @@
+<?php require_once "../common.inc";
+# $Id: research.php,v 1.1 2005/03/28 12:03:19 frabcus Exp $
+
+# The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
+# This is free software, and you are welcome to redistribute it under
+# certain conditions.  However, it comes with ABSOLUTELY NO WARRANTY.
+# For details see the file LICENSE.html in the top level of the source.
+
+$title = "Parliamentary Research"; include "../header.inc" 
+?>
+
+<h2>Introduction</h2>
+
+<p>Lots of people use Public Whip to understand what parliament is doing,
+and to research their area of special interest.  We'd like to share this
+workload, so you get other peoples research and clarifications, and 
+contribute your own.  It's also much less lonely this way!
+
+<p>There are lots of ways of doing this, here are some tools we provide
+so far.  <a href="mailto:team@publicwhip.org.uk">Email us</a> if you have 
+questions and comments, or <a href="/forum">chat in the forum</a>.
+
+<h2>Motion Text Editing</h2>
+
+<p>Above the motion text for every parliamentary division, we currently
+make the following statement:
+
+   <blockquote>"Procedural text extracted from the debate, so you can try to work
+   out what 'aye' (for the motion) and 'no' (against the motion)
+   meant. This is for guidance only, irrelevant text may be shown,
+   crucial text may be missing."</blockquote>
+
+<p>While <a href="http://www.theyworkforyou.com">TheyWorkForYou.com</a> helps
+make debates more friendly, it doesn't make the motions more readable, and we
+still often guess it wrong. Not only that, even when we get it right, the
+accurate text uses too much Parliamentary jargon and refers to other documents
+by citing page and line numbers (for example).
+
+<p>Public Whip allows anyone to write a plain english version of the
+motion, with links to explanations and relevant document.  Julian has taken on
+the task of doing <a href="/dreammp.php?id=210">all the Iraq votes</a> and the 
+<a href="/dreammp.php?id=258">counter-terrorism legislation</a>.  Have a
+look at them as examples.
+
+<p>We are calling for anyone who is passionate about other issues in
+Parliament that have been subject to frequent, repeated votes by MPs.
+It could be immigration, education, the environment, transport, ID
+cards, international development, and so on.  Find the divisions, mark
+them with a Dream MP, and fix the motion text so that other people can
+see the precise measures that were voted upon.  Public Whip is about
+informed choice, and not having to take anyone's word for things.
+
+<h2>Commenting on Debates</h2>
+
+<p>Over at <a href="http://www.theyworkforyou.com">TheyWorkForYou.com</a>
+you can add comments to parliamentary debates.    This is a good way to add
+information you have found out about the discussion where anyone can find it.
+
+<?php include "../footer.inc" ?>
+
