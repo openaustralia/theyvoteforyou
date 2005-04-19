c5cd73b5217cacf03ca026864d4f1f0e874f0e35
frabcus <frabcus>
2005-04-19 00:23:55 +0000

    Added guidelines from Julian

--- website/project/research.php
+++ website/project/research.php
@@ -1,5 +1,5 @@
 <?php require_once "../common.inc";
-# $Id: research.php,v 1.1 2005/03/28 12:03:19 frabcus Exp $
+# $Id: research.php,v 1.2 2005/04/19 00:23:55 frabcus Exp $
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
 # This is free software, and you are welcome to redistribute it under
@@ -55,6 +55,116 @@ informed choice, and not having to take anyone's word for things.
 <p>Over at <a href="http://www.theyworkforyou.com">TheyWorkForYou.com</a>
 you can add comments to parliamentary debates.    This is a good way to add
 information you have found out about the discussion where anyone can find it.
+
+<h2>Guidelines for editing the text of motions</h2>
+
+<ul>
+
+<li>Avoid Parliamentary jargon.  Phrases like "Question Put", "I beg to move",
+and "Third Reading" should not be used unless explained.  
+
+<li>Make it clear which side won the vote so people don't need to read the
+numbers to discover the result.  When they do look at the vote chart after
+reading the motion text, they should already know what the Ayes and Noes mean.  
+
+<li>Whenever there is an amendment to a motion, both the original motion and
+the amended motion should be listed, never the amendment which is meaningless
+on its own.  
+
+<li>Include all possible hyper-links.  Parliamentary motions can mention
+Departmental Reports, Command Papers, UN resolutions, items in the news, and
+past statements or votes in the House.  Track everything down using Google and
+make links to them so that people who don't know these source documents exist
+can find them.   Political statements must at no time be more than one click
+away from the facts that support or dispute it.  
+
+<li>If there is a vote to pass a Statutory Instrument, link to the Statutory
+Instrument and the Statute.  Votes to change the Parliamentary Standing Orders
+should link to both versions of the Standing Order and provide an explanation
+and examples.  However, not every mention of a Standing Order needs to be
+linked if it's the Speaker reading them out to justify what going to happen
+next.  
+
+<li>A division on a piece of legislation must have the full and correct name of
+the Bill in the title followed by a dash "--" and any further qualifiers:
+"Money Resolution", "Second Reading", "Programme Motion", etc.  
+
+<li>The reason for these qualifiers in a legislation division is to put the
+vote in its place within the standard procedural plot.  Links to the next unit
+(eg the Standing Committee page) and the preceding unit of the plot is also
+desirable.  
+
+<li>You must endeavor to link to the correct version of the Bill.  There can be
+several and you must carefully examine the titles and the dates to be sure to
+get them right.  The links to them are often broken, but the pages can be found
+by searching for the title in quotes plus site:parliament.uk in Google.  
+
+<li>When a clause or set of clauses are agreed to, link to them directly and
+summarize their meaning if possible.  Interpret the meaning as best you can,
+and if you can't fully understand it, say so.  If a clause amends another Act
+that is online, then link directly to that part of the Act that has been
+changed.  In the future we will be able to create back-links.  The word
+"clause" and "section" are sometimes used interchangeably.  
+
+<li>Amendments to clauses are the hardest part of the job.  You have to read
+the top and the bottom of the debate relating to the division very carefully.
+When the Speaker says: "With this it will be convenient to discuss the
+following Amendments" he's telling you the subject of the debate, not the
+meaning of the division.  The division only applies to what the MP said in the
+phrase: "I beg to move".  However, some of the amendments the speaker lists may
+need to be considered because the amendment that was moved is incomplete on its
+own.  This is because each contiguous change in the text is a separate
+amendment.  It's sometimes possible to find a webpage listing all the
+amendments.  
+
+<li>Watch out for the phrase "Amendment by leave withdrawn" just before the
+division is called, or you will put in the wrong text.  Sometimes the Speaker
+restates the amendment before or after the division, but many times not.  Watch
+out for negations: "That the original words of the clause stand part" versus
+"That the amendment be made."  
+
+<li>Once you have started working through the votes on a Bill, stick to it.  It
+can take as long as an hour to piece together the meaning of a division in the
+worst case.  Fortunately, they don't vote on the most boring parts of the Bill,
+so you don't need to analyze them.  However, it's a good idea to page through
+the full text of a Bill to get a feel for its content.  Once you get to know it
+a little bit you'll be best placed to finish it off, so do so.  
+
+<li>As well as making the motion text as clear, readable and honest as possible
+so that people can decide how they feel about the MPs who voted for or against
+it, leave it in a state that gives those who want to do further research a head
+start.  If you spent an hour working out a motion, think of it as saving an
+hour of someone else's time, which means they will be able to do research that
+would otherwise not have been possible.  For example, newspaper reporters need
+to get to creditable facts quickly.  You may have given them a channel into the
+heart of the process which means they don't have to rely on a self-serving
+Government press notice to track down the news.  
+
+<li>Motion text editors are independent of Parliament, which means we can be
+critical of its process in the way that anyone in the system is unwilling or
+unable to be.  But check up on each other's work whenever you can, and discuss
+it in the forum.  
+
+<li>We have experienced little use and no actual signs of abuse.  Until we
+experience abuse, we cannot make provisions for it.  In practice, wikis
+experience surprisingly little abuse, probably because it's usually obvious and
+there are more people prepared to erase the abuse than create it.  There's
+always the chance that a party may try to hijack this outlet as they do so many
+political happening.  However it's unlikely because it could too easily be
+found out and backfire.  
+
+<li>To prevent us (Francis and Julian) from abusing PublicWhip, we have
+released all the code under the General Public License.  We would also like to
+establish a public repository for all the DreamMP and Motion Text information
+so that we can't be accused of owning the information.  The safeguard is that
+anyone with the right skills can download and establish a rival version of
+PublicWhip (under another domain name) in case we get lazy, sell out, or run
+into some sort of legal hassle.  We must ensure that it can live on by
+disabling our ability to abolish it.  
+
+</ul>
+
+<p>&mdash; Julian Todd, 10 April 2005
 
 <?php include "../footer.inc" ?>
 
