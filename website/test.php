ad129d2216793fc316425df7170188947947c056
frabcus <frabcus>
2005-10-11 23:43:02 +0000

    Code to programmatically post to forum from within another request.

--- website/test.php
+++ website/test.php
@@ -1,11 +1,13 @@
 <?
+exit;
 require_once "common.inc";
 require_once "forummagic.inc";
 require_once "db.inc";
 print "<p>doing";
 
 $db = new DB();
-post_to_forum($db, "Policies", "Renaming Dream MPs as Policies");
+#post_to_forum($db, "Policies", "Renaming Dream MPs as Policies", "This is the content with lots of stuff in it blah blah.\nAnd so it goes on.\n");
+forummagic_post($db, "Policies", "Another Topic Made Auto Post", "This is the content with lots of stuff in it blah blah.\nAnd so it goes on.\n");
 
 
 
