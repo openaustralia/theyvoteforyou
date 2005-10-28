8fe759b5e11a2f6b84fa385cbcdb62f9818c0875
publicwhip <publicwhip>
2005-10-28 17:33:56 +0000

    Reuse config file on level above

--- website/forum/config.php
+++ website/forum/config.php
@@ -1,15 +1,16 @@
 <?php
 
+require_once "$phpbb_root_path/../config.php";
 
 // phpBB 2.x auto-generated config file
 // Do not change anything in this file!
 
 $dbms = 'mysql';
 
-$dbhost = '';
-$dbname = '';
-$dbuser = '';
-$dbpasswd = '';
+$dbhost = $pw_host;
+$dbname = $pw_database;
+$dbuser = $pw_user;
+$dbpasswd = $pw_password;
 
 $table_prefix = 'phpbb_';
 
