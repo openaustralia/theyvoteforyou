79def8bdea1f87f07d167f1a32a26503bec5c14e
publicwhip <publicwhip>
2006-03-12 13:14:28 +0000

    Fix broken XML feeds. Other stuff.

--- website/mps-xml.php
+++ website/mps-xml.php
@@ -1,5 +1,5 @@
 <?php require_once "common.inc";
-    # $Id: mps-xml.php,v 1.7 2006/03/07 14:17:45 frabcus Exp $
+    # $Id: mps-xml.php,v 1.8 2006/03/12 13:14:30 publicwhip Exp $
 
     # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
     # This is free software, and you are welcome to redistribute it under
@@ -21,6 +21,16 @@
 	$db->query($query);
     $row = $db->fetch_row_assoc();
     $last_div_date = $row['division_date'];
+
+    $mps_query_start = "select first_name, last_name, title, constituency,
+            party, pw_mp.mp_id as mp_id,
+            round(100*rebellions/votes_attended,0) as rebellions,
+            round(100*votes_attended/votes_possible,0) as attendance,
+            entered_reason, left_reason, entered_house, left_house,
+            house
+            from pw_mp,
+            pw_cache_mpinfo where
+            pw_mp.mp_id = pw_cache_mpinfo.mp_id";
 
     $order = "entered_house, last_name, first_name, constituency";
     $query = "$mps_query_start and house = 'commons' order by $order";
