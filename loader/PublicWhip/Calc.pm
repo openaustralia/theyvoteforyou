0b601b2916eed4affd1fa5debd21343ab43f35de
publicwhip <publicwhip>
2006-03-20 19:08:24 +0000

    Rankings for Lords

--- loader/PublicWhip/Calc.pm
+++ loader/PublicWhip/Calc.pm
@@ -1,4 +1,4 @@
-# $Id: Calc.pm,v 1.11 2006/03/15 17:57:32 frabcus Exp $
+# $Id: Calc.pm,v 1.12 2006/03/20 19:08:24 publicwhip Exp $
 # Calculates various data and caches it in the database.
 
 # The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
@@ -41,20 +41,27 @@ sub current_rankings {
         );"
     );
 
+    do_house_ranking($dbh, "commons");
+    do_house_ranking($dbh, "lords");
+}
+
+sub do_house_ranking {
+    my ($dbh, $house) = @_;
+    
     # Select all MPs in force today, and their attendance/rebellions
     my $mps_query_start = "select pw_mp.mp_id as mp_id, 
             round(100*rebellions/votes_attended,2) as rebellions,
             round(100*votes_attended/votes_possible,2) as attendance
             from pw_mp, pw_cache_mpinfo 
             where pw_mp.mp_id = pw_cache_mpinfo.mp_id 
-                  and house = 'commons' ";
+                  and house = '$house' ";
     my $sth = PublicWhip::DB::query( $dbh, $mps_query_start .
             "and entered_house <= curdate() and curdate() <= left_house");
     if ($sth->rows == 0) {
         $sth = PublicWhip::DB::query( $dbh, $mps_query_start .
             "and left_house = '2005-04-11'");
         if ($sth->rows == 0) {
-            PublicWhip::Error::log( "No MPs currently active have been found, change General Election date in code if you are coming up to one", "", ERR_IMPORTANT );
+            PublicWhip::Error::log( "No MPs/Lords currently active have been found, change General Election date in code if you are coming up to one", "", ERR_IMPORTANT );
             return;
         }
     }
