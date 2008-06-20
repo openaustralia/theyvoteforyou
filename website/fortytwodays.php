2749c0733574c93afb932f29682b0077b0c69ae4
goatchurch <goatchurch>
2008-06-20 13:57:21 +0000

    more progress and feedback on 42 days

--- website/fortytwodays.php
+++ website/fortytwodays.php
@@ -47,123 +47,8 @@ function GetVote($votes, $date, $number)
     return "notmp";
 }
 
-
-$db = new DB();
-$db2 = new DB();
-
-header("Content-Type: text/html; charset=UTF-8");
-
-print "<html>\n";
-print "<head>\n";
-print "<title>The Public Whip - Forty-Two Days</title>\n";
-print "<link href=\"publicwhip.css\" type=\"text/css\" rel=\"stylesheet\"/>\n";
-print "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
-
-print "<style type=\"text/css\">\n";
-print "
-body { width:800px; margin-left:auto; margin-right:auto; }
-input { color: #303070 }
-div#Zopform {  border: thin black solid; padding: 10px }
-div.quessec { border: thin black solid; padding: 10px; margin-top:10px; }
-div.oprad { margin-top:10px; padding:5px; background-color:#e0e0d0; font-size:120%;}
-div#checkmpbutton { font-size: 200%; text-align:center; margin:10px;}
-div#footer { background-color:black; color:white;  border: thin black solid; }
-table.votetab td { border: thin black solid; }
-div#yourfav { background-color:#fed9d8; }
-    ";
-
-print "</style>\n";
-
-print "</head>\n";
-print "<body class=\"fortytwodays\">\n";
-#print "<script type=\"text/javascript\" src=\"quiz/election2008.js\"></script>\n";
-
-print "<h1>The Public Whip - Forty-two days</h1>\n";
-#print "<h4 id=\"th4\">The candidate calculator for <i>$constituency</i></h4>\n";
-#print "<p></p>\n\n";
-
-if (is_postcode($_GET["mppc"]) or $_GET["mpc"])
-    $mpval = get_mpid_attr_decode($db, $db2, "");
-$vterdet = $_GET["vterdet"];
-$vpubem = $_GET["vpubem"];
-
-if ($mpval)
+function WriteYourSummary($vterdet)
 {
-    $mpprop = $mpval["mpprop"];
-    $party = $mpprop["party"];
-    $name = $mpprop["name"];
-    $constituency = $mpprop["constituency"];
-    $minentered_house = $mpval["minentered_house"];
-
-    #print_r($mpval);
-    $votes = GetVotes($db, $mpprop["person"], 1039);
-    #print_r($rows);
-
-    $part4_terroristcertification = GetVote($votes, "2001-11-21", 75);
-    $part4_indefinitedetention = GetVote($votes, "2001-11-21", 76);
-    $part4_siac = GetVote($votes, "2001-11-21", 77);
-    $part4_privy = GetVote($votes, "2004-02-25", 59);
-    $part4_renewal = GetVote($votes, "2004-03-03", 71);
-
-    $d90days = GetVote($votes, "2005-11-09", 84);
-    $d28days = GetVote($votes, "2005-11-09", 85);
-
-    $d42days_enable = GetVote($votes, "2008-06-11", 219);
-    $d42days_procedure = GetVote($votes, "2008-06-11", 220);
-
-    print "<p>Your MP, <b>$name</b>,
-           currently representing the constituency of <b>$constituency</b>,
-           entered Parliament\n";
-    if ($minentered_house == "1997-05-01")
-        print " on or before the May 1997 General Election,\n";
-    else
-        print " on ".pretty_date($minentered_house).",\n";
-    print " and was able to vote on the detention of terrorist suspects without charge ";
-    print " for up to 42 days";
-    if ($d90days <> "notmp")
-        print " and 90 days.";
-    else
-        print " but not 90 days.";
-    print "</p>\n";
-
-    print "<p>\n";
-    if ($minentered_house <= "2001-11-21")
-        print " $name was also in Parliament for the votes which established detention without charge or trial of
-                foreign nationals whom the Home Secretary believed were terrorists.";
-    else if ($minentered_house <= "2004-03-03")
-        print " $name was not in Parliament for the vote which established detention without charge of
-                foreign nationals whom the Home Secretary believed were terrorists, but was there 
-                when MPs voted to renew these powers were renewed.";
-    else
-        print " $name was not in Parliament at the time when MPs established detention without charge of
-                foreign nationals whom the Home Secretary believed were terrorists.";
-    print "</p>\n";
-
-    print "<p>The votes on these issues were as follows:</p>\n";
-    print "<table class=\"votetab\">\n";
-    if ($part4_terroristcertification <> "notmp")
-    {
-        print "<tr><td>21&nbsp;November&nbsp;2001</td><td>Power of Home Secretary to certify people as terrorists</td><td>$part4_terroristcertification</td></tr>\n";
-        print "<tr><td>21&nbsp;November&nbsp;2001</td><td>Ability to detain certified terrorists indefinitely if they are foreign</td><td>$part4_terroristcertification</td></tr>\n";
-    }
-    if ($part4_renewal <> "notmp")
-    {
-        print "<tr><td>3&nbsp;March&nbsp;2004</td><td>Renewal of the regime to detain foreign terrorists indefinitely</td><td>$part4_renewal</td></tr>\n";
-    }
-    if ($d90days <> "notmp")
-    {
-        print "<tr><td>9&nbsp;November&nbsp;2005</td><td>Extention of period of detention to 90 days</td><td>$d90days</td></tr>\n";
-        print "<tr><td>9&nbsp;November&nbsp;2005</td><td>Extention of period of detention to 28 days (avoiding 60 days)</td><td>$d28days</td></tr>\n";
-    }
-
-    if ($d42days_enable <> "notmp")
-    {
-        print "<tr><td>11 June 2008</td><td>Define the conditions for detention up to 42 days</td><td>$d42days_enable</td></tr>\n";
-        print "<tr><td>11 June 2008</td><td>Create the power to detain terrorist suspects up to 42 days</td><td>$d42days_procedure</td></tr>\n";
-    }
-    print "</table>\n";
-
-    print "<div id=\"yourfav\">\n";
     if ($vterdet == "ind")
     {
         print "<p>You are in favour of indefinite detention without charge of
@@ -213,37 +98,228 @@ if ($mpval)
                and was the state of the law between 2000 and November 2003.</p>\n";
     }
     else if ($vterdet == "7i")
-        print "<p>You are in favour of detaining terrorist suspects in connection to 
-               the affairs of Northern Ireland for up to 7 days before charging 
-               them with a crime.  
-               This was the state of the law from 1974 until 2000 when the powers 
+    {
+        print "<p>You are in favour of detaining terrorist suspects in connection to
+               the affairs of Northern Ireland for up to 7 days before charging
+               them with a crime.
+               This was the state of the law from 1974 until 2000 when the powers
                of detention were widened to all terrorism-related acts.</p>";
-    else
-        print "<p>You are in favour of treating terrorism on the level of any other 
-               crime, like murder, kidnapping, or rape, where the police have 
-               no more than 48 hours to decide whether to charge someone they 
-               have arrested, or let them go.  
-               This was the state of the law until 1974 when a special case 
-               was made for terrorism relating to Northern Ireland, but 
+    }
+    else if ($vterdet == "2")
+    {
+        print "<p>You are in favour of treating terrorism on the level of any other
+               crime, like murder, kidnapping, or rape, where the police have
+               no more than 48 hours to decide whether to charge someone they
+               have arrested, or let them go.
+               This was the state of the law until 1974 when a special case
+               was made for terrorism relating to Northern Ireland, but
                it was not until 2000 when it was extended to all other sources of terrorism.</p>";
-    print "</div>\n"; 
+    }
+    else if ($vterdet)
+        print "<p>Not recognized: $vterdet</p>\n";
+}
 
-    #print_r($mpval);
-    print "<p>Your selection was <b>$vterdet</b></p>\n";
-    print "<p>We need some thoughts on how we are going to design this page to respond to the four options.  Also,
-           the answers for David Davis and Boris Johnson's constituencies will be different.
-           We can also add -- in special cases -- a: 'BTW, your MP also voted to exempt himself and Parliament from
-           the Freedom of Information Act'.</p>\n";
-    print "<p>The material we have to work with can be seen by <b>clicking <a href=\"/mp.php?".$mpval["mpprop"]["mpanchor"]."&dmp=1039\">here</a> to compare ";
-    print $mpval["mpprop"]["fullname"]." to 'No detention without charge'.</b></p>\n";
-    print "<p>We can begin with what we are going to say to someone who agrees it should be less than 28.</p>\n";
-    print "<p>The next bubble along, Aidan, is probably ID cards, with questions like:\n";
-    print "<a href=\"http://www.publicwhip.org.uk/division.php?date=2006-02-13&number=160&dmp=230\">Are you for or against the government having to publish a detailed report about their cost and benefits?</a>.</p>\n";
+
+function WriteMPvoterow($mpanchor, $vnvote)
+{
+    if ($vnvote[3] == "notmp")
+        return;
+    print "<tr><td>".pretty_date($vnvote[0])."</td>";
+    print "<td><a href=\"http://www.publicwhip.org.uk/division.php?date=".$vnvote[0]."&number=".$vnvote[1]."&$mpanchor\">";
+    print $vnvote[2];
+    print "</a></td>";
+    print "<td>".$vnvote[3]."</td>";
+    print "</tr>\n";
+}
+
+function WriteMPvotetable($vnvotes, $vterdet, $vpubem, $mpanchor)
+{
+    # could also add a colour in to the aye column according to vterdet
+    WriteMPvoterow($mpanchor, $vnvotes["part4_terroristcertification"]);
+    WriteMPvoterow($mpanchor, $vnvotes["part4_indefinitedetention"]);
+    WriteMPvoterow($mpanchor, $vnvotes["public_emergency"]);
+    WriteMPvoterow($mpanchor, $vnvotes["part4_renewal"]);
+    WriteMPvoterow($mpanchor, $vnvotes["d90days"]);
+    WriteMPvoterow($mpanchor, $vnvotes["d28days"]);
+    WriteMPvoterow($mpanchor, $vnvotes["d42days_procedure"]);
+}
+
+function WriteTimeline()
+{
+    print "<ul>
+           <li>1974 1986 1989 Temporary Provisions (up to seven days)</li>
+           <li>2000 7 days Terrorism Act</li>
+           <li>2001 indefinitely for foreign nationals - Anti-Terrorism Act</li>
+           <li>2001 public emergency declared</li>
+           <li>2003 14 days Criminal Justice Act</li>
+           <li>2004 (indefinite detention struck down due to violation of ECHR)</li>
+           <li>2005 public emergency lifted</li>
+           <li>2006 28 days Terrorism Act</li>
+           <li>2008 42 days Counter-Terrorism Act</li>
+           </ul>
+          ";
+}
+
+function WritePubemChart($vpubem)
+{
+    $query = "SELECT SUM(vpubem = 'no-disagree') AS no_disagree,
+                     SUM(vpubem = 'no-agree') AS no_agree,
+                     SUM(vpubem = 'yes-agree') AS yes_agree,
+                     SUM(vpubem = 'yes-disagree') AS yes_disagree,
+                     COUNT(vpubem is not NULL) AS total
+              FROM pw_logincoming
+              LEFT JOIN pw_dyn_fortytwoday_comments ON pw_dyn_fortytwoday_comments.vrand = pw_logincoming.thing_id
+             ";
+	$row = $db2->query_one_row_assoc($query);
+    print "<table><tr><th>Didn't know<br/>don't agree</th><th>Didn't know<br/>but agree</th>
+                      <th>Knew<br/>and agree</th><th>Knew<br/>but disagreed</th></tr>";
+    print "<tr><td>".$row["no_disagree"]."</td><td>".$row["no_agree"]."</td>
+               <td>".$row["yes_agree"]."</td><td>".$row["yes_disagree"]."</td></tr>";
+    print "</table>\n";
+}
+
+# start of output
+$db = new DB();
+$db2 = new DB();
+
+header("Content-Type: text/html; charset=UTF-8");
+
+print "<html>\n";
+print "<head>\n";
+print "<title>The Public Whip - Forty-Two Days</title>\n";
+print "<link href=\"publicwhip.css\" type=\"text/css\" rel=\"stylesheet\"/>\n";
+print "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
+
+print "<style type=\"text/css\">\n";
+print "
+body { width:800px; margin-left:auto; margin-right:auto; }
+input { color: #303070 }
+div#Zopform {  border: thin black solid; padding: 10px }
+div.quessec { border: thin black solid; padding: 10px; margin-top:10px; }
+div.oprad { margin-top:10px; padding:5px; background-color:#e0e0d0; font-size:120%;}
+div#checkmpbutton { font-size: 200%; text-align:center; margin:10px;}
+div#footer { background-color:black; color:white;  border: thin black solid; }
+table.votetab td { border: thin black solid; }
+div#yourfav { background-color:#fed9d8; }
+    ";
+
+print "</style>\n";
+
+print "</head>\n";
+print "<body class=\"fortytwodays\">\n";
+#print "<script type=\"text/javascript\" src=\"quiz/election2008.js\"></script>\n";
+
+print "<h1>The Public Whip - Forty-two days</h1>\n";
+#print "<h4 id=\"th4\">The candidate calculator for <i>$constituency</i></h4>\n";
+#print "<p></p>\n\n";
+
+if (is_postcode($_GET["mppc"]) or $_GET["mpc"])
+    $mpval = get_mpid_attr_decode($db, $db2, "");
+$vterdet = db_scrub($_GET["vterdet"]);
+$vpubem = db_scrub($_GET["vpubem"]);
+$vrand = db_scrub($_GET["vrand"]);
+if ($vrand)
+    $vrand = (int)$vrand;
+else
+    $vrand = rand(10, 10000000);
+
+$vdash = mysql_escape_string(db_scrub($_GET["dash"])); # used to tell if /by-election or /byelection was used
+
+$referrer = $_SERVER["HTTP_REFERER"];
+$querystring = $_SERVER["QUERY_STRING"];
+$ipnumber = $_SERVER["REMOTE_ADDR"];
+if (!$referrer)
+    $referrer = $_SERVER["HTTP_USER_AGENT"];
+
+if ($mpval)
+{
+    $mpprop = $mpval["mpprop"];
+    $party = $mpprop["party"];
+    $name = $mpprop["name"];
+    $constituency = $mpprop["constituency"];
+    $minentered_house = $mpval["minentered_house"];
+
+    $vnvotes = array(
+        "part4_terroristcertification" => array("2001-11-21", 75, "Power of Home Secretary to certify individuals as terrorists"),
+        "part4_indefinitedetention" => array("2001-11-21", 76, "Power to detain certified terrorists indefinitely if foreign"),
+        "public_emergency" => array("2001-11-21", 70, "Declaration of public emergency threatening the life of the nation"),
+        "part4_siac" => array("2001-11-21", 77),
+        "part4_privy" => array("2004-02-25", 59),
+        "part4_renewal" => array("2004-03-03", 71, "Renewal of law for detaining certified international terrorists indefinitely"),
+        "d90days" => array("2005-11-09", 84, "Extention of period of detention without charge to 90 days"),
+        "d28days" => array("2005-11-09", 85, "Extention of period of detention without charge to 28 days"),
+        "d42days_enable" => = array("2008-06-11", 219, "Conditions to enable detention without charge up to 42 days"),
+        "d42days_procedure" => array("2008-06-11", 220, "Power to detain suspects without charge for up to 42 days"),
+                    )
+
+    # book the values into a database
+    $db->query("drop table if exists pw_dyn_fortytwoday_comments");
+    $db->query("create table pw_dyn_fortytwoday_comments (vterdet varchar(10), vpubem varchar(10), ltime timestamp, vrand int, vpostcode varchar(20), constituency varchar(80))");
+    $db->query("INSERT INTO pw_dyn_fortytwoday_comments (vterdet, vpubem, ltime, vrand, vpostcode, constituency)
+                VALUES ('$vterdet', '$vpubem', NOW(), $vrand, '$vpostcode', '".mysql_escape_string($constituency)."')");
+
+    # looks up as a batch.  Or we could look up individually for easier coding
+    $votes = GetVotes($db, $mpprop["person"], 1039);
+    foreach ($vnvotes as $vname => &$vnvote)
+        $vnvote[] = GetVote($votes, $vnvote[0], $vnvote[1]);
+
+    print "<p><b>$name</b>,
+           represents the constituency of <b>$constituency</b> and
+           entered Parliament\n";
+    if ($minentered_house == "1997-05-01")
+        print " on or before the May 1997 General Election,\n";
+    else
+        print " on ".pretty_date($minentered_house).",\n";
+    print " which means they were able to vote on the detention of terrorist suspects without charge ";
+    print " for up to 42 days";
+    if ($d90days <> "notmp")
+        print " and to 90 days.";
+    else
+        print " but not to 90 days.";
+    print "</p>\n";
+
+    print "<p>\n";
+    if ($minentered_house <= "2001-11-21")
+        print " $name was also in Parliament for the votes which established detention without charge or trial of
+                foreign nationals whom the Home Secretary believed were terrorists.";
+    else if ($minentered_house <= "2004-03-03")
+        print " $name was not in Parliament for the vote which established detention without charge of
+                foreign nationals whom the Home Secretary believed were terrorists, but was there
+                when MPs voted to renew these powers were renewed.";
+    else
+        print " $name was not in Parliament at the time when MPs established detention without charge of
+                foreign nationals whom the Home Secretary believed were terrorists.";
+    print "</p>\n";
+
+    print "<p>The votes on these issues were as follows:</p>\n";
+    print "<table class=\"votetab\">\n";
+    WriteMPvotetable($vnvotes, $vterdet, $vpubem, $mpprop["mpanchor"]);
+    print "</table>\n";
+
+    print "<div id=\"yourfav\">\n";
+    WriteYourSummary($vterdet);
+    print "</div>\n";
+
+    print "<div id=\"lawsummary\">\n";
+    WriteTimeline();
+    print "</div>\n";
+
+    print "<div id=\"pubemchart\">\n";
+    print "<h2>Responses to public emergency question</h2>";
+    WritePubemChart($vpubem);
+    print "</div>\n";
 }
 
 
 else
 {
+    if (!isrobot() and !preg_match("/.*?house=z/", $querystring))
+    {
+        $db->query("INSERT INTO pw_logincoming
+                (referrer, ltime, ipnumber, page, subject, url, thing_id)
+                VALUES ('$referrer', NOW(), '$ipnumber', 'fortytwodays', '', '$vdash', $vrand)");
+    }
+
     print "<p>Fill in this form to find out if your MP
            agrees with you, according to their votes in Parliament.
            </p>";
@@ -283,7 +359,10 @@ else
     print "</div>\n";
     print "</div>\n";
 
+    print "<div style=\"display:block\">Random number: <input type=\"text\" name=\"vrand\" id=\"vrand\" value=\"$vrand\" readonly></div>\n";
+
     print "<div id=\"checkmpbutton\"><input type=\"submit\" value=\"Check my MP\"></div>\n";
+
     print "</form>\n";
 
     print "<div id=\"footer\">Produced by the Public Whip</div>\n";
