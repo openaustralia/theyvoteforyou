a886d8d2c7e31332b7a0eae750ce8459feb80045
publicwhip <publicwhip>
2008-11-17 06:30:38 +0000

    I think we use this.

--- loader/create.sql
+++ loader/create.sql
@@ -1,4 +1,4 @@
--- $Id: create.sql,v 1.50 2008/07/23 10:54:50 frabcus Exp $
+-- $Id: create.sql,v 1.51 2008/11/17 06:30:38 publicwhip Exp $
 -- SQL script to create the empty database tables for publicwhip.
 --
 -- The Public Whip, Copyright (C) 2003 Francis Irving and Julian Todd
@@ -358,6 +358,18 @@ create table pw_cache_divwiki (
     house enum('commons', 'lords') not null,
     wiki_id int not null,
     unique(division_date, division_number, house)
+);
+
+create table pw_logincoming (
+    referrer varchar(120),
+    ltime timestamp,
+    ipnumber varchar(20),
+    page varchar(20),
+    subject varchar(60),
+    url varchar(120),
+    thing_id int,
+    index(ltime),
+    index(thing_id)
 );
 
 -------------------------------------------------------------------------------
