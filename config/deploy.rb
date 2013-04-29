8ae2befab4a5516369e137585136eba54fc12419
Matthew Landauer <matthew@openaustralia.org>
2013-04-29 15:11:59 +1000

    Beginnings of bare bones cap deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "publicwhip-test.openaustraliafoundation.org.au"
+set :scm, :git
+set :repository,  "https://github.com/openaustralia/publicwhip.git"
+set :branch, :test
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+server "kedumba.openaustraliafoundation.org.au", :app, :web, :db, :primary => true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :use_sudo, false
+set :user, "deploy"
+set :deploy_to, "/srv/www/publicwhip-test.openaustraliafoundation.org.au"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
