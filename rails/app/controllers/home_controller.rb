91c255ccdb0968096351a18d3dd04066979a8d52
Matthew Landauer <matthew@openaustralia.org>
2013-07-26 12:10:03 +1000

    Make completely static copy of home page with all the assets copied over

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index
+    render layout: false
+  end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,8 +2,7 @@ Publicwhip::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
