a4b123e2aeb4a364ac1df1f5ea43a2b28e6981b8
Matthew Landauer <matthew@openaustralia.org>
2013-07-29 12:14:48 +1000

    Extract helper method

--- rails/app/helpers/members_helper.rb
+++ rails/app/helpers/members_helper.rb
@@ -1,2 +1,9 @@
 module MembersHelper
+  def sort_link(sort, sort_name, name, current_sort)
+    if current_sort == sort
+      content_tag(:b, name)
+    else
+      link_to name, "mps.php?sort=#{sort}", alt: "Sort by #{sort_name}"
+    end
+  end
 end
