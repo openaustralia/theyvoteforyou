9808ebf99ab3bbe87d045e3b9535fbbf987032db
Matthew Landauer <matthew@openaustralia.org>
2013-07-29 18:30:26 +1000

    Should be sorting in the correct order now

--- rails/app/controllers/divisions_controller.rb
+++ rails/app/controllers/divisions_controller.rb
@@ -3,6 +3,18 @@ class DivisionsController < ApplicationController
     @sort = params[:sort]
     @title = "Divisions &#8212; 2010 (current) &#8212; The Public Whip".html_safe
 
-    @divisions = Division.order("division_date DESC", "clock_time DESC").all
+    order = case @sort
+    when nil
+      ["division_date DESC", "clock_time DESC", "division_name", "division_number DESC"]
+    when "subject"
+      ["division_name", "division_date DESC", "clock_time DESC", "division_number DESC"]
+    when "rebellions"
+      ["rebellions DESC", "division_date DESC", "clock_time DESC", "division_name", "division_number DESC"]
+    when "turnout"
+      ["turnout DESC", "division_date DESC", "clock_time DESC", "division_name", "division_number DESC"]
+    else
+      raise "Unexpected value"
+    end
+    @divisions = Division.joins(:division_info).order(order).all
   end
 end
