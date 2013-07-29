1601b8abfd49ea71a4da7bf11348e7eb6be4087a
Matthew Landauer <matthew@openaustralia.org>
2013-07-29 18:13:51 +1000

    Make sort links do the right thing

--- rails/app/controllers/divisions_controller.rb
+++ rails/app/controllers/divisions_controller.rb
@@ -1,5 +1,6 @@
 class DivisionsController < ApplicationController
   def index
+    @sort = params[:sort]
     @title = "Divisions &#8212; 2010 (current) &#8212; The Public Whip".html_safe
 
     @divisions = Division.order("division_date DESC", "clock_time DESC").all
--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
   def electorate_path(member)
     "mp.php?mpc=#{member.electorate}"
   end
+
+  def sort_link(sort, sort_name, name, current_sort)
+    if current_sort == sort
+      content_tag(:b, name)
+    else
+      link_to name, params.merge(sort: sort), alt: "Sort by #{sort_name}"
+    end
+  end
 end
