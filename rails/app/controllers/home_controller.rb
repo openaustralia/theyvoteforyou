0effd6f9eb8f3c265e71b6930761d8aa64306b98
Matthew Landauer <matthew@openaustralia.org>
2013-08-04 08:13:39 +1000

    Use layout

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
+# encoding: UTF-8
+
 class HomeController < ApplicationController
   def index
     @title = "The Public Whip &#8212; ".html_safe
   end
 
   def faq
-    render layout: false
+    @title = "Help — Frequently Asked Questions — The Public Whip"
   end
 end
