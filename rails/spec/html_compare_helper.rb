4d0a9f9881489138c97d002ee97427fff19108be
Matthew Landauer <matthew@openaustralia.org>
2013-08-04 16:01:44 +1000

    Extract comparing code into helper

--- rails/spec/html_compare_helper.rb
+++ rails/spec/html_compare_helper.rb
@@ -1,47 +1,44 @@
 # Originally from https://raw.github.com/openaustralia/planningalerts-app/d5b1ead73f220b7e56ef402bb833f51b2e5144d3/spec/html_compare_helper.rb
 
 require 'open-uri'
-require 'hpricot'
+require 'net/http'
 
 module HTMLCompareHelper
+  def compare(path)
+    get path
+    text = Net::HTTP.get('localhost', path)
+    text.force_encoding(Encoding::UTF_8)
+    compare_html(text, response.body)
+  end
+
+  private
+
+  def compare_html(old_html, new_html)
+    n = normalise_html(new_html)
+    o = normalise_html(old_html)
+
+    if n != o
+      # Write it out to a file
+      File.open("old.html", "w") {|f| f.write(o.to_s)}
+      File.open("new.html", "w") {|f| f.write(n.to_s)}
+      raise "Don't match. Writing to file old.html and new.html"
+    end
+  end
+
+  # Convert into a form where html can be reliably diff'd
+  def normalise_html(text)
+    tidy(text)
+  end
+
   def tidy(text)
     File.open("temp.html", "w") {|f| f.write(text) }
     # Requires HTML Tidy (http://tidy.sourceforge.net/) version 14 June 2007 or later
-    system("/usr/local/bin/tidy --sort-attributes alpha -q -m temp.html")
+    # Can install on OS X with "brew install tidy"
+    # Note the version installed with OS X by default is a version that's too old
+    system("/usr/local/bin/tidy --sort-attributes alpha -utf8 -q -m temp.html")
     r = File.read("temp.html")
     # Make sure that comments of the form <!-- comment --> are followed by a new line
     File.delete("temp.html")
     r.gsub("--><", "-->\n<")
-  end
-  
-  def compare_with_php(url, name, use_cache_file = false)
-    cached_filename = File.dirname(__FILE__) + "/../regression_data/#{name}.html"
-    if use_cache_file && File.exists?(cached_filename)
-      expected = File.read(cached_filename)
-    else
-      # Setting User-Agent so that the php code outputs the default stylesheets
-      expected = Hpricot(open("http://localhost#{url}", "User-Agent" => "Ruby/#{RUBY_VERSION}")).to_html
-    end
-    if use_cache_file && !File.exists?(cached_filename)
-      File.open(cached_filename, "w") {|f| f.write(expected)}
-    end
-    
-    get url
-    result = Hpricot(@response.body).to_html
-
-    expected_tidy = tidy(expected)
-    result_tidy = tidy(result)
-    if result_tidy == expected_tidy
-      ["expected_#{name}.html", "result_#{name}.html", "expected_#{name}_tidy.html", "result_#{name}_tidy.html"].each do |f|
-        File.delete(f) if File.exists?(f)
-      end
-    else
-      # If failed then write out result for easy comparison
-      File.open("expected_#{name}.html", "w") {|f| f.write(expected)}
-      File.open("result_#{name}.html", "w") {|f| f.write(result)}
-      File.open("expected_#{name}_tidy.html", "w") {|f| f.write(expected_tidy)}
-      File.open("result_#{name}_tidy.html", "w") {|f| f.write(result_tidy)}
-    end
-    result_tidy.should == expected_tidy
   end
 end
--- rails/spec/requests/compare_spec.rb
+++ rails/spec/requests/compare_spec.rb
@@ -1,43 +1,8 @@
 require 'spec_helper'
-require 'net/http'
 # Compare results of rendering pages via rails and via the old php app
 
-def tidy(text)
-  File.open("temp.html", "w") {|f| f.write(text) }
-  # Requires HTML Tidy (http://tidy.sourceforge.net/) version 14 June 2007 or later
-  # Can install on OS X with "brew install tidy"
-  # Note the version installed with OS X by default is a version that's too old
-  system("/usr/local/bin/tidy --sort-attributes alpha -utf8 -q -m temp.html")
-  r = File.read("temp.html")
-  # Make sure that comments of the form <!-- comment --> are followed by a new line
-  File.delete("temp.html")
-  r.gsub("--><", "-->\n<")
-end
-
-# Convert into a form where html can be reliably diff'd
-def normalise_html(text)
-  tidy(text)
-end
-
-def compare_html(old_html, new_html)
-  n = normalise_html(new_html)
-  o = normalise_html(old_html)
-
-  if n != o
-    # Write it out to a file
-    File.open("old.html", "w") {|f| f.write(o.to_s)}
-    File.open("new.html", "w") {|f| f.write(n.to_s)}
-    raise "Don't match. Writing to file old.html and new.html"
-  end
-end
-
 describe "Comparing" do
-  def compare(path)
-    get path
-    text = Net::HTTP.get('localhost', path)
-    text.force_encoding(Encoding::UTF_8)
-    compare_html(text, response.body)
-  end
+  include HTMLCompareHelper
 
   before :each do
     m = Member.create!(first_name: "Tony", last_name: "Abbott", party: "Liberal Party",
