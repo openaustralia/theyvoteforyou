90ff643852190e997dbc81ebe678a1c4985ce3a4
Matthew Landauer <matthew@openaustralia.org>
2013-08-07 14:15:11 +1000

    Extract helper

--- rails/app/helpers/members_helper.rb
+++ rails/app/helpers/members_helper.rb
@@ -6,4 +6,14 @@ module MembersHelper
       link_to name, params.merge(sort: sort), alt: "Sort by #{sort_name}"
     end
   end
+
+  def display_link2(member, display, name, title, current_display)
+    if current_display == display
+      content_tag(:li, name, class: "on")
+    else
+      content_tag(:li, class: "off") do
+        link_to name, member_path(member, display: display), title: title, class: "off"
+      end
+    end
+  end
 end
