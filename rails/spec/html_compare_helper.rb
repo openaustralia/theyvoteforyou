30c4b762aa7a50c384c273a55d86100e76f56caa
Matthew Landauer <matthew@openaustralia.org>
2013-08-11 11:11:34 +1000

    Do more noddy fallback between tidy and diff versions on different platforms

--- rails/spec/html_compare_helper.rb
+++ rails/spec/html_compare_helper.rb
@@ -6,7 +6,7 @@ require 'net/http'
 module HTMLCompareHelper
   def compare(path)
     get path
-    text = Net::HTTP.get((ENV['PHP_SERVER'] || 'localhost'), path)
+    text = Net::HTTP.get(php_server, path)
     text.force_encoding(Encoding::UTF_8)
     compare_html(text, response.body, path)
   end
@@ -21,7 +21,7 @@ module HTMLCompareHelper
       # Write it out to a file
       output("old.html", o, path)
       output("new.html", n, path)
-      exec("diff old.html new.html")
+      exec("#{diff_path} old.html new.html")
       raise "Don't match. Writing to file old.html and new.html"
     end
   end
@@ -42,12 +42,35 @@ module HTMLCompareHelper
     File.open("temp.html", "w") {|f| f.write(text) }
     # Requires HTML Tidy (http://tidy.sourceforge.net/) version 14 June 2007 or later
     # Note the version installed with OS X by default is a version that's too old
-    # Install on OS X with "brew install tidy" and replace your system "tidy" with
-    # a symlink to the homebrew installed one
-    system("tidy --show-warnings no --sort-attributes alpha -utf8 -q -m temp.html")
+    # Install on OS X with "brew install tidy"
+    system("#{tidy_path} --show-warnings no --sort-attributes alpha -utf8 -q -m temp.html")
     r = File.read("temp.html")
     # Make sure that comments of the form <!-- comment --> are followed by a new line
     File.delete("temp.html")
     r.gsub("--><", "-->\n<")
+  end
+
+  def php_server
+    ENV['PHP_SERVER'] || 'localhost'
+  end
+
+  def tidy_path
+    # On OS X use tidy installed with Homebrew in preference to any other
+    # It would normally be first in the path but we can't depend on that being the case here
+    if File.exists? "/usr/local/bin/tidy"
+      "/usr/local/bin/tidy"
+    else
+      "tidy"
+    end
+  end
+
+  def diff_path
+    # On OS X use opendiff in preference to any other diff. Fallback to regular diff somewhere
+    # in the path so that this works on Linux as well.
+    if File.exists? "/usr/bin/opendiff"
+      "/usr/bin/opendiff"
+    else
+      "diff"
+    end
   end
 end
