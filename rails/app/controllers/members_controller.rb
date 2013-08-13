b0858df73cc0f5fbb47767b5ebc34261da326acb
Matthew Landauer <matthew@openaustralia.org>
2013-08-13 11:38:20 +1000

    Rename variable

--- rails/app/controllers/members_controller.rb
+++ rails/app/controllers/members_controller.rb
@@ -4,8 +4,8 @@ class MembersController < ApplicationController
     # By default sort by last name
     @sort = "lastname" if @sort.nil?
 
-    @australian_house = params[:house]
-    @australian_house = "representatives" if @australian_house.nil?
+    @house = params[:house]
+    @house = "representatives" if @house.nil?
 
     order = case @sort
     when "lastname"
@@ -22,22 +22,22 @@ class MembersController < ApplicationController
       raise "Unexpected value"
     end
 
-    @members = Member.current.in_australian_house(@australian_house).joins(:member_info).select("*, votes_attended/votes_possible as attendance_fraction, rebellions/votes_attended as rebellions_fraction").order(order)
+    @members = Member.current.in_australian_house(@house).joins(:member_info).select("*, votes_attended/votes_possible as attendance_fraction, rebellions/votes_attended as rebellions_fraction").order(order)
   end
 
   def show
     @first_name = params[:mpn].split("_")[0]
     @last_name = params[:mpn].split("_")[1]
     @electorate = params[:mpc]
-    @australian_house = params[:house]
+    @house = params[:house]
     @display = params[:display]
 
     # TODO In reality there could be several members matching this and we should relate this back to being
     # a single person
     if @electorate == "Senate"
-      @member = Member.in_australian_house(@australian_house).where(first_name: @first_name, last_name: @last_name).first
+      @member = Member.in_australian_house(@house).where(first_name: @first_name, last_name: @last_name).first
     else
-      @member = Member.in_australian_house(@australian_house).where(first_name: @first_name, last_name: @last_name, constituency: @electorate).first
+      @member = Member.in_australian_house(@house).where(first_name: @first_name, last_name: @last_name, constituency: @electorate).first
     end
 
     if @display == "allvotes"
