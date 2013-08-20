2c63296a00519fa6a80997d42fcc0e1b4f42eb4a
Henare Degan <henare.degan@gmail.com>
2013-08-20 14:57:35 +0300

    Change wording. It's how often they attend divisions, not parliament

--- website/index.php
+++ website/index.php
@@ -76,7 +76,7 @@ advertisement('homepage');
 <p><a href="mps.php?sort=rebellions" title="Show all MPs ordered by rebellions">Show all MPs ordered by rebellions</a></p>
 </div>
 
-<div class="col2"><h2>Representatives and Senators who attend parliament most often</h2>
+<div class="col2"><h2>Representatives and Senators who attend votes most often</h2>
 <p><a href="mps.php?sort=attendance&amp;house=all" title="Show all MPs ordered by attendance">Show all MPs ordered by attendance</a></p>
 </div>
 <div class="col3"><h2>Test a Representative or Senator against policies you care about</h2>
