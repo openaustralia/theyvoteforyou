b062bedfffab53aa679bc9913c1378290b96984d
Henare Degan <henare.degan@gmail.com>
2013-10-22 15:12:35 +0200

    Add failing test for second policy

--- rails/spec/requests/policies_controller_spec.rb
+++ rails/spec/requests/policies_controller_spec.rb
@@ -13,5 +13,9 @@ describe PoliciesController do
     compare("/policy.php?id=1")
     # compare("/policy.php?id=1&display=motions")
     # compare("/policy.php?id=1&display=editdefinition")
+
+    compare("/policy.php?id=2")
+    # compare("/policy.php?id=2&display=motions")
+    # compare("/policy.php?id=2&display=editdefinition")
   end
 end
