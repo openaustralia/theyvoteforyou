066c178e65259a4e860e4287a3a6d30cbc7e884b
Henare Degan <henare.degan@gmail.com>
2013-12-31 19:25:28 -0300

    Add second level nav links to members page in Rails
    This was added back to the PHP version in 1dc02ac1954cd9b16bcf95100fa701f12cb2d538

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 module ApplicationHelper
-  def member_path(member, params = {})
-    if member.senator?
-      # TODO Seems odd to me the mpc=Senate would expect mpc=Tasmania
-      r = "mp.php?mpn=#{member.url_name}&mpc=Senate&house=#{member.australian_house}"
-    else
-      r = "mp.php?mpn=#{member.url_name}&mpc=#{member.electorate}&house=#{member.australian_house}"
-    end
-    r += "&parliament=#{params[:parliament]}" if params[:parliament]
-    r += "&display=#{params[:display]}" if params[:display]
-    r += "##{params[:anchor]}" if params[:anchor]
-    r
-  end
-
   def electorate_path(member)
     "mp.php?mpc=#{member.electorate}"
   end
--- rails/app/helpers/members_helper.rb
+++ rails/app/helpers/members_helper.rb
@@ -1,4 +1,27 @@
 module MembersHelper
+  def member_path(member, params = {})
+    if member.senator?
+      # TODO Seems odd to me the mpc=Senate would expect mpc=Tasmania
+      r = "mp.php?mpn=#{member.url_name}&mpc=Senate&house=#{member.australian_house}"
+    else
+      r = "mp.php?mpn=#{member.url_name}&mpc=#{member.electorate}&house=#{member.australian_house}"
+    end
+    r += "&parliament=#{params[:parliament]}" if params[:parliament]
+    r += "&display=#{params[:display]}" if params[:display]
+    r += "##{params[:anchor]}" if params[:anchor]
+    r
+  end
+
+  def members_path(params)
+    p = ""
+    p += "&parliament=#{params[:parliament]}" if params[:parliament]
+    p += "&house=#{params[:house]}" if params[:house] && params[:house] != "representatives"
+    p += "&sort=#{params[:sort]}"
+    r = "/mps.php"
+    r += "?" + p[1..-1] if p != ""
+    r
+  end
+
   def sort_link(sort, sort_name, name, current_sort)
     if current_sort == sort
       content_tag(:b, name)
