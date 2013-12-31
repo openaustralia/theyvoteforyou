e300ff5de3c6b24b9c6c0593a6298080c23df141
Henare Degan <henare.degan@gmail.com>
2013-12-31 18:27:53 -0300

    Add 44th Parliament to Rails
    Added to PHP version in b4aff0b20d327f0e9860fe9fed420bcdc0132ecd

--- rails/app/models/member.rb
+++ rails/app/models/member.rb
@@ -12,7 +12,8 @@ class Member < ActiveRecord::Base
   # List of parliaments (temporarily here)
   def self.parliaments
     {
-      "2010" => {from: Date.new(2010,9,28),  to: Date.new(9999,12,31), name: "2010 (current)"},
+      "2013" => {from: Date.new(2013,11,12), to: Date.new(9999,12,31), name: "2013 (current)"},
+      "2010" => {from: Date.new(2010,9,28),  to: Date.new(2013,8,05), name: "2010-2013"},
       "2007" => {from: Date.new(2008,2,12),  to: Date.new(2010,7,19),  name: "2008-2010"},
       "2004" => {from: Date.new(2004,11,16), to: Date.new(2007,10,17), name: "2004-2007"},
     }
