9241a0d8c89904594b17dfc32afabd50b1a03858
Henare Degan <henare.degan@gmail.com>
2014-02-20 15:09:25 +1100

    Use relative links instead of pointing to the test instance

--- website/faq.php
+++ website/faq.php
@@ -53,7 +53,7 @@ $db = new DB();
 
 <h2 class="faq"><a name="#how">How does the Public Whip work?</a></h2>
 <p>
-Debate transcripts of the House of Representatives and the Senate are published online as <a href="http://www.openaustralia.org/">Hansard</a>. Public Whip takes these records and compiles lists of votes that you can access under <a href="http://publicwhip-test.openaustraliafoundation.org.au/divisions.php">Divisions</a>. You can search through these votes using our <a href="http://publicwhip-test.openaustraliafoundation.org.au/index.php">search function</a> on the home page or you can browse the votes that are relevant to the particular policy areas listed under <a href="http://publicwhip-test.openaustraliafoundation.org.au/policies.php">Policies</a> (for more on our policies, see <a href="#policies">What are Policies and how do they work?</a>).
+Debate transcripts of the House of Representatives and the Senate are published online as <a href="http://www.openaustralia.org/">Hansard</a>. Public Whip takes these records and compiles lists of votes that you can access under <a href="/divisions.php">Divisions</a>. You can search through these votes using our <a href="/">search function</a> on the home page or you can browse the votes that are relevant to the particular policy areas listed under <a href="/policies.php">Policies</a> (for more on our policies, see <a href="#policies">What are Policies and how do they work?</a>).
 </p>
 
 <h2 class="faq"><a name="#timeperiod">What time period does it cover?</a></h2>
@@ -127,20 +127,20 @@ Unfortunately, most decisions in Parliament are not made by division (see <a hre
 <h2 class="faq"><a name="#summaries">Why don’t all the divisions have edited summaries?</a></h2>
 
 <p>
-When you click on a link for a <a href="http://publicwhip-test.openaustraliafoundation.org.au/divisions.php">division</a>, you will be taken to a summary that will either contain an edited description of the division or text taken automatically from the official record of Parliament (known as Hansard). In some cases, the summary will contain no text.
+When you click on a link for a <a href="/divisions.php">division</a>, you will be taken to a summary that will either contain an edited description of the division or text taken automatically from the official record of Parliament (known as Hansard). In some cases, the summary will contain no text.
 </p>
 <p>
-Currently, the divisions with edited summaries are those that are relevant to one of the <a href="http://publicwhip-test.openaustraliafoundation.org.au/policies.php">Policies</a>. See our <a href="publicwhip-test.openaustraliafoundation.org.au/project/research.php">Research</a> page to find out more about how the summaries are edited.
+Currently, the divisions with edited summaries are those that are relevant to one of the <a href="/policies.php">Policies</a>. See our <a href="/project/research.php">Research</a> page to find out more about how the summaries are edited.
 </p>
 
 
 <h2 class="faq"><a name="#policies">What are Policies and how do they work?</a></h2>
 
 <p>
-On Public Whip, the <a href="http://publicwhip-test.openaustraliafoundation.org.au/policies.php">Policies</a> are sets of votes on an issue.
+On Public Whip, the <a href="/policies.php">Policies</a> are sets of votes on an issue.
 </p>
 <p>
-We choose and develop particular Policies for a number of reasons. For example, we prioritise issues where politicians have rebelled (e.g. the <a href="http://publicwhip-test.openaustraliafoundation.org.au/policy.php?id=10">local government recognition divisions</a>) or where parties have allowed their members to take a free vote (e.g. the <a href="http://publicwhip-test.openaustraliafoundation.org.au/policy.php?id=1">same sex marriage divisions</a>) because these divisions give a strong indication of an individual politician’s voting patterns (see <a href="#rebelandfree">What are “Rebel Voters” and what is a “Free Vote”?</a>). Other reasons for selecting a particular Policy include whether the matter was an election issue (e.g. the <a href="http://publicwhip-test.openaustraliafoundation.org.au/policy.php?id=3">carbon price</a>) and whether there was a high level of attendance (see <a href="#clarify">What do the “attendance” and “rebellion” figures mean?</a>).
+We choose and develop particular Policies for a number of reasons. For example, we prioritise issues where politicians have rebelled (e.g. the <a href="/policy.php?id=10">local government recognition divisions</a>) or where parties have allowed their members to take a free vote (e.g. the <a href="/policy.php?id=1">same sex marriage divisions</a>) because these divisions give a strong indication of an individual politician’s voting patterns (see <a href="#rebelandfree">What are “Rebel Voters” and what is a “Free Vote”?</a>). Other reasons for selecting a particular Policy include whether the matter was an election issue (e.g. the <a href="/policy.php?id=3">carbon price</a>) and whether there was a high level of attendance (see <a href="#clarify">What do the “attendance” and “rebellion” figures mean?</a>).
 </p>
 <p>
 Unfortunately, Policies are restricted to issues that are voted on by division because those are the only decisions that appear on Public Whip (see <a href="#decisions">Why don’t all decisions made in Parliament appear on Public Whip?</a>)
