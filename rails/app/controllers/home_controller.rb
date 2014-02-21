75df380a21258246db11eaad20f47baf5eb9b66b
Henare Degan <henare.degan@gmail.com>
2014-02-21 13:52:59 +1100

    Redirect to an MP page when a postcode is entered #131
    By doing the really dumb thing

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class HomeController < ApplicationController
   def index
     @divisions = Division.with_rebellions.order("division_date DESC", "clock_time DESC", "division_name", "division_number DESC").limit(5)
@@ -7,5 +9,13 @@ class HomeController < ApplicationController
   end
 
   def search
+    @postcode = params[:query] if params[:query] =~ /^\d{4}$/
+
+    electorates = JSON.parse(open("http://www.openaustralia.org/api/getDivisions?output=js&key=CcV3KBBX2Em7GQeV3RA8qzgS&postcode=#{@postcode}").read)
+
+    if electorates.count == 1
+      # FIXME: We should redirect but this is how the PHP app does it currently
+      render nothing: true, location: view_context.electorate_path(Member.find_by_constituency(electorates.first['name']))
+    end
   end
 end
