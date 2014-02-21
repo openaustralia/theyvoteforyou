dc244fcf24980416138275a057b44d803690dd23
Henare Degan <henare.degan@gmail.com>
2014-02-21 11:53:03 +1100

    Add failing test for search page #131

--- rails/spec/requests/home_controller_spec.rb
+++ rails/spec/requests/home_controller_spec.rb
@@ -12,4 +12,20 @@ describe HomeController do
   it "#faq" do
     compare("/faq.php")
   end
+
+  it "#search" do
+    compare("/search.php")
+
+    compare("/search.php?query=2042&button=Search")
+    compare("/search.php?query=Tony+Abbott&button=Search")
+    compare("/search.php?query=supplementary+explanatory+memorandum&button=Search")
+    compare("/search.php?query=This+is+some+test+text&button=Search")
+    compare("/search.php?query=Wa-pa-pa-pa-pa-pow&button=Search")
+
+    compare("/search.php?query=2042&button=Submit")
+    compare("/search.php?query=Tony+Abbott&button=Submit")
+    compare("/search.php?query=supplementary+explanatory+memorandum&button=Submit")
+    compare("/search.php?query=This+is+some+test+text&button=Submit")
+    compare("/search.php?query=Wa-pa-pa-pa-pa-pow&button=Submit")
+  end
 end
