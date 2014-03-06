7f7acd2d5f5f3d8068b5198875c7d0762af0e488
Henare Degan <henare.degan@gmail.com>
2014-03-06 16:29:40 +1100

    Rework to make it clearer what this is doing

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -9,11 +9,10 @@ class HomeController < ApplicationController
   end
 
   def search
-    @postcode = params[:query] if params[:query] =~ /^\d{4}$/
-
-    electorates = JSON.parse(open("http://www.openaustralia.org/api/getDivisions?output=js&key=CcV3KBBX2Em7GQeV3RA8qzgS&postcode=#{@postcode}").read)
-
-    if electorates.count == 1
+    if params[:query] =~ /^\d{4}$/
+      @postcode = params[:query]
+      electorates = JSON.parse(open("http://www.openaustralia.org/api/getDivisions?output=js&key=CcV3KBBX2Em7GQeV3RA8qzgS&postcode=#{@postcode}").read)
+      raise unless electorates.count == 1
       # FIXME: We should redirect but this is how the PHP app does it currently
       render nothing: true, location: view_context.electorate_path(Member.find_by_constituency(electorates.first['name']))
     end
