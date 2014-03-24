d5d578a403b97e5bfc59ae26dbaedbe83ec42209
Henare Degan <henare.degan@gmail.com>
2014-03-24 12:42:53 +1100

    cap install

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,58 @@
-set :application, "publicwhip-test.openaustraliafoundation.org.au"
-set :scm, :git
-set :deploy_via, :remote_cache
-set :repository,  "https://github.com/openaustralia/publicwhip.git"
-set :branch, :test
+# config valid only for Capistrano 3.1
+lock '3.1.0'
 
-server "kedumba.openaustraliafoundation.org.au", :app, :web, :db, :primary => true
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
 
-set :use_sudo, false
-set :user, "deploy"
-set :deploy_to, "/srv/www/publicwhip-test.openaustraliafoundation.org.au"
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    # Do a graceful restart of Apache?
-    # For the time being doing nothing
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
 
-  task :finalize_update, :except => { :no_release => true } do
-    escaped_release = latest_release.to_s.shellescape
-    commands = []
+  after :publishing, :restart
 
-    commands << "ln -s -- #{shared_path}/Config.pm #{escaped_release}/loader/PublicWhip/Config.pm"
-    commands << "ln -s -- #{shared_path}/config.php #{escaped_release}/website/config.php"
-
-    run commands.join(' && ') if commands.any?
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
   end
 
 end
