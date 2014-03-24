4b8b211fa9f91900f94997b4dd787aeb5acc5788
Henare Degan <henare.degan@gmail.com>
2014-03-24 15:08:40 +1100

    Install Gems when deploying

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -8,3 +8,5 @@ set :deploy_to, "/srv/www/publicwhip-test.openaustraliafoundation.org.au"
 set :linked_files, %w{loader/PublicWhip/Config.pm website/config.php}
 
 set :rvm_ruby_version, '2.0.0'
+
+set :bundle_gemfile, -> { release_path.join('rails').join('Gemfile') }
