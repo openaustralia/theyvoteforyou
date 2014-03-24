7fc65225609bef71449badae7916bb175c14776b
Henare Degan <henare.degan@gmail.com>
2014-03-24 15:17:00 +1100

    Restart the Rails app when we deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,26 +33,3 @@ set :repo_url, 'https://github.com/openaustralia/publicwhip.git'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-# namespace :deploy do
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :publishing, :restart
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-# end
--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -10,3 +10,15 @@ set :linked_files, %w{loader/PublicWhip/Config.pm website/config.php}
 set :rvm_ruby_version, '2.0.0'
 
 set :bundle_gemfile, -> { release_path.join('rails').join('Gemfile') }
+
+namespace :deploy do
+  desc 'Restart Rails application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :mkdir, "-p", release_path.join('rails/tmp/')
+      execute :touch, release_path.join('rails/tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
