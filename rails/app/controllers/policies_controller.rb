85c39184948a3bb29c22af474d121a918d23d0eb
Henare Degan <henare.degan@gmail.com>
2014-04-15 15:20:46 +1000

    Sort policies by id. Fixes #145

--- rails/app/controllers/policies_controller.rb
+++ rails/app/controllers/policies_controller.rb
@@ -1,6 +1,6 @@
 class PoliciesController < ApplicationController
   def index
-    @policies = Policy.joins(:policy_info)
+    @policies = Policy.joins(:policy_info).order(dream_id: :desc)
   end
 
   def show
