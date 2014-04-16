cc8ca8dfe84ac4ab254447e9c3bf67ba51783762
Henare Degan <henare.degan@gmail.com>
2014-04-16 17:27:11 +1000

    Extract Parliaments into its own model

--- rails/app/models/member.rb
+++ rails/app/models/member.rb
@@ -9,14 +9,9 @@ class Member < ActiveRecord::Base
   # Divisions that have been attended
   has_many :divisions, through: :votes
 
-  # List of parliaments (temporarily here)
+  # TODO: Get rid of this now that it has it's own model
   def self.parliaments
-    {
-      "2013" => {from: Date.new(2013,11,12), to: Date.new(9999,12,31), name: "2013 (current)"},
-      "2010" => {from: Date.new(2010,9,28),  to: Date.new(2013,8,05), name: "2010-2013"},
-      "2007" => {from: Date.new(2008,2,12),  to: Date.new(2010,7,19),  name: "2008-2010"},
-      "2004" => {from: Date.new(2004,11,16), to: Date.new(2007,10,17), name: "2004-2007"},
-    }
+    Parliament.all
   end
 
   # All divisions that this member could have attended
