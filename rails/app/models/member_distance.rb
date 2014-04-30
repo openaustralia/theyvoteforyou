877da19b26eba0cf76cd2f8b54b8e9b580544189
Henare Degan <henare.degan@gmail.com>
2014-04-30 12:54:41 +1000

    Extract method

--- rails/app/models/member_distance.rb
+++ rails/app/models/member_distance.rb
@@ -2,6 +2,11 @@ class MemberDistance < ActiveRecord::Base
   self.table_name = "pw_cache_realreal_distance"
   belongs_to :member, foreign_key: :mp_id1
 
+  # TODO: Can we do this as an association?
+  def member2
+    Member.find mp_id2
+  end
+
   def agreement_percentage
     (1 - distance_a) * 100
   end
