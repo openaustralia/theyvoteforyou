0d94886e61d17910a6d38962b1cd54934b4cc1ca
Henare Degan <henare.degan@gmail.com>
2014-04-30 21:11:58 +1000

    Show a message if the log in fails

--- rails/app/controllers/account_controller.rb
+++ rails/app/controllers/account_controller.rb
@@ -3,5 +3,12 @@ class AccountController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def settings
+    if params[:submit] == 'Login to Public Whip'
+      if (user = User.find_by_user_name params[:username]) && user.password == Digest::MD5.hexdigest(params[:password])
+        # log in and render settings page
+      else
+        @login_failed = true
+      end
+    end
   end
 end
