2bc35a613631b9c9b95500d6e52f86c7ec95e8ed
Henare Degan <henare.degan@gmail.com>
2014-05-01 19:01:40 +1000

    Remove superfluous code

--- rails/app/controllers/account_controller.rb
+++ rails/app/controllers/account_controller.rb
@@ -3,7 +3,6 @@ class AccountController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def settings
-    redirect_to params[:r] if @current_user && params[:r]
     if params[:submit] == 'Login to Public Whip'
       if !authenticate_user(params[:user_name], params[:password])
         @login_failed = true
