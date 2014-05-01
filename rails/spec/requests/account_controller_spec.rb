1100f87cd8979366e282897a2031b70b6fdafe95
Henare Degan <henare.degan@gmail.com>
2014-05-01 18:09:05 +1000

    Revert "Test with redirects"
    This reverts commit 747dd369cc98045e30feab5f50d785c4c2f5764e.
    
    It's more trouble than it's worth to test redirects. All the compare
    stuff needs to be updated to follow the redirects and it's just too
    fiddly so I'm reverting this.

--- rails/spec/requests/account_controller_spec.rb
+++ rails/spec/requests/account_controller_spec.rb
@@ -8,23 +8,11 @@ describe AccountController do
     compare('/account/settings.php')
   end
 
-  context 'without redirects' do
-    it 'logs in with valid credentials' do
-      compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'henare', password: 'password'
-    end
-
-    it "doesn't log in with invalid credentials" do
-      compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'new_user', password: 'letmein'
-    end
+  it 'logs in with valid credentials' do
+    compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'henare', password: 'password'
   end
 
-  context 'with redirects' do
-    it 'logs in with valid credentials' do
-      compare_post '/account/settings.php?r=/', submit: 'Login to Public Whip', user_name: 'henare', password: 'password'
-    end
-
-    it "doesn't log in with invalid credentials" do
-      compare_post '/account/settings.php?r=/', submit: 'Login to Public Whip', user_name: 'new_user', password: 'letmein'
-    end
+  it "doesn't log in with invalid credentials" do
+    compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'new_user', password: 'letmein'
   end
 end
