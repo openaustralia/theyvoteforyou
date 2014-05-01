747dd369cc98045e30feab5f50d785c4c2f5764e
Henare Degan <henare.degan@gmail.com>
2014-05-01 16:59:29 +1000

    Test with redirects

--- rails/spec/requests/account_controller_spec.rb
+++ rails/spec/requests/account_controller_spec.rb
@@ -8,11 +8,23 @@ describe AccountController do
     compare('/account/settings.php')
   end
 
-  it 'logs in with valid credentials' do
-    compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'henare', password: 'password'
+  context 'without redirects' do
+    it 'logs in with valid credentials' do
+      compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'henare', password: 'password'
+    end
+
+    it "doesn't log in with invalid credentials" do
+      compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'new_user', password: 'letmein'
+    end
   end
 
-  it "doesn't log in with invalid credentials" do
-    compare_post '/account/settings.php', submit: 'Login to Public Whip', user_name: 'new_user', password: 'letmein'
+  context 'with redirects' do
+    it 'logs in with valid credentials' do
+      compare_post '/account/settings.php?r=/', submit: 'Login to Public Whip', user_name: 'henare', password: 'password'
+    end
+
+    it "doesn't log in with invalid credentials" do
+      compare_post '/account/settings.php?r=/', submit: 'Login to Public Whip', user_name: 'new_user', password: 'letmein'
+    end
   end
 end
