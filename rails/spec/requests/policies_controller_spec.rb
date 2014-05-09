cf6e95ceb8cc879e260a7a483cfa307621b32ddf
Henare Degan <henare.degan@gmail.com>
2014-05-09 17:13:14 +1000

    Add static policy edit page

--- rails/spec/requests/policies_controller_spec.rb
+++ rails/spec/requests/policies_controller_spec.rb
@@ -13,6 +13,7 @@ describe PoliciesController do
     it { compare("/policy.php?id=1") }
     # compare("/policy.php?id=1&display=motions")
     # compare("/policy.php?id=1&display=editdefinition")
+    it { compare("/policy.php?id=1&display=editdefinition", true) }
 
     it { compare("/policy.php?id=2") }
     # compare("/policy.php?id=2&display=motions")
