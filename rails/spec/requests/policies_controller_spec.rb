e9ccb5d401e47c2a90bf212020e5f8115f71038d
Henare Degan <henare.degan@gmail.com>
2014-05-09 17:11:40 +1000

    Split examples into separate tests

--- rails/spec/requests/policies_controller_spec.rb
+++ rails/spec/requests/policies_controller_spec.rb
@@ -9,12 +9,12 @@ describe PoliciesController do
     compare("/policies.php")
   end
 
-  it "#show" do
-    compare("/policy.php?id=1")
+  describe "#show" do
+    it { compare("/policy.php?id=1") }
     # compare("/policy.php?id=1&display=motions")
     # compare("/policy.php?id=1&display=editdefinition")
 
-    compare("/policy.php?id=2")
+    it { compare("/policy.php?id=2") }
     # compare("/policy.php?id=2&display=motions")
     # compare("/policy.php?id=2&display=editdefinition")
   end
