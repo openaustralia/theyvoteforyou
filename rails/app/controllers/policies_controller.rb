d542c5066d059ddeb721391d8790c496776f5eec
Henare Degan <henare.degan@gmail.com>
2014-05-20 18:18:16 +1000

    Extract signed in check into a before filter

--- rails/app/controllers/policies_controller.rb
+++ rails/app/controllers/policies_controller.rb
@@ -2,6 +2,8 @@ class PoliciesController < ApplicationController
   # TODO: Reenable CSRF protection
   skip_before_action :verify_authenticity_token
 
+  before_filter :check_user_signed_in, only: [:new, :create]
+
   def index
     @policies = Policy.joins(:policy_info).order(:private, :name)
   end
@@ -10,6 +12,7 @@ class PoliciesController < ApplicationController
     @policy = Policy.find(params[:id])
     @display = params[:display]
 
+    # TODO: Extract into check_user_signed_in method
     if @display == 'editdefinition' && !user_signed_in?
       redirect_to controller: 'account',
                   action: 'settings',
@@ -18,13 +21,16 @@ class PoliciesController < ApplicationController
   end
 
   def new
-    redirect_to action: 'settings', params: { r: '/account/addpolicy.php' } unless user_signed_in?
   end
 
   def create
-    redirect_to action: 'settings', params: { r: '/account/addpolicy.php' } unless user_signed_in?
-
     @policy = Policy.new name: params[:name], description: params[:description], user: current_user, private: 2
     render 'new' unless @policy.save!
+  end
+
+  private
+
+  def check_user_signed_in
+    redirect_to controller: 'account', action: 'settings', params: { r: '/account/addpolicy.php' } unless user_signed_in?
   end
 end
