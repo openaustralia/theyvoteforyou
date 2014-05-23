35569d4f3cb51736d4ba72f723861767f096a6f0
Henare Degan <henare.degan@gmail.com>
2014-05-23 18:41:41 +1000

    ERMAHGERD MOAR DASH SWAPPING BULLSHIT

--- rails/app/models/division.rb
+++ rails/app/models/division.rb
@@ -143,12 +143,12 @@ class Division < ActiveRecord::Base
   end
 
   def motion
-    text = wiki_motion ? wiki_motion.text_body[/--- MOTION EFFECT ---(.*)--- COMMENT/m, 1].strip : read_attribute(:motion)
-    # For some reason some characters are stored in the database using html entities
-    # rather than using unicode.
-    text = HTMLEntities.new.decode(text)
-    # FIXME This is just to match the PHP app. Why the hell is it the opposite to the name??
-    text.gsub('—', '-')
+    if wiki_motion
+      # FIXME The PHP app bizarrely swaps emdash for endash here and only here. Huh?
+      HTMLEntities.new.decode(wiki_motion.text_body[/--- MOTION EFFECT ---(.*)--- COMMENT/m, 1].strip).gsub('—', '-')
+    else
+      HTMLEntities.new.decode(read_attribute(:motion))
+    end
   end
 
   def motion_edited?
