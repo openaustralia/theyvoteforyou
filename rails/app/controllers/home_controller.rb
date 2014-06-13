9f599a62b5a3cf366ddf3424fe6c618d9efa8d0e
Daniel Kinsman <danielkinsman@riseup.net>
2014-06-13 12:57:10 +1000

    Handle invalid postcodes. Fixes openaustralia/publicwhip#134.

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -11,7 +11,16 @@ class HomeController < ApplicationController
   def search
     if params[:query] =~ /^\d{4}$/
       @postcode = params[:query]
-      electorates = JSON.parse(open("http://www.openaustralia.org/api/getDivisions?output=js&key=CcV3KBBX2Em7GQeV3RA8qzgS&postcode=#{@postcode}").read)
+
+      # Temporary work around for https://github.com/openaustralia/openaustralia/issues/502
+      json_response = open("http://www.openaustralia.org/api/getDivisions?output=js&key=CcV3KBBX2Em7GQeV3RA8qzgS&postcode=#{@postcode}").read
+      json_response = "{\"error\":\"Unknown postcode\"}" if json_response == "{\"error\":\"Unknown postcode\"}{}"
+      electorates = JSON.parse(json_response)
+
+      if electorates.respond_to?("has_key?") && electorates.has_key?("error")
+        @postcode_error = electorates["error"]
+        return
+      end
 
       if electorates.count == 1 && member = Member.find_by_constituency(electorates.first['name'])
         # FIXME: We should redirect but this is how the PHP app does it currently
