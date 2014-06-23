2042cd458a621a8adbe47d699cabebc4a2bfd140
Henare Degan <henare.degan@gmail.com>
2014-06-23 11:59:26 +1000

    Handle situation where no active policy is set (new user account)

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -14,6 +14,6 @@ class User < ActiveRecord::Base
 
   # FIXME This should be an association
   def active_policy
-    Policy.find active_policy_id
+    Policy.find active_policy_id if active_policy_id
   end
 end
