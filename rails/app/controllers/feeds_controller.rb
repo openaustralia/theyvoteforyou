355af583336b2b20171033cd1aa5eaaa44c279c5
Daniel Kinsman <danielkinsman@riseup.net>
2014-06-24 15:23:38 +1000

    Make rails mp-info xml feed match php one

--- rails/app/controllers/feeds_controller.rb
+++ rails/app/controllers/feeds_controller.rb
@@ -2,7 +2,7 @@ class FeedsController < ApplicationController
   def mp_info
     # FIXME: We should change the accepted value to senate instead of lords
     house =  params[:house] == 'lords' ? 'senate' : 'representatives'
-    @members = Member.in_australian_house(house).order(:entered_house, :last_name, :first_name, :constituency)
+    @members = Member.in_australian_house(house).joins(:member_info).order(:entered_house, :last_name, :first_name, :constituency)
     @most_recent_division = Division.most_recent_date
   end
 
