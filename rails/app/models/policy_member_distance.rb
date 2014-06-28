c90563e0bc193146b41cba3efb1840070ab772bc
Henare Degan <henare.degan@gmail.com>
2014-06-28 18:15:57 +1000

    Add an update method we can use without a primary key

--- rails/app/models/policy_member_distance.rb
+++ rails/app/models/policy_member_distance.rb
@@ -10,4 +10,10 @@ class PolicyMemberDistance < ActiveRecord::Base
     increment(attribute, by)
     PolicyMemberDistance.where(dream_id: policy.id, person: member.person).update_all(attribute => read_attribute(attribute))
   end
+
+  # Use update_all because we don't yet have a primary key on this model
+  # TODO: Add a primary key and get rid of this function
+  def update!(attributes)
+    PolicyMemberDistance.where(dream_id: policy.id, person: member.person).update_all(attributes)
+  end
 end
