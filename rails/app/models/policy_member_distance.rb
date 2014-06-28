26c239a5d554c73d0e125d8b5a54752fe1da8be6
Henare Degan <henare.degan@gmail.com>
2014-06-28 17:22:28 +1000

    Reimplement method because there's no primary key for ActiveRecord

--- rails/app/models/policy_member_distance.rb
+++ rails/app/models/policy_member_distance.rb
@@ -3,4 +3,11 @@ class PolicyMemberDistance < ActiveRecord::Base
 
   belongs_to :policy, foreign_key: :dream_id
   belongs_to :member, foreign_key: :person, primary_key: :person
+
+  # Use update_all because we don't yet have a primary key on this model
+  # TODO: Add a primary key and get rid of this function
+  def increment!(attribute, by = 1)
+    increment(attribute, by)
+    PolicyMemberDistance.where(dream_id: policy.id, person: member.person).update_all(attribute => read_attribute(attribute))
+  end
 end
