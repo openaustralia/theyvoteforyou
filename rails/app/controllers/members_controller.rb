2e98970b4787d95670922060209bac18aea6b890
Henare Degan <henare.degan@gmail.com>
2014-07-09 11:20:51 +1000

    Get the latest MP since it's the one we're likely interested in

--- rails/app/controllers/members_controller.rb
+++ rails/app/controllers/members_controller.rb
@@ -54,7 +54,7 @@ class MembersController < ApplicationController
     if electorate == "Senate" || electorate.nil?
       @member = Member.in_australian_house(@house).where(first_name: @first_name, last_name: @last_name).first
     elsif @first_name && @last_name
-      @member = Member.in_australian_house(@house).where(first_name: @first_name, last_name: @last_name, constituency: electorate).first
+      @member = Member.in_australian_house(@house).where(first_name: @first_name, last_name: @last_name, constituency: electorate).order(entered_house: :desc).first
     else
       # TODO This is definitely wrong. Should return multiple members in this electorate
       # TEMP HACK hardcoded date 1 Jan 2006 (start of Hansard data)
