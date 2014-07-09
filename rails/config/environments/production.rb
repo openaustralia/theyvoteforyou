6107f83349d5700ce38c2e3e7374c1e4f9577417
Henare Degan <henare.degan@gmail.com>
2014-07-09 16:57:57 +1000

    Set the default URL for ActionMailer so that Devise can send email

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -64,6 +64,7 @@ Publicwhip::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'publicwhip-rails.openaustraliafoundation.org.au' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
