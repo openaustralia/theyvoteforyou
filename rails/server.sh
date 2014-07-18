511ff49bd204e47e4e5054b098e7bb12409e5a80
Daniel Kinsman <danielkinsman@riseup.net>
2014-07-19 09:57:22 +1000

    Bash script to start webserver for manual testing

---
+++ rails/server.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Runs the rails server and delayed job processing.
+# The advantage to doing this in a script is that the jobs:work stuff will
+# automatically be stopped when the web server is.
+# This should only be used for running a test server during development of
+# course.
+
+JOB_LOG="$(mktemp --tmpdir publicwhip_jobs.XXXXXX)"
+bundle exec rake jobs:work > $JOB_LOG 2>&1 &
+echo "Processing delayed jobs in the background, log file at $JOB_LOG"
+bundle exec rails server
