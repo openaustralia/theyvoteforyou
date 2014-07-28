e62a4ba82f52750c76f5bca52150f3e0bcd179a1
Henare Degan <henare.degan@gmail.com>
2014-07-28 12:12:21 +1000

    Add a test for the first part of the footnote adding function

--- rails/spec/helpers/divisions_helper_spec.rb
+++ rails/spec/helpers/divisions_helper_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe DivisionsHelper do
   describe '#formatted_motion_text' do
-    subject { formatted_motion_text division }
-
-    let(:division) { mock_model(Division, motion: "A bill [No. 2] and votes") }
-    it { should eq("<p>A bill [No. 2] and votes</p>\n") }
+    it { formatted_motion_text(mock_model(Division, motion: "A bill [No. 2] and votes")).should eq("<p>A bill [No. 2] and votes</p>\n") }
+    it { formatted_motion_text(mock_model(Division, motion: "This remark[1] deserves a footnote")).should eq("<p>This remark<sup class=\"sup-1\"><a class=\"sup\" href='#footnote-1' onclick=\"ClickSup(1); return false;\">[1]</a></sup> deserves a footnote</p>\n") }
   end
 end
