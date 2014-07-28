1e4bb9457bb7947f75b16faf5aa3c91cb3242914
Henare Degan <henare.degan@gmail.com>
2014-07-28 10:32:30 +1000

    Enable test now this is working since we switched to Marker

--- rails/spec/helpers/divisions_helper_spec.rb
+++ rails/spec/helpers/divisions_helper_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe DivisionsHelper do
-  # TODO Enable this test
-  # describe '#formatted_motion_text' do
-  #   subject { formatted_motion_text division }
+  describe '#formatted_motion_text' do
+    subject { formatted_motion_text division }
 
-  #   let(:division) { mock_model(Division, motion: "A bill [No. 2] and votes") }
-  #   it { should eq("\n<p>A bill [No. 2] and votes</p>") }
-  # end
+    let(:division) { mock_model(Division, motion: "A bill [No. 2] and votes") }
+    it { should eq("<p>A bill [No. 2] and votes</p>\n") }
+  end
 end
