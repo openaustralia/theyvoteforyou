5bcf6f4a11ed59e43269a49c1a492df11f41cf50
Matthew Landauer <matthew@oaf.org.au>
2014-08-04 10:40:43 +1000

    Fix link hopefully

--- rails/app/helpers/divisions_helper.rb
+++ rails/app/helpers/divisions_helper.rb
@@ -153,7 +153,7 @@ module DivisionsHelper
     # We're using a different member for the link to try to make things the same as the php
     # TODO get rid of this silliness as soon as we can
     member2 = Member.where(person: @member.person, house: division.house).current_on(division.date).first
-    sentence = link_to member2.full_name, member_path(@member)
+    sentence = link_to member2.full_name, member_path(member2)
     sentence += " "
     if @member.vote_on_division_without_tell(@division) == "absent"
       sentence += "did not vote."
