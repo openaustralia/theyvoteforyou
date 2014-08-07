447d71e73b51599f1cbc05675daba4e4bcfe2138
Henare Degan <henare.degan@gmail.com>
2014-08-07 14:37:49 +1000

    Update deploy paths, remove unnecessary or duplicated tasks

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -5,41 +5,16 @@ role :db,  %w{deploy@kedumba.openaustraliafoundation.org.au}
 set :branch, :test
 set :deploy_to, "/srv/www/publicwhip-test.openaustraliafoundation.org.au"
 
-set :linked_files, %w{loader/PublicWhip/Config.pm website/config.php rails/config/database.yml rails/config/secrets.yml rails/config/newrelic.yml}
+set :linked_files, %w{php/loader/PublicWhip/Config.pm php/website/config.php config/database.yml config/secrets.yml config/newrelic.yml}
 
 set :rvm_ruby_version, '2.0.0'
-
-set :bundle_gemfile, -> { release_path.join('rails').join('Gemfile') }
-
-namespace :deploy do
-  desc 'Restart Rails application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :mkdir, "-p", release_path.join('rails/tmp/')
-      execute :touch, release_path.join('rails/tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  desc 'Run pending Rails migrations'
-  task :migrate do
-    on roles(:db) do
-      within current_path.join('rails') do
-        with rails_env: :production do
-          execute :rake, 'db:migrate'
-        end
-      end
-    end
-  end
-end
 
 namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export do
     on roles(:app) do
       within current_path.join('rails') do
-        execute :sudo, :foreman, :export, :upstart, "/etc/init -u deploy -a publicwhip -f Procfile.production -l #{shared_path}/log --root #{current_path.join('rails')}"
+        execute :sudo, :foreman, :export, :upstart, "/etc/init -u deploy -a publicwhip -f Procfile.production -l #{shared_path}/log --root #{current_path}"
       end
     end
   end
