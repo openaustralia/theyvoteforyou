92c8df6092e02272f578f797d0f6eea44ec8ef0b
Matthew Landauer <matthew@oaf.org.au>
2014-08-15 14:02:35 +1000

    Enumerate every combination

--- spec/models/member_distance_spec.rb
+++ spec/models/member_distance_spec.rb
@@ -23,12 +23,32 @@ describe MemberDistance, :type => :model do
         expect(MemberDistance.calculate_nvotessame(membera, memberb)).to eq nvotessame
       end
 
-      it { check_vote_combination("absent", "aye",     0) }
-      it { check_vote_combination("aye",    "aye",     1) }
-      it { check_vote_combination("aye",    "no",      0) }
-      it { check_vote_combination("aye",    "tellaye", 1) }
-      it { check_vote_combination("aye",    "tellno",  0) }
-      it { check_vote_combination("no",     "tellno",  1) }
+      it { check_vote_combination("absent",     "absent", 0) }
+      it { check_vote_combination("absent",     "aye",    0) }
+      it { check_vote_combination("absent",     "no",     0) }
+      it { check_vote_combination("absent",     "tellaye",0) }
+      it { check_vote_combination("absent",     "tellno", 0) }
+      it { check_vote_combination("aye",        "absent", 0) }
+      it { check_vote_combination("aye",        "aye",    1) }
+      it { check_vote_combination("aye",        "no",     0) }
+      it { check_vote_combination("aye",        "tellaye",1) }
+      it { check_vote_combination("aye",        "tellno", 0) }
+      it { check_vote_combination("no",         "absent", 0) }
+      it { check_vote_combination("no",         "aye",    0) }
+      it { check_vote_combination("no",         "no",     1) }
+      it { check_vote_combination("no",         "tellaye",0) }
+      it { check_vote_combination("no",         "tellno", 1) }
+      it { check_vote_combination("tellaye",    "absent", 0) }
+      it { check_vote_combination("tellaye",    "aye",    1) }
+      it { check_vote_combination("tellaye",    "no",     0) }
+      it { check_vote_combination("tellaye",    "tellaye",1) }
+      it { check_vote_combination("tellaye",    "tellno", 0) }
+      it { check_vote_combination("tellno",     "absent", 0) }
+      it { check_vote_combination("tellno",     "aye",    0) }
+      it { check_vote_combination("tellno",     "no",     1) }
+      it { check_vote_combination("tellno",     "tellaye",0) }
+      it { check_vote_combination("tellno",     "tellno", 1) }
+
     end
 
     context "with votes on five divisions" do
