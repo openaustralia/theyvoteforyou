ed88fe03a524295a500c0f0ee1253afbf12c8906
Matthew Landauer <matthew@oaf.org.au>
2014-08-15 16:29:31 +1000

    Rename variables

--- lib/distance.rb
+++ lib/distance.rb
@@ -1,14 +1,14 @@
 class Distance
   # TODO: Need to make this formula more clear
-  def self.calculate(nvotessame, nvotessamestrong, nvotesdiffer, nvotesdifferstrong,
-    nvotesabsent, nvotesabsentstrong)
+  def self.calculate(same, samestrong, differ, differstrong, absent, absentstrong)
     # absents have low weighting, except where it is a strong vote
     strong_weight = 5.0
     absent_weight = 0.2
-    weight = nvotessame + strong_weight * nvotessamestrong + nvotesdiffer + strong_weight * nvotesdifferstrong +
-          absent_weight * nvotesabsent + strong_weight * nvotesabsentstrong
 
-    score = nvotesdiffer + strong_weight * nvotesdifferstrong + absent_weight / 2 * nvotesabsent + strong_weight / 2 * nvotesabsentstrong
+    score = differ + strong_weight * differstrong + absent_weight / 2 * absent + strong_weight / 2 * absentstrong
+    weight = same + strong_weight * samestrong + differ + strong_weight * differstrong +
+          absent_weight * absent + strong_weight * absentstrong
+
     if weight > 0
       score / weight
     else
