f958b8d807a418cba22c0a0d012800a40ce9239f
Matthew Landauer <matthew@oaf.org.au>
2014-08-21 11:35:09 +1000

    Move methods to a more appropriate class

--- app/models/member.rb
+++ app/models/member.rb
@@ -13,41 +13,6 @@ class Member < ActiveRecord::Base
   has_many :divisions, through: :votes
   has_many :member_distances, foreign_key: :mp_id1
 
-  def self.all_rebellion_counts
-    Vote.rebellious.group("pw_mp.mp_id").count
-  end
-
-  def self.all_tells_counts
-    Vote.tells.group("pw_vote.mp_id").count
-  end
-
-  def self.all_votes_attended_counts
-    Vote.all.group("pw_vote.mp_id").count
-  end
-
-  def self.all_ayes_counts
-    Vote.ayes.group("pw_vote.mp_id").count
-  end
-
-  def self.all_noes_counts
-    Vote.noes.group("pw_vote.mp_id").count
-  end
-
-  def self.all_aye_majority_counts
-    ayes = all_ayes_counts
-    noes = all_noes_counts
-    keys = (ayes.keys + noes.keys).uniq
-    r = {}
-    keys.each do |key|
-      r[key] = (ayes[key] || 0) - (noes[key] || 0)
-    end
-    r
-  end
-
-  def self.all_votes_possible_counts
-    Division.joins("INNER JOIN pw_mp ON pw_division.house = pw_mp.house AND pw_mp.entered_house <= pw_division.division_date AND pw_division.division_date < pw_mp.left_house").group("pw_mp.mp_id").count
-  end
-
   # Give it a name like "Kevin Rudd" returns ["Kevin", "Rudd"]
   def self.parse_first_last_name(name)
     name = name.split(" ")
