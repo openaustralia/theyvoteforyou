170da0706304562df6d6471672ed3119ead0fb13
Matthew Landauer <matthew@oaf.org.au>
2014-08-21 18:18:19 +1000

    Add hack to deal with transactional fixtures

--- spec/models/member_info_spec.rb
+++ spec/models/member_info_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe MemberInfo, :type => :model do
+  # TODO Figure out why we need to do this horrible hack to remove the fixtures
+  # we shouldn't have them loaded
+  before :each do
+    Member.delete_all
+    Division.delete_all
+    Whip.delete_all
+    Vote.delete_all
+  end
+
   describe "counts" do
     let(:membera) { Member.create(mp_id: 1, first_name: "Member", last_name: "A", gid: "", source_gid: "",
       title: "", constituency: "", party: "A", house: "commons",
