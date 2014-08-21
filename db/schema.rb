5d02348e7bd0adceb1cb1c9b49db92eab0c81b8d
Matthew Landauer <matthew@oaf.org.au>
2014-08-21 18:23:44 +1000

    Drop unused tables from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711063712) do
+ActiveRecord::Schema.define(version: 20140821075351) do
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
@@ -28,28 +28,6 @@ ActiveRecord::Schema.define(version: 20140711063712) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
-
-  create_table "pw_cache_attendrank_today", id: false, force: true do |t|
-    t.integer "mp_id",        null: false
-    t.integer "attend_rank",  null: false
-    t.integer "attend_outof", null: false
-  end
-
-  add_index "pw_cache_attendrank_today", ["mp_id"], name: "mp_id", using: :btree
-
-  create_table "pw_cache_divdiv_distance", id: false, force: true do |t|
-    t.integer "division_id",              null: false
-    t.integer "division_id2",             null: false
-    t.integer "nvotespossible", limit: 2
-    t.integer "nvotessame",     limit: 2
-    t.integer "nvotesdiff",     limit: 2
-    t.integer "nvotesabsent"
-    t.float   "distance"
-  end
-
-  add_index "pw_cache_divdiv_distance", ["division_id", "division_id2"], name: "division_id_2", unique: true, using: :btree
-  add_index "pw_cache_divdiv_distance", ["division_id"], name: "division_id", using: :btree
-  add_index "pw_cache_divdiv_distance", ["division_id2"], name: "division_id2", using: :btree
 
   create_table "pw_cache_divinfo", id: false, force: true do |t|
     t.integer "division_id",      null: false
@@ -71,24 +49,17 @@ ActiveRecord::Schema.define(version: 20140711063712) do
 
   add_index "pw_cache_divwiki", ["division_date", "division_number", "house"], name: "division_date", unique: true, using: :btree
 
-  create_table "pw_cache_dreaminfo", primary_key: "dream_id", force: true do |t|
-    t.integer "cache_uptodate",       null: false
-    t.integer "votes_count",          null: false
-    t.integer "edited_motions_count", null: false
-    t.float   "consistency_with_mps"
-  end
-
   create_table "pw_cache_dreamreal_distance", id: false, force: true do |t|
-    t.integer "dream_id",           null: false
-    t.integer "person",             null: false
+    t.integer "dream_id",                      null: false
+    t.integer "person",                        null: false
     t.integer "nvotessame"
     t.integer "nvotessamestrong"
     t.integer "nvotesdiffer"
     t.integer "nvotesdifferstrong"
     t.integer "nvotesabsent"
     t.integer "nvotesabsentstrong"
-    t.float   "distance_a"
-    t.float   "distance_b"
+    t.float   "distance_a",         limit: 24
+    t.float   "distance_b",         limit: 24
   end
 
   add_index "pw_cache_dreamreal_distance", ["dream_id", "person"], name: "dream_id_2", unique: true, using: :btree
@@ -106,33 +77,19 @@ ActiveRecord::Schema.define(version: 20140711063712) do
 
   add_index "pw_cache_mpinfo", ["mp_id"], name: "mp_id", using: :btree
 
-  create_table "pw_cache_partyinfo", id: false, force: true do |t|
-    t.string  "party",       limit: 100, null: false
-    t.string  "house",       limit: 8,   null: false
-    t.integer "total_votes",             null: false
-  end
-
   create_table "pw_cache_realreal_distance", id: false, force: true do |t|
-    t.integer "mp_id1",       null: false
-    t.integer "mp_id2",       null: false
+    t.integer "mp_id1",                  null: false
+    t.integer "mp_id2",                  null: false
     t.integer "nvotessame"
     t.integer "nvotesdiffer"
     t.integer "nvotesabsent"
-    t.float   "distance_a"
-    t.float   "distance_b"
+    t.float   "distance_a",   limit: 24
+    t.float   "distance_b",   limit: 24
   end
 
   add_index "pw_cache_realreal_distance", ["mp_id1", "mp_id2"], name: "mp_id1_2", unique: true, using: :btree
   add_index "pw_cache_realreal_distance", ["mp_id1"], name: "mp_id1", using: :btree
   add_index "pw_cache_realreal_distance", ["mp_id2"], name: "mp_id2", using: :btree
-
-  create_table "pw_cache_rebelrank_today", id: false, force: true do |t|
-    t.integer "mp_id",       null: false
-    t.integer "rebel_rank",  null: false
-    t.integer "rebel_outof", null: false
-  end
-
-  add_index "pw_cache_rebelrank_today", ["mp_id"], name: "mp_id", using: :btree
 
   create_table "pw_cache_whip", id: false, force: true do |t|
     t.integer "division_id",                  null: false
@@ -148,24 +105,6 @@ ActiveRecord::Schema.define(version: 20140711063712) do
   end
 
   add_index "pw_cache_whip", ["division_id", "party"], name: "division_id", unique: true, using: :btree
-
-  create_table "pw_candidate", primary_key: "candidate_id", force: true do |t|
-    t.string "first_name",       limit: 100,                        null: false
-    t.string "last_name",        limit: 100,                        null: false
-    t.string "constituency",     limit: 100,                        null: false
-    t.string "party",            limit: 100,                        null: false
-    t.string "house",            limit: 7,                          null: false
-    t.date   "became_candidate",             default: '1000-01-01', null: false
-    t.date   "left_candidate",               default: '9999-12-31', null: false
-    t.text   "url",                                                 null: false
-  end
-
-  add_index "pw_candidate", ["became_candidate"], name: "became_candidate", using: :btree
-  add_index "pw_candidate", ["constituency"], name: "constituency", using: :btree
-  add_index "pw_candidate", ["first_name", "last_name", "constituency", "became_candidate", "left_candidate"], name: "first_name", unique: true, using: :btree
-  add_index "pw_candidate", ["house"], name: "house", using: :btree
-  add_index "pw_candidate", ["left_candidate"], name: "left_candidate", using: :btree
-  add_index "pw_candidate", ["party"], name: "party", using: :btree
 
   create_table "pw_constituency", id: false, force: true do |t|
     t.integer "cons_id",                                      null: false
@@ -201,23 +140,6 @@ ActiveRecord::Schema.define(version: 20140711063712) do
   add_index "pw_division", ["division_number"], name: "division_number", using: :btree
   add_index "pw_division", ["house"], name: "house", using: :btree
 
-  create_table "pw_dyn_aggregate_dreammp", id: false, force: true do |t|
-    t.integer "dream_id_agg",            null: false
-    t.integer "dream_id_sel",            null: false
-    t.string  "vote_strength", limit: 6, null: false
-  end
-
-  add_index "pw_dyn_aggregate_dreammp", ["dream_id_agg", "dream_id_sel"], name: "dream_id_agg_2", unique: true, using: :btree
-  add_index "pw_dyn_aggregate_dreammp", ["dream_id_agg"], name: "dream_id_agg", using: :btree
-  add_index "pw_dyn_aggregate_dreammp", ["dream_id_sel"], name: "dream_id_sel", using: :btree
-
-  create_table "pw_dyn_auditlog", primary_key: "auditlog_id", force: true do |t|
-    t.integer  "user_id",     null: false
-    t.datetime "event_date"
-    t.text     "event"
-    t.text     "remote_addr"
-  end
-
   create_table "pw_dyn_dreammp", primary_key: "dream_id", force: true do |t|
     t.string  "name",        limit: 100, null: false
     t.integer "user_id",                 null: false
@@ -240,22 +162,6 @@ ActiveRecord::Schema.define(version: 20140711063712) do
   add_index "pw_dyn_dreamvote", ["division_date"], name: "division_date", using: :btree
   add_index "pw_dyn_dreamvote", ["division_number"], name: "division_number", using: :btree
   add_index "pw_dyn_dreamvote", ["dream_id"], name: "dream_id", using: :btree
-
-  create_table "pw_dyn_newsletter", primary_key: "newsletter_id", force: true do |t|
-    t.string   "email"
-    t.text     "token",                null: false
-    t.integer  "confirm",    limit: 1
-    t.datetime "subscribed"
-  end
-
-  add_index "pw_dyn_newsletter", ["email"], name: "email", unique: true, using: :btree
-
-  create_table "pw_dyn_newsletters_sent", id: false, force: true do |t|
-    t.integer "newsletter_id",               null: false
-    t.string  "newsletter_name", limit: 100, null: false
-  end
-
-  add_index "pw_dyn_newsletters_sent", ["newsletter_id", "newsletter_name"], name: "newsletter_id", unique: true, using: :btree
 
   create_table "pw_dyn_user", primary_key: "user_id", force: true do |t|
     t.text     "user_name"
@@ -296,19 +202,6 @@ ActiveRecord::Schema.define(version: 20140711063712) do
   end
 
   add_index "pw_dyn_wiki_motion", ["division_date", "division_number", "house"], name: "division_date", using: :btree
-
-  create_table "pw_logincoming", id: false, force: true do |t|
-    t.string   "referrer", limit: 120
-    t.datetime "ltime",                null: false
-    t.string   "ipnumber", limit: 20
-    t.string   "page",     limit: 20
-    t.string   "subject",  limit: 60
-    t.string   "url",      limit: 120
-    t.integer  "thing_id"
-  end
-
-  add_index "pw_logincoming", ["ltime"], name: "ltime", using: :btree
-  add_index "pw_logincoming", ["thing_id"], name: "thing_id", using: :btree
 
   create_table "pw_moffice", primary_key: "moffice_id", force: true do |t|
     t.string  "dept",           limit: 100,                        null: false
