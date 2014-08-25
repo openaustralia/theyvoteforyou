09f099fd1f13d6b1f639caccb09ab0aa6f2c8c66
Henare Degan <henare.degan@gmail.com>
2014-08-26 09:57:48 +1000

    Set table name back after we're done
    Ensures future migrations that change data in the User model work.

--- db/migrate/20140623022321_add_confirmable_to_devise.rb
+++ db/migrate/20140623022321_add_confirmable_to_devise.rb
@@ -6,8 +6,13 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :pw_dyn_user, :unconfirmed_email, :string
     add_index :pw_dyn_user, :confirmation_token, :unique => true
 
+    # Temporarily use old table name
+    user_table_name = User.table_name
     User.table_name = "pw_dyn_user"
+    # Update existing records
     User.update_all(:confirmed_at => Time.now)
+    # Reset table name
+    User.table_name = user_table_name
   end
 
   def self.down
