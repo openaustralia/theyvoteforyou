c7ae64c173b0f257733f27c2e4e402e066888416
Matthew Landauer <matthew@oaf.org.au>
2014-08-26 17:08:34 +1000

    Can now edit real name. Fixes #262

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :real_name
+    devise_parameter_sanitizer.for(:account_update) << :real_name
   end
 end
