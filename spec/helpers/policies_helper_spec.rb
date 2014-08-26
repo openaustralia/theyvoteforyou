fba9e88f37244d4e680bf8587c89b250564004c0
Henare Degan <henare.degan@gmail.com>
2014-08-26 13:27:42 +1000

    Use RSpec helper object so ApplicationHelper is included

--- spec/helpers/policies_helper_spec.rb
+++ spec/helpers/policies_helper_spec.rb
@@ -11,8 +11,8 @@ describe PoliciesHelper, :type => :helper do
     let(:policy1) { Policy.create!(id: 1, name: "A nice policy", description: "nice", user: user, private: 0) }
     let(:policy2) { Policy.create!(id: 2, name: "A provisional policy", description: "prov", user: user, private: 2) }
 
-    it { expect(policies_list_sentence([policy1])).to eq '<a href="policy.php?id=1">A nice policy</a>' }
-    it { expect(policies_list_sentence([policy2])).to eq '<a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
-    it { expect(policies_list_sentence([policy1, policy2])).to eq '<a href="policy.php?id=1">A nice policy</a> and <a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
+    it { expect(helper.policies_list_sentence([policy1])).to eq '<a href="policy.php?id=1">A nice policy</a>' }
+    it { expect(helper.policies_list_sentence([policy2])).to eq '<a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
+    it { expect(helper.policies_list_sentence([policy1, policy2])).to eq '<a href="policy.php?id=1">A nice policy</a> and <a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
   end
 end
