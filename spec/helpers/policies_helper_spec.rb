7e1e6b8932675398d1adfe028bcf59bf136fe07f
Henare Degan <henare.degan@gmail.com>
2014-08-26 13:26:25 +1000

    Correct test expectations (makes tests fail)

--- spec/helpers/policies_helper_spec.rb
+++ spec/helpers/policies_helper_spec.rb
@@ -11,9 +11,8 @@ describe PoliciesHelper, :type => :helper do
     let(:policy1) { Policy.create!(id: 1, name: "A nice policy", description: "nice", user: user, private: 0) }
     let(:policy2) { Policy.create!(id: 2, name: "A provisional policy", description: "prov", user: user, private: 2) }
 
-    # TODO The path it's testing against is WRONG but couldn't figure it out. Ugh.
-    it { expect(policies_list_sentence([policy1])).to eq '<a href="/policy.php.1">A nice policy</a>' }
-    it { expect(policies_list_sentence([policy2])).to eq '<a href="/policy.php.2">A provisional policy</a> <i>(provisional)</i>'}
-    it { expect(policies_list_sentence([policy1, policy2])).to eq '<a href="/policy.php.1">A nice policy</a> and <a href="/policy.php.2">A provisional policy</a> <i>(provisional)</i>'}
+    it { expect(policies_list_sentence([policy1])).to eq '<a href="policy.php?id=1">A nice policy</a>' }
+    it { expect(policies_list_sentence([policy2])).to eq '<a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
+    it { expect(policies_list_sentence([policy1, policy2])).to eq '<a href="policy.php?id=1">A nice policy</a> and <a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
   end
 end
