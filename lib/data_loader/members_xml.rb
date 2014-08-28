413607cd283b05cdc8cbf65019517c5448b54447
Matthew Landauer <matthew@oaf.org.au>
2014-08-28 12:03:56 +1000

    Move offices loader to its own class

--- lib/data_loader/members_xml.rb
+++ lib/data_loader/members_xml.rb
@@ -6,37 +6,8 @@ module DataLoader
     class << self
       def load_all
         Electorates.load!
-        load_offices
+        Offices.load!
         load_representatives_and_senators
-      end
-
-      # ministers.xml
-      def load_offices
-        Rails.logger.info "Reloading offices..."
-        Rails.logger.info "Deleted #{Office.delete_all} offices"
-        ministers_xml = Nokogiri.parse(File.read("#{Settings.xml_data_directory}/members/ministers.xml"))
-        ministers_xml.search(:moffice).each do |moffice|
-          person = member_to_person[moffice[:matchid]]
-          raise "MP #{moffice[:name]} has no person" unless person
-
-          # FIXME: Don't truncate position https://github.com/openaustralia/publicwhip/issues/278
-          position = moffice[:position]
-          if position.size > 100
-            Rails.logger.warn "Truncating position \"#{position}\""
-            position.slice! 100..-1
-          end
-
-          responsibility = moffice[:responsibility] || ''
-
-          Office.create!(moffice_id: moffice[:id][/uk.org.publicwhip\/moffice\/(\d*)/, 1],
-                         dept: MembersXML.escape_html(moffice[:dept]),
-                         position: MembersXML.escape_html(position),
-                         responsibility: MembersXML.escape_html(responsibility),
-                         from_date: moffice[:fromdate],
-                         to_date: moffice[:todate],
-                         person: person[/uk.org.publicwhip\/person\/(\d*)/, 1])
-        end
-        Rails.logger.info "Loaded #{Office.count} offices"
       end
 
       # representatives.xml & senators.xml
---
+++ lib/data_loader/offices.rb
@@ -0,0 +1,32 @@
+module DataLoader
+  class Offices
+    # ministers.xml
+    def self.load!
+      Rails.logger.info "Reloading offices..."
+      Rails.logger.info "Deleted #{Office.delete_all} offices"
+      ministers_xml = Nokogiri.parse(File.read("#{Settings.xml_data_directory}/members/ministers.xml"))
+      ministers_xml.search(:moffice).each do |moffice|
+        person = member_to_person[moffice[:matchid]]
+        raise "MP #{moffice[:name]} has no person" unless person
+
+        # FIXME: Don't truncate position https://github.com/openaustralia/publicwhip/issues/278
+        position = moffice[:position]
+        if position.size > 100
+          Rails.logger.warn "Truncating position \"#{position}\""
+          position.slice! 100..-1
+        end
+
+        responsibility = moffice[:responsibility] || ''
+
+        Office.create!(moffice_id: moffice[:id][/uk.org.publicwhip\/moffice\/(\d*)/, 1],
+                       dept: MembersXML.escape_html(moffice[:dept]),
+                       position: MembersXML.escape_html(position),
+                       responsibility: MembersXML.escape_html(responsibility),
+                       from_date: moffice[:fromdate],
+                       to_date: moffice[:todate],
+                       person: person[/uk.org.publicwhip\/person\/(\d*)/, 1])
+      end
+      Rails.logger.info "Loaded #{Office.count} offices"
+    end
+  end
+end
