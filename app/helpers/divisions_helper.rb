4e086033b8dabef5bffe371b014c6a975dc52a25
Matthew Landauer <matthew@oaf.org.au>
2014-08-29 17:53:07 +1000

    On display change links for old test site to new test site. Fixes #225

--- app/helpers/divisions_helper.rb
+++ app/helpers/divisions_helper.rb
@@ -219,6 +219,10 @@ module DivisionsHelper
     text.gsub!(/(?<![<li>\s])(\[(\d+)\])/) { %(<sup class="sup-#{$~[2]}"><a class="sup" href='#footnote-#{$~[2]}' onclick="ClickSup(#{$~[2]}); return false;">#{$~[1]}</a></sup>) }
     # Footnotes
     text.gsub(/<li>\[(\d+)\]/) { %(<li class="footnote" id="footnote-#{$~[1]}">[#{$~[1]}]) }
+
+    # This is a small hack to make links to an old site point to the new site
+    text.gsub("<a href=\"http://publicwhip-test.openaustraliafoundation.org.au",
+      "<a href=\"http://publicwhip-rails.openaustraliafoundation.org.au")
   end
 
   # Use this in situations where the text is huge and all we want is it to output something
