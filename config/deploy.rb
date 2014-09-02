4e412daf58b8e29e3611a4fd2d5672612d51287e
Henare Degan <henare.degan@gmail.com>
2014-09-02 19:26:53 +1000

    Don't clobber linked_dirs as capistrano-rails wants to add to it

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, 'https://github.com/openaustralia/publicwhip.git'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, fetch(:linked_dirs, []) + %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
