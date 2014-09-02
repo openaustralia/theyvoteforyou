ec79bb110e602658b4c5ab04d0b47bf69e4196c5
Henare Degan <henare.degan@gmail.com>
2014-09-02 19:41:39 +1000

    Read attributes directly since they're delegated to non-existent model

--- db/migrate/20140828062935_add_division_id_to_policy_divisions.rb
+++ db/migrate/20140828062935_add_division_id_to_policy_divisions.rb
@@ -3,7 +3,7 @@ class AddDivisionIdToPolicyDivisions < ActiveRecord::Migration
     add_column :policy_divisions, :division_id, :integer
     PolicyDivision.reset_column_information
     PolicyDivision.all.find_each do |pd|
-      division = Division.find_by!(date: pd.division_date, number: pd.division_number, house: pd.house)
+      division = Division.find_by!(date: pd.read_attribute(:division_date), number: pd.read_attribute(:division_number), house: pd.read_attribute(:house))
       pd.update!(division_id: division.id)
     end
   end
