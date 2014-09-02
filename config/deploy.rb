79d55d16bde2f14cebfecb3f0e04e5eba10a5182
Henare Degan <henare.degan@gmail.com>
2014-09-02 19:33:37 +1000

    Only put stage-specific Capistrano config in stage-specific file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,14 @@ lock '3.2.1'
 set :application, 'publicwhip-test.openaustraliafoundation.org.au'
 set :repo_url, 'https://github.com/openaustralia/publicwhip.git'
 
+role :app, %w{deploy@kedumba.openaustraliafoundation.org.au}
+role :web, %w{deploy@kedumba.openaustraliafoundation.org.au}
+role :db,  %w{deploy@kedumba.openaustraliafoundation.org.au}
+
+set :rails_env, 'production'
+
+set :rvm_ruby_version, '2.0.0'
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
@@ -23,7 +31,7 @@ set :repo_url, 'https://github.com/openaustralia/publicwhip.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{php/loader/PublicWhip/Config.pm php/website/config.php config/database.yml config/secrets.yml config/newrelic.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []) + %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -43,4 +51,36 @@ namespace :deploy do
   end
 
   after :publishing, :restart
+end
+
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export do
+    on roles(:app) do
+      within current_path.join('rails') do
+        execute :sudo, :foreman, :export, :upstart, "/etc/init -u deploy -a publicwhip -f Procfile.production -l #{shared_path}/log --root #{current_path}"
+      end
+    end
+  end
+
+  desc "Start the application services"
+  task :start do
+    on roles(:app) do
+      execute :sudo, :service, :publicwhip, :start
+    end
+  end
+
+  desc "Stop the application services"
+  task :stop do
+    on roles(:app) do
+      execute :sudo, :service, :publicwhip, :stop
+    end
+  end
+
+  desc "Restart the application services"
+  task :restart do
+    on roles(:app) do
+      execute :sudo, :service, :publicwhip, :restart
+    end
+  end
 end
