38b3ec308c40bb0f355f425296298815238aa5b3
Luke Bacon <luke@equivalentideas.com>
2014-09-02 15:35:57 +1000

    Fix html safe on policies_list_sentence

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -7,9 +7,9 @@ module PoliciesHelper
 
   def policies_list_sentence(policies)
     policies.map do |policy|
-      text = link_to policy.name, policy
+      text = link_to h(policy.name), policy
       text += " ".html_safe + content_tag(:i, "(provisional)") if policy.provisional?
       text
-    end.to_sentence
+    end.to_sentence.html_safe
   end
 end
