3b27d0b7e08afdd41e35ea3d3c52769db596e0a0
Luke Bacon <luke@equivalentideas.com>
2014-09-02 10:40:12 +1000

    update test for sentence helper to <em> from <i>

--- spec/helpers/policies_helper_spec.rb
+++ spec/helpers/policies_helper_spec.rb
@@ -12,7 +12,7 @@ describe PoliciesHelper, :type => :helper do
     let(:policy2) { Policy.create!(id: 2, name: "A provisional policy", description: "prov", user: user, private: 2) }
 
     it { expect(helper.policies_list_sentence([policy1])).to eq '<a href="policy.php?id=1">A nice policy</a>' }
-    it { expect(helper.policies_list_sentence([policy2])).to eq '<a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
-    it { expect(helper.policies_list_sentence([policy1, policy2])).to eq '<a href="policy.php?id=1">A nice policy</a> and <a href="policy.php?id=2">A provisional policy</a> <i>(provisional)</i>'}
+    it { expect(helper.policies_list_sentence([policy2])).to eq '<a href="policy.php?id=2">A provisional policy</a> <em>(provisional)</em>'}
+    it { expect(helper.policies_list_sentence([policy1, policy2])).to eq '<a href="policy.php?id=1">A nice policy</a> and <a href="policy.php?id=2">A provisional policy</a> <em>(provisional)</em>'}
   end
 end
