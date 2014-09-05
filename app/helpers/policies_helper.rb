e49c697e32005c841ba4d436fc39675f93eb5012
Matthew Landauer <matthew@oaf.org.au>
2014-09-05 10:21:06 +1000

    Refactor helper

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -8,19 +8,22 @@ module PoliciesHelper
   end
 
   # Returns things like "voted strongly against", "has never voted on", etc..
-  def policy_agreement_summary(number_of_votes_on_policy, agreement_fraction_with_policy)
-    if number_of_votes_on_policy == 0
+  def policy_agreement_summary(policy, person)
+    if person.number_of_votes_on_policy(policy) == 0
       "has <em>never voted</em> on".html_safe
-    elsif agreement_fraction_with_policy >= 0.80
-      "voted <em>strongly for</em>".html_safe
-    elsif agreement_fraction_with_policy >= 0.60
-      "voted <em>moderately for</em>".html_safe
-    elsif agreement_fraction_with_policy <= 0.20
-      "voted <em>strongly against</em>".html_safe
-    elsif agreement_fraction_with_policy <= 0.40
-      "voted <em>moderately against</em>".html_safe
     else
-      "voted <em>ambiguously</em> on".html_safe
+      fraction = person.agreement_fraction_with_policy(policy)
+      if fraction >= 0.80
+        "voted <em>strongly for</em>".html_safe
+      elsif fraction >= 0.60
+        "voted <em>moderately for</em>".html_safe
+      elsif fraction <= 0.20
+        "voted <em>strongly against</em>".html_safe
+      elsif fraction <= 0.40
+        "voted <em>moderately against</em>".html_safe
+      else
+        "voted <em>ambiguously</em> on".html_safe
+      end
     end
   end
 end
