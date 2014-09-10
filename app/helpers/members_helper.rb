61e3c34f381ce116ad7124e5b60315b73e8a4964
Matthew Landauer <matthew@oaf.org.au>
2014-09-10 17:10:05 +1000

    Extract helper

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -26,16 +26,27 @@ module MembersHelper
     end
   end
 
-  def member_type_place_date_sentence(member)
+  def member_type_place_sentence(member)
     # TODO: if not a senator, add the state after the electorate. e.g. Goldstein, Vic
     if member.currently_in_parliament?
-      member_type(member.australian_house) + " for " +
-        content_tag(:span, member.electorate, class: "electorate") + " " +
-        content_tag(:span, "since #{vote_records_start_date(member)}", class: 'member-period')
+      text = member_type(member.australian_house) + " for " +
+        content_tag(:span, member.electorate, class: "electorate")
     else
-      "Former " + member_type(member.australian_house) + " for " +
-        content_tag(:span, member.electorate, class: 'electorate') + ", " +
-        content_tag(:span, "#{vote_records_start_date(member)} – #{member_until(member)}", class: 'member-period')
-    end.html_safe
+      text = "Former " + member_type(member.australian_house) + " for " +
+        content_tag(:span, member.electorate, class: 'electorate')
+    end
+    text.html_safe
+  end
+
+  def member_type_place_date_sentence(member)
+    text = member_type_place_sentence(member)
+    if member.currently_in_parliament?
+      text += (" " +
+        content_tag(:span, "since #{vote_records_start_date(member)}", class: 'member-period')).html_safe
+    else
+      text += (", " +
+        content_tag(:span, "#{vote_records_start_date(member)} – #{member_until(member)}", class: 'member-period')).html_safe
+    end
+    text
   end
 end
