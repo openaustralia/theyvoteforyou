48bd45b17c714950a8656051cd9c73d0bbbefbdc
Matthew Landauer <matthew@oaf.org.au>
2014-09-11 16:18:46 +1000

    Remove unused variables

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -89,8 +89,6 @@ class DivisionsController < ApplicationController
     end
 
     if @display.nil?
-      # TODO Fix this hacky nonsense by doing this query in the db
-      @votes = @division.votes.joins(:member).order(order).find_all{|v| v.rebellion?}
       @members = Member.in_australian_house(house).current_on(@division.date).joins("LEFT OUTER JOIN votes ON members.id = votes.member_id AND votes.division_id = #{@division.id}").order(order)
     elsif @display == "policies"
       if params[:dmp]
