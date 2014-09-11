6a32553a332e7ac3653aa5d78588883d4286aa7a
Henare Degan <henare.degan@gmail.com>
2014-09-11 10:44:56 +1000

    Change date from when our records started to when entered parliament

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -31,11 +31,6 @@ module MembersHelper
     }
   end
 
-  def vote_records_start_date(member)
-    # HACK WARNING
-    [member.entered_house, Date.new(2006,1,1)].max.strftime('%B %Y')
-  end
-
   def member_until(member)
     member.left_house > Date.today ? 'today' : member.left_house.strftime('%B %Y')
   end
