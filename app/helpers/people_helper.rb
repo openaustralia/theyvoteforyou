ee82d8330ae7cda5af855c7f2cf3a7bd2f894111
Matthew Landauer <matthew@oaf.org.au>
2014-09-11 13:14:50 +1000

    Extract helper

---
+++ app/helpers/people_helper.rb
@@ -0,0 +1,11 @@
+module PeopleHelper
+  # If this person has been a member of only one house then call them a "Senator" or a "Representative"
+  # otherwise just call them "person"
+  def person_type(person)
+    if person.members.count > 1 && person.members.map{|m| m.house}.uniq.count > 1
+      "person"
+    else
+      member_type(person.members.first.australian_house)
+    end
+  end
+end
