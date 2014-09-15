925b285083f02404fa92a332dcf5ef314a9d6034
Matthew Landauer <matthew@oaf.org.au>
2014-09-16 09:38:35 +1000

    Remove unused code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  # TODO: Reenable CSRF protection
-  skip_before_action :verify_authenticity_token
-
   before_action :authenticate_user!
 
   def show
