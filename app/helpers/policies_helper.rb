8581f749bdbc78c6cb800a5fb925dfe94ed447e3
Matthew Landauer <matthew@oaf.org.au>
2014-09-18 16:11:20 +1000

    Extract methods

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -27,45 +27,54 @@ module PoliciesHelper
     end
   end
 
+  def policy_version_sentence(version)
+    if version.event == "create"
+      name = version.changeset["name"].second
+      description = version.changeset["description"].second
+      if version.changeset["private"].second == 2
+        result = "Created provisional policy &ldquo;" + name + "&rdquo; with description &ldquo;" + description + "&rdquo;"
+      else
+        result = "Created policy &ldquo;" + name + "&rdquo; with description &ldquo;" + description + "&rdquo;"
+      end
+    elsif version.event == "update"
+      changes = []
+      if version.changeset.has_key?("name")
+        name1 = version.changeset["name"].first
+        name2 = version.changeset["name"].second
+        changes << "name from &ldquo;" + name1 + "&rdquo; to &ldquo;" + name2 + "&rdquo;"
+      end
+      if version.changeset.has_key?("description")
+        description1 = version.changeset["description"].first
+        description2 = version.changeset["description"].second
+        changes << "description from &ldquo;" + description1 + "&rdquo; to &ldquo;" + description2 + "&rdquo;"
+      end
+      if version.changeset.has_key?("private")
+        if version.changeset["private"].second == 0
+          changes << "status to not provisional"
+        elsif version.changeset["private"].second == 2
+          changes << "status to provisional"
+        else
+          raise
+        end
+      end
+      result = "Changed " + changes.to_sentence
+    else
+      raise
+    end
+    result
+  end
+
+  def policy_division_version_sentence(version)
+    vote = version.changeset["vote"].second
+    division = Division.find(version.changeset["division_id"].second).name
+    "Added " + vote + " vote on division " + division
+  end
+
   def version_sentence(version)
     if version.item_type == "Policy"
-      if version.event == "create"
-        name = version.changeset["name"].second
-        description = version.changeset["description"].second
-        if version.changeset["private"].second == 2
-          result = "Created provisional policy &ldquo;" + name + "&rdquo; with description &ldquo;" + description + "&rdquo;"
-        else
-          result = "Created policy &ldquo;" + name + "&rdquo; with description &ldquo;" + description + "&rdquo;"
-        end
-      elsif version.event == "update"
-        changes = []
-        if version.changeset.has_key?("name")
-          name1 = version.changeset["name"].first
-          name2 = version.changeset["name"].second
-          changes << "name from &ldquo;" + name1 + "&rdquo; to &ldquo;" + name2 + "&rdquo;"
-        end
-        if version.changeset.has_key?("description")
-          description1 = version.changeset["description"].first
-          description2 = version.changeset["description"].second
-          changes << "description from &ldquo;" + description1 + "&rdquo; to &ldquo;" + description2 + "&rdquo;"
-        end
-        if version.changeset.has_key?("private")
-          if version.changeset["private"].second == 0
-            changes << "status to not provisional"
-          elsif version.changeset["private"].second == 2
-            changes << "status to provisional"
-          else
-            raise
-          end
-        end
-        result = "Changed " + changes.to_sentence
-      else
-        raise
-      end
+      result = policy_version_sentence(version)
     elsif version.item_type == "PolicyDivision"
-      vote = version.changeset["vote"].second
-      division = Division.find(version.changeset["division_id"].second).name
-      result = "Added " + vote + " vote on division " + division
+      result = policy_division_version_sentence(version)
     else
       raise
     end
