6e004f7d56d9a5db9ca5286a84e010220afdede1
Matthew Landauer <matthew@oaf.org.au>
2014-09-19 12:00:56 +1000

    After being forced to login redirect to the correct page again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
+    stored_location_for(resource) || session[:previous_url] || root_path
   end
 
   def after_sign_out_path_for(resource)
