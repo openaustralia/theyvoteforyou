81f503947fc8d5c087e54d464f736959aecd86d7
Matthew Landauer <matthew@oaf.org.au>
2014-09-23 12:55:45 +1000

    Rename action for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -140,7 +140,7 @@ Publicwhip::Application.routes.draw do
   get '/divisions/:house/:date/:number/policies' => 'divisions#show_policies', as: :division_policies
   get '/divisions/:house/:date/:number/policies/:dmp' => 'divisions#show_policies', as: :division_policy
   post '/divisions/:house/:date/:number/policies/:dmp' => 'divisions#add_policy_vote'
-  get '/divisions/:house/:date/:number/history' => 'divisions#show_edits', as: :history_division
+  get '/divisions/:house/:date/:number/history' => 'divisions#history', as: :history_division
   get '/divisions/:house/:date/:number/edit' => 'divisions#edit', as: :edit_division
 
   resources :policies, except: :destroy do
