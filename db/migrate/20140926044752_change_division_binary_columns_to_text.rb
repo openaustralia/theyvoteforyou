5524d7d5edf94008ef69290c29d508fd5c92adb4
Henare Degan <henare.degan@gmail.com>
2014-09-26 14:54:05 +1000

    Change binary fields to text in divisions table

---
+++ db/migrate/20140926044752_change_division_binary_columns_to_text.rb
@@ -0,0 +1,8 @@
+class ChangeDivisionBinaryColumnsToText < ActiveRecord::Migration
+  def change
+    change_column :divisions, :source_url, :text
+    change_column :divisions, :debate_url, :text
+    change_column :divisions, :motion, :text
+    change_column :divisions, :notes, :text
+  end
+end
