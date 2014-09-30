a36f20c49a4a831cf2f9afa383da1083a031f5df
Matthew Landauer <matthew@oaf.org.au>
2014-10-01 07:30:58 +1000

    Add timestamps to all tables that don't have it

---
+++ db/migrate/20140930212321_add_timestamps_to_all_tables.rb
@@ -0,0 +1,18 @@
+class AddTimestampsToAllTables < ActiveRecord::Migration
+  def change
+    add_timestamps :division_infos
+    add_timestamps :divisions
+    add_timestamps :electorates
+    add_timestamps :member_distances
+    add_timestamps :member_infos
+    add_timestamps :members
+    add_timestamps :offices
+    add_timestamps :policies
+    add_timestamps :policy_divisions
+    add_timestamps :policy_person_distances
+    add_timestamps :users
+    add_timestamps :votes
+    add_timestamps :whips
+    add_timestamps :wiki_motions
+  end
+end
