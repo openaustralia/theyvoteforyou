7e35b7a1f623084f0172637d21dda83d3b8a0b98
Henare Degan <henare.degan@gmail.com>
2014-10-09 15:14:26 +1100

    Revert "Don't show provisional policies on public user profile pages"
    This reverts commit a0bf29a4a213d3e18ec334a28e32c8a867504413.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @you = (current_user && @user == current_user)
-    @policies = @you ? @user.policies : @user.policies.visible
   end
 end
