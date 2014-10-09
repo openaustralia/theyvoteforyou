a0bf29a4a213d3e18ec334a28e32c8a867504413
Henare Degan <henare.degan@gmail.com>
2014-10-09 14:55:31 +1100

    Don't show provisional policies on public user profile pages
    Fixes #675

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @you = (current_user && @user == current_user)
+    @policies = @you ? @user.policies : @user.policies.visible
   end
 end
