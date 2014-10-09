d712542b7df3faef017f278afbf1ebf5ea501d41
Henare Degan <henare.degan@gmail.com>
2014-10-09 15:30:17 +1100

    Don't show provisional policies on member#show pages. Fixes #675

--- app/models/policy_person_distance.rb
+++ app/models/policy_person_distance.rb
@@ -11,6 +11,7 @@ class PolicyPersonDistance < ActiveRecord::Base
   belongs_to :policy
   has_one :person, foreign_key: :id, primary_key: :person_id
 
+  scope :visible, -> { joins(:policy).where(policies: {private: 0}) }
   scope :very_strongly_for,     -> { where(distance_a: (0.00...0.05)) }
   scope :strongly_for,          -> { where(distance_a: (0.05...0.15)) }
   scope :moderately_for,        -> { where(distance_a: (0.15...0.40)) }
