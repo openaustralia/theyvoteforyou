d96569527a3f6d4f7d19eed372605e15c2cfd7e6
Matthew Landauer <matthew@oaf.org.au>
2014-10-14 12:34:26 +1100

    Extract vote select into helper

--- app/helpers/divisions_helper.rb
+++ app/helpers/divisions_helper.rb
@@ -159,4 +159,12 @@ module DivisionsHelper
       "display-house-all"
     end
   end
+
+  def vote_select(f, value = nil)
+    select_options = {'Aye (strong)' => 'aye3',
+                        'Aye' => 'aye',
+                        'No' => 'no',
+                        'No (strong)' => 'no3'}
+    f.select :vote, options_for_select(select_options, value), {prompt: "Select vote"}, size: 1, class: "form-control"
+  end
 end
