203ffb07727e63b8903cb72c0dbb8cf9a0f034d9
Matthew Landauer <matthew@oaf.org.au>
2014-10-21 16:41:47 +1100

    Generate api keys on the fly as they are needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,12 @@ class User < ActiveRecord::Base
     name != User.system_name
   end
 
+  def api_key
+    api_key = read_attribute(:api_key) || User.random_api_key
+    update_attribute(:api_key, api_key)
+    api_key
+  end
+
   def self.system_name
     "system"
   end
@@ -24,5 +30,9 @@ class User < ActiveRecord::Base
   # System user - used when updating divisions from a rake task
   def self.system
     find_or_create_by!(name: system_name)
+  end
+
+  def self.random_api_key
+    Digest::MD5.base64digest(rand.to_s + Time.now.to_s)[0...20]
   end
 end
