bc3d82ded3982c085c0d3f7e101f5c93f0b73cb8
Luke Bacon <luke@equivalentideas.com>
2014-10-27 14:01:16 +1100

    move interesting divisions into members controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -86,6 +86,8 @@ class MembersController < ApplicationController
     @member = @member.where(constituency: electorate)
     @member = @member.order(entered_house: :desc).first
 
+    @interesting_divisions = @member.person.members.order(entered_house: :desc) if @member
+
     render 'member_not_found', status: 404 if @member.nil?
   end
 end
