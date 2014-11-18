8e2371a7fda4170f4d2be7435dd87185541aba8e
Henare Degan <henare.degan@gmail.com>
2014-11-18 16:39:49 +1100

    Disable bootstrap-accessibility dropdown extension due to conflict
    After much agonising I think this is the simplest solution for now. We
    get all the accessibility benefits of bootstrap-accessibility (minus
    the things on dropdowns) and the usability benefits of
    bootstrap-select.

--- vendor/assets/javascripts/bootstrap-accessibility.js
+++ vendor/assets/javascripts/bootstrap-accessibility.js
@@ -98,58 +98,62 @@ $('.close').removeAttr('aria-hidden').wrapInner('<span aria-hidden="true"></span
        console.log('modalOpener' , modalOpener)
        modalOpener.focus()
     }
-  // DROPDOWN Extension
-  // ===============================
+  //////////////
+  // DISABLED due to a conflict with bootstrap-select where this would
+  // clear the whole contents of the dropdown when trying to select something
+  //
+  // // DROPDOWN Extension
+  // // ===============================
   
-  var toggle   = '[data-toggle=dropdown]'
-      , $par
-      , firstItem
-      , focusDelay = 200
-      , menus = $(toggle).parent().find('ul').attr('role','menu')
-      , lis = menus.find('li').attr('role','presentation')
+  // var toggle   = '[data-toggle=dropdown]'
+  //     , $par
+  //     , firstItem
+  //     , focusDelay = 200
+  //     , menus = $(toggle).parent().find('ul').attr('role','menu')
+  //     , lis = menus.find('li').attr('role','presentation')
 
-    lis.find('a').attr({'role':'menuitem', 'tabIndex':'-1'})
-    $(toggle).attr({ 'aria-haspopup':'true', 'aria-expanded': 'false'})
+  //   lis.find('a').attr({'role':'menuitem', 'tabIndex':'-1'})
+  //   $(toggle).attr({ 'aria-haspopup':'true', 'aria-expanded': 'false'})
 
-    $(toggle).parent().on('shown.bs.dropdown',function(e){
-      $par = $(this)
-      var $toggle = $par.find(toggle)
-      $toggle.attr('aria-expanded','true')
+  //   $(toggle).parent().on('shown.bs.dropdown',function(e){
+  //     $par = $(this)
+  //     var $toggle = $par.find(toggle)
+  //     $toggle.attr('aria-expanded','true')
 
-      setTimeout(function(){
-            firstItem = $('.dropdown-menu [role=menuitem]:visible', $par)[0]
-            try{ firstItem.focus()} catch(ex) {}
-      }, focusDelay)
-    })
+  //     setTimeout(function(){
+  //           firstItem = $('.dropdown-menu [role=menuitem]:visible', $par)[0]
+  //           try{ firstItem.focus()} catch(ex) {}
+  //     }, focusDelay)
+  //   })
 
-    $(toggle).parent().on('hidden.bs.dropdown',function(e){
-      $par = $(this)
-      var $toggle = $par.find(toggle)
-      $toggle.attr('aria-expanded','false')
-    })
+  //   $(toggle).parent().on('hidden.bs.dropdown',function(e){
+  //     $par = $(this)
+  //     var $toggle = $par.find(toggle)
+  //     $toggle.attr('aria-expanded','false')
+  //   })
 
-    //Adding Space Key Behaviour, opens on spacebar
-    $.fn.dropdown.Constructor.prototype.keydown = function (e) {
-      var  $par
-        , firstItem
-      if (!/(32)/.test(e.keyCode)) return
-        $par = $(this).parent()
-        $(this).trigger ("click")
-        e.preventDefault() && e.stopPropagation()
-    }
+  //   //Adding Space Key Behaviour, opens on spacebar
+  //   $.fn.dropdown.Constructor.prototype.keydown = function (e) {
+  //     var  $par
+  //       , firstItem
+  //     if (!/(32)/.test(e.keyCode)) return
+  //       $par = $(this).parent()
+  //       $(this).trigger ("click")
+  //       e.preventDefault() && e.stopPropagation()
+  //   }
 
-    $(document)
-      .on('focusout.dropdown.data-api', '.dropdown-menu', function(e){
-        var $this = $(this)
-                    , that = this
-        setTimeout(function() {
-         if(!$.contains(that, document.activeElement)){
-          $this.parent().removeClass('open')
-          $this.parent().find('[data-toggle=dropdown]').attr('aria-expanded','false')
-         }
-        }, 150)
-       })
-      .on('keydown.bs.dropdown.data-api', toggle + ', [role=menu]' , $.fn.dropdown.Constructor.prototype.keydown)
+  //   $(document)
+  //     .on('focusout.dropdown.data-api', '.dropdown-menu', function(e){
+  //       var $this = $(this)
+  //                   , that = this
+  //       setTimeout(function() {
+  //        if(!$.contains(that, document.activeElement)){
+  //         $this.parent().removeClass('open')
+  //         $this.parent().find('[data-toggle=dropdown]').attr('aria-expanded','false')
+  //        }
+  //       }, 150)
+  //      })
+  //     .on('keydown.bs.dropdown.data-api', toggle + ', [role=menu]' , $.fn.dropdown.Constructor.prototype.keydown)
   // Tab Extension
   // ===============================
   
