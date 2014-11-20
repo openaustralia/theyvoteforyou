82d8b12d3eb0ad23e897a49f934109594f02a990
Luke Bacon <luke@equivalentideas.com>
2014-11-20 18:48:40 +1100

    remove strong emphasis from policy vote change notice.

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -145,11 +145,11 @@ module PoliciesHelper
 
   def policy_division_version_vote(version)
     if version.event == "create"
-      content_tag(:strong, vote_display(version.changeset["vote"].second).downcase)
+      vote_display(version.changeset["vote"].second)
     elsif version.event == "destroy"
-      content_tag(:strong, vote_display(version.reify.vote).downcase)
+      vote_display(version.reify.vote)
     elsif version.event == "update"
-      content_tag(:strong, vote_display(version.changeset["vote"].first).downcase) + " to ".html_safe + content_tag(:strong, vote_display(version.changeset["vote"].second).downcase)
+      vote_display(version.changeset["vote"].first) + " to ".html_safe + vote_display(version.changeset["vote"].second)
     end
   end
 
