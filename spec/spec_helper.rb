80b8b68a73567782c9b5e2c2b4868648b7e8248e
Henare Degan <henare.degan@gmail.com>
2014-11-20 17:20:43 +1100

    Use VCR so tests don't need a network

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'html_compare_helper'
-require 'webmock/rspec'
-
-# tests can't make external requests
-WebMock.disable_net_connect!(allow: 'www.openaustralia.org')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -15,6 +11,12 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :webmock
+  # c.default_cassette_options = { record: :new_episodes }
+end
 
 RSpec.configure do |config|
   # ## Mock Framework
