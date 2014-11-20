14f091d97be43a15a7452835e54586f750ae6250
Luke Bacon <luke@equivalentideas.com>
2014-11-20 18:42:03 +1100

    added colon to make sentence not terrible if on all history page

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -178,7 +178,7 @@ module PoliciesHelper
 
     if options[:show_policy]
       policy = Policy.find(version.policy_id)
-      "On policy ".html_safe + link_to(policy.name, policy) + " ".html_safe + actions[version.event].downcase.html_safe + " ".html_safe + vote + " on ".html_safe + link_to(division.name, division)
+      "On policy ".html_safe + link_to(policy.name, policy) + ": ".html_safe + actions[version.event].downcase.html_safe + " ".html_safe + vote + " on ".html_safe + link_to(division.name, division)
     else
       if version.event == "update"
         actions[version.event].html_safe + " vote from ".html_safe + vote + " on division ".html_safe + content_tag(:em, link_to(division.name, division)) + ".".html_safe
