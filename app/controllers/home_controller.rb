020bf493c6089f356ad3c4545e78c2e516b68e25
Luke Bacon <luke@equivalentideas.com>
2014-11-21 15:01:22 +1100

    move history page logic from view into controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -41,5 +41,8 @@ class HomeController < ApplicationController
   end
 
   def history
+    @history = PaperTrail::Version.where("created_at > ?", 1.week.ago) +
+                WikiMotion.where("edit_date > ?", 1.week.ago)
+    @history.sort_by! {|v| -v.created_at.to_i}
   end
 end
