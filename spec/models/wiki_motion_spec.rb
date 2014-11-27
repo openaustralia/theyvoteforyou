aed1342e29caf64ff83ee019aedb81a88444b998
Henare Degan <henare.degan@gmail.com>
2014-11-27 14:55:15 +1100

    Duh, don't actually look at the timezone
    Allows the test to work on machines with different timezones

--- spec/models/wiki_motion_spec.rb
+++ spec/models/wiki_motion_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe WikiMotion, type: :model do
   describe 'storing edit_date in local time zone' do
-    it { expect(create(:wiki_motion, edit_date: Time.new(2014,1,1,1,1,1)).edit_date).to eq "2014-01-01 01:01:01 +1100" }
+    it { expect(create(:wiki_motion, edit_date: Time.new(2014,1,1,1,1,1)).edit_date.strftime('%F %T')).to eq "2014-01-01 01:01:01" }
   end
 end
