51e2f350fe20002b42171540ef4f3a51d47e82cf
Luke Bacon <luke@equivalentideas.com>
2014-11-27 17:20:38 +1100

    add history to the show method on UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @you = (current_user && @user == current_user)
+
+    @history = PaperTrail::Version.where("created_at > ?", 1.week.ago).where(whodunnit: @user) +
+    WikiMotion.where("edit_date > ?", 1.week.ago).where(user: @user)
+    @history.sort_by! {|v| -v.created_at.to_i}
   end
 
   def subscriptions
