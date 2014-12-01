1d0045d53288e6f8e6cf892d8a6a8726133d15ce
Henare Degan <henare.degan@gmail.com>
2014-12-01 13:25:03 +1100

    Indentation only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @you = (current_user && @user == current_user)
 
     @history = PaperTrail::Version.where(whodunnit: @user).limit(20) +
-    WikiMotion.where(user: @user).limit(20)
+               WikiMotion.where(user: @user).limit(20)
     @history = @history.sort_by {|v| -v.created_at.to_i}.take(20)
   end
 
