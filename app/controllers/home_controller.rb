5807ac6797edaf6dab2a29aae53d4322396a1a66
Henare Degan <henare.degan@gmail.com>
2014-12-10 11:33:30 +1100

    Revert "Replace member SQL search with Searchkick's elasticsearch"
    This reverts commit da5204d509161e143d6fd416c94cce29a1af14c4.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,7 +34,7 @@ class HomeController < ApplicationController
         end
       end
     elsif !params[:query].blank?
-      @mps = Member.search params[:query]
+      @mps = Member.find_by_search_query params[:query]
       @divisions = Division.find_by_search_query params[:query]
       @policies = Policy.find_by_search_query params[:query]
     end
