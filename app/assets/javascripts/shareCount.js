77cf16937025afaf5d08aee6a976562a64ea3f41
Luke Bacon <luke@equivalentideas.com>
2014-12-10 11:59:43 +1100

    only add to share count if there are more than 4 shares

--- app/assets/javascripts/shareCount.js
+++ app/assets/javascripts/shareCount.js
@@ -24,7 +24,7 @@ function updateTooltip() {
 }
 
 function addToShareCount(val) {
-  if (val !== 0) {
+  if (val > 4) {
     $shareCountEls.addClass("js-sharecount-active");
 
     var hasRun = false;
