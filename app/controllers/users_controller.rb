4548fea3120c7a30f5de399e4ae1c4ecfdd54364
Henare Degan <henare.degan@gmail.com>
2014-12-17 17:11:37 +1100

    Welcome page is only available to signed in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: :subscriptions
+  before_action :authenticate_user!, only: [:subscriptions, :welcome]
 
   def show
     @user = User.find(params[:id])
