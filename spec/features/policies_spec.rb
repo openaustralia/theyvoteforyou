d26554adc5e2d1d45bf77cb73a98ba81932f267e
Henare Degan <henare.degan@gmail.com>
2014-12-18 14:58:57 +1100

    Replace policy creation regession tests with integration test

---
+++ spec/features/policies_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature 'Policies' do
+  background do
+    # TODO: Remove this hack to delete fixtures
+    Member.delete_all
+    User.delete_all
+  end
+
+  given(:user) { create(:user, confirmed_at: Time.now) }
+
+  scenario 'successfully creating new' do
+    visit new_policy_path
+    within '#new_user' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+    end
+    click_button 'Log in'
+    within '#new_policy' do
+      fill_in 'If you are for', with: 'the creation of quality policies on this site'
+      fill_in 'you believe that', with: 'quality contributions are the bedrock of community projects'
+    end
+    click_button 'Make Policy'
+    expect(page).to have_content 'Successfully made new policy'
+    expect(page).to have_content 'For the creation of quality policies on this site'
+    expect(page).to have_content 'Quality contributions are the bedrock of community projects'
+  end
+end
--- spec/requests/policies_controller_spec.rb
+++ spec/requests/policies_controller_spec.rb
@@ -28,16 +28,6 @@ describe PoliciesController, type: :request do
   end
 
   describe '#add' do
-    # The PHP app does something really silly when we're not logged in,
-    # it turns this page into a login page. We're going to redirect to the
-    # login page instead (which redirects back here after login) so disabling
-    # this test
-    #it { compare url }
-
-    it { compare_static '/policies/new', true, false }
-
-    it { compare_static '/policies', true, {commit: 'Make Policy', policy: {name: 'nuclear power', description: 'nuclear power is great.'}} }
-
     it { compare_static '/policies', true, {commit: 'Make Policy', policy: {name: '', description: 'nuclear power is great.'}}, "_2" }
     it { compare_static '/policies', true, {commit: 'Make Policy', policy: {name: 'nuclear power', description: ''}}, "_3" }
   end
