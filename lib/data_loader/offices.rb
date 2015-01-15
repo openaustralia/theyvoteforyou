acafc0988ce730e643b26a3bc987c59c3db0af9c
Henare Degan <henare.degan@gmail.com>
2015-01-16 10:29:52 +1100

    Revert "Update existing offices"
    This reverts commit b3e8bc792de405ca2a99e2d68d980012d2994beb.
    
    Conflicts:
    	lib/data_loader/offices.rb
    
    It turns out that office IDs can change upstream because they're just
    generated sequentially. The upstream file always has a complete list
    so it's safe to completely reload.
    
    Fixes #1045

--- lib/data_loader/offices.rb
+++ lib/data_loader/offices.rb
@@ -7,19 +7,20 @@ module DataLoader
       Rails.logger.info "Reloading offices..."
       agent = Mechanize.new
       ministers_xml = agent.get "#{Settings.xml_data_base_url}members/ministers.xml"
+      Rails.logger.info "Deleted #{Office.delete_all} offices"
       ministers_xml.search(:moffice).each do |moffice|
         person_id = People.member_to_person[moffice[:matchid]]
         raise "MP #{moffice[:name]} has no person" unless person_id
 
         responsibility = moffice[:responsibility] || ''
 
-        o = Office.find_or_initialize_by(id: moffice[:id][/uk.org.publicwhip\/moffice\/(\d*)/, 1],
-          person_id: person_id[/uk.org.publicwhip\/person\/(\d*)/, 1])
-        o.update!(dept: moffice[:dept],
-          position: moffice[:position],
-          responsibility: responsibility,
-          from_date: moffice[:fromdate],
-          to_date: moffice[:todate])
+        Office.create!(id: moffice[:id][/uk.org.publicwhip\/moffice\/(\d*)/, 1],
+                       person_id: person_id[/uk.org.publicwhip\/person\/(\d*)/, 1],
+                       dept: moffice[:dept],
+                       position: moffice[:position],
+                       responsibility: responsibility,
+                       from_date: moffice[:fromdate],
+                       to_date: moffice[:todate])
       end
       Rails.logger.info "Loaded #{Office.count} offices"
     end
