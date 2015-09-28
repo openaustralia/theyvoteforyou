6658582fe8d671e425405cc5c6b302d826cfcd5f
Henare Degan <henare.degan@gmail.com>
2015-09-28 11:28:16 +1000

    Add Mina stages to deploy Ukraine to

---
+++ config/deploy/ukraine-dev.rb
@@ -0,0 +1,3 @@
+set :domain, 'theyvoteforyou.org.au.dev'
+set :repository, 'https://github.com/opora/publicwhip.git'
+set :branch, 'develop'
---
+++ config/deploy/ukraine-production.rb
@@ -0,0 +1,3 @@
+set :domain, 'li1413-108.members.linode.com'
+set :repository, 'https://github.com/opora/publicwhip.git'
+set :branch, 'develop'
