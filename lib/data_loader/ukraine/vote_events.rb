f7a4a58e0fb5ca1497ad7f3636e43829b6c0c0e6
Henare Degan <henare.degan@gmail.com>
2015-11-17 16:05:19 +1100

    Use shared method to load vote events

--- lib/data_loader/ukraine/vote_events.rb
+++ lib/data_loader/ukraine/vote_events.rb
@@ -1,6 +1,3 @@
-require "open-uri"
-require "json"
-
 module DataLoader
   module Ukraine
     class VoteEvents
@@ -8,53 +5,49 @@ module DataLoader
 
       def self.load!(date)
         url = BASE_URL + date.to_s
-        Rails.logger.info "Loading Ukraine vote events from #{url}..."
-        data = JSON.parse(open(url).read)
+        data = DataLoader::Ukraine::Popolo.load(url)
+        vote_events = data["vote_events"]
 
-        if vote_events = data["vote_events"]
-          Rails.logger.info "Loading #{vote_events.count} vote_events..."
-          vote_events.each do |v_e|
-            ActiveRecord::Base.transaction do
-              division = Division.find_or_initialize_by(id: v_e["identifier"])
-              division.date = DateTime.parse(v_e["start_date"]).strftime("%F")
-              division.number = v_e["identifier"]
-              division.house = v_e["organization_id"]
-              division.name = v_e["title"]
-              division.source_url = v_e["sources"].find { |s| s["note"] == "Source URL" }["url"]
-              division.debate_url = v_e["sources"].find { |s| s["note"] == "Debate URL" }["url"]
-              division.motion = ""
-              division.clock_time = DateTime.parse(v_e["start_date"]).strftime("%T")
-              division.source_gid = v_e["identifier"]
-              division.debate_gid = ""
-              division.result = v_e["result"]
-              division.save!
+        Rails.logger.info "Loading #{vote_events.count} vote_events..."
+        vote_events.each do |v_e|
+          ActiveRecord::Base.transaction do
+            division = Division.find_or_initialize_by(id: v_e["identifier"])
+            division.date = DateTime.parse(v_e["start_date"]).strftime("%F")
+            division.number = v_e["identifier"]
+            division.house = v_e["organization_id"]
+            division.name = v_e["title"]
+            division.source_url = v_e["sources"].find { |s| s["note"] == "Source URL" }["url"]
+            division.debate_url = v_e["sources"].find { |s| s["note"] == "Debate URL" }["url"]
+            division.motion = ""
+            division.clock_time = DateTime.parse(v_e["start_date"]).strftime("%T")
+            division.source_gid = v_e["identifier"]
+            division.debate_gid = ""
+            division.result = v_e["result"]
+            division.save!
 
-              votes = v_e["votes"]
-              Rails.logger.info "Loading #{votes.count} votes..."
-              votes.each do |v|
-                member = Member.current_on(division.date).find_by!(person_id: v["voter_id"])
-                vote = division.votes.find_or_initialize_by(member: member)
-                if option = popolo_to_publicwhip_vote(v["option"])
-                  vote.vote = option
-                  vote.save!
-                else
-                  vote.destroy
-                end
-              end
-
-              bills = v_e["bills"]
-              Rails.logger.info "Loading #{bills.count} bills..."
-              bills.each do |b|
-                # We need to use create here because otherwise the association isn't saved
-                bill = division.bills.find_or_create_by(official_id: b["official_id"])
-                bill.url = b["url"]
-                bill.title = b["title"]
-                bill.save!
+            votes = v_e["votes"]
+            Rails.logger.info "Loading #{votes.count} votes..."
+            votes.each do |v|
+              member = Member.current_on(division.date).find_by!(person_id: v["voter_id"])
+              vote = division.votes.find_or_initialize_by(member: member)
+              if option = popolo_to_publicwhip_vote(v["option"])
+                vote.vote = option
+                vote.save!
+              else
+                vote.destroy
               end
             end
+
+            bills = v_e["bills"]
+            Rails.logger.info "Loading #{bills.count} bills..."
+            bills.each do |b|
+              # We need to use create here because otherwise the association isn't saved
+              bill = division.bills.find_or_create_by(official_id: b["official_id"])
+              bill.url = b["url"]
+              bill.title = b["title"]
+              bill.save!
+            end
           end
-        else
-          raise "No loadable data found"
         end
       end
 
