1a2a2231c5591b2c6f069c2e3bebd2d8dce8853e
Henare Degan <henare.degan@gmail.com>
2015-11-17 16:03:57 +1100

    Add a method to download and parse Popolo

--- lib/data_loader/ukraine/popolo.rb
+++ lib/data_loader/ukraine/popolo.rb
@@ -4,6 +4,13 @@ require "json"
 module DataLoader
   module Ukraine
     class Popolo
+      def self.load(url)
+        Rails.logger.info "Loading Ukraine Popolo data from #{url}..."
+        data = JSON.parse(open(url).read)
+        raise "No loadable data found" unless data["persons"] || data["vote_events"]
+
+        data
+      end
     end
   end
 end
