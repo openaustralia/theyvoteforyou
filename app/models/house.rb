7f82ca2c9ea8d2937cbd41ce49694f388a51059b
Henare Degan <henare.degan@gmail.com>
2015-12-07 17:38:37 +1100

    Make house model work in Ukraine
    This allows us to see the page at "/people/rada"

--- app/models/house.rb
+++ app/models/house.rb
@@ -4,8 +4,16 @@ class House
       %w(representatives senate)
     end
 
+    def ukrainian
+      "rada"
+    end
+
     def valid?(name)
-      australian.include?(name)
+      if I18n.locale == :uk
+        name == ukrainian
+      else
+        australian.include?(name)
+      end
     end
   end
 end
