214ce09ddd0cb17426974bb8970c67420ddeadee
essymo <esthermonchari@gmail.com>
2017-03-28 12:31:12 +0300

    Adding Generic 404 page error

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Publicwhip
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.exceptions_app = self.routes
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
