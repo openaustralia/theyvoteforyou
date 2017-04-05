663e16123480f53beeb8f74d5d6f389423ce866b
Mário Areias <mario.s.s.areias@gmail.com>
2017-04-05 21:11:04 +1000

    Added bullet gem to look for N+1 queries

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,4 +31,13 @@ Publicwhip::Application.configure do
   config.assets.debug = true
 
   config.middleware.use Rack::LiveReload
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
