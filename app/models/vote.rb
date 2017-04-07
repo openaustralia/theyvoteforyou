43a03d178e5bb09a31ce986f9e9c03f2e450adb9
Mário Areias <mario.s.s.areias@gmail.com>
2017-04-07 19:19:57 +1000

    Fixed rebellion method to accept nil parameter

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -29,12 +29,12 @@ class Vote < ActiveRecord::Base
     where("votes.vote = 'no'")
   end
 
-  def rebellion?
-    !free? && vote != whip_guess
-  end
-
-  def rebellion?(w)
-    !w.free? && vote != w.whip_guess
+  def rebellion?(whip=nil)
+    if whip.nil?
+      !free? && vote != whip_guess
+    else
+      !whip.free? && vote != whip.whip_guess
+    end
   end
 
   # TODO What if the vote is tied?
