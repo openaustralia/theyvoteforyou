b392cfd8429f8167798eed16f8a2b53393c16149
Mário Areias <mario.s.s.areias@gmail.com>
2017-04-07 19:30:11 +1000

    Removed alert of the gem bullet to make notification less intrusive

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,6 @@ Publicwhip::Application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
