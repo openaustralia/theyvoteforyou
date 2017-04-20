712208131529289184370f066dee4d53887e2b5f
Luke Bacon <luke@equivalentideas.com>
2017-04-20 18:49:42 +1000

    Extract helper from view logic

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def search_people_form_placeholder_text
+    if Member.any?
+      "e.g. #{Member.random_postcode} or #{Member.random_current.name_without_title}"
+    else
+      nil
+    end
+  end
 end
--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe HomeHelper, type: :helper do
+  describe "#search_people_form_placeholder_text" do
+    context "when there are no people" do
+      it "is nil" do
+        expect(helper.search_people_form_placeholder_text).to eq nil
+      end
+    end
+
+    context "when there are people" do
+      before do
+        member = create :member, first_name: "Magic", last_name: "Jane"
+        allow(Member).to receive(:random_postcode).and_return "1234"
+      end
+
+      it "uses the details of a random person" do
+        expect(helper.search_people_form_placeholder_text).to eql "e.g. 1234 or Magic Jane"
+      end
+    end
+  end
 end
