b357d447696c11df00c061c3860c6d4bbfebb114
Luke Bacon <luke@equivalentideas.com>
2017-04-23 21:16:17 +0200

    Move these specs onto the factory data off fixtures
    This makes it a bit easier to understand what data this is using to run
    the test.

--- spec/requests/policies_controller_spec.rb
+++ spec/requests/policies_controller_spec.rb
@@ -8,10 +8,22 @@ describe PoliciesController, type: :request do
     clear_db_of_fixture_data
     DatabaseCleaner.start
 
+    create_people
+    create_members
+    create_member_infos
+
     create_divisions
+    create_whips
+    create_votes
+    create_wiki_motions
+
     create_policies
     create_policy_divisions
-    create_wiki_motions
+    # We want to feed specific PolicyPersonDistance records here,
+    # so we need to clear out the ones generated when the PolicyDivisions
+    # are created/the callback is triggered.
+    PolicyPersonDistance.delete_all
+    create_policy_person_distances
   end
 
   after(:each) do
@@ -23,7 +35,6 @@ describe PoliciesController, type: :request do
   end
 
   describe "#show" do
-    fixtures :all
     it { compare_static("/policy.php?id=1") }
     it { compare_static("/policy.php?id=1&display=motions") }
 
