cfaac6f0380f02e74bd5a3eb0678f0acde93af37
Luke Bacon <luke@equivalentideas.com>
2017-04-23 22:50:17 +0200

    Move another test onto factory data off fixtures

--- spec/requests/feeds_controller_spec.rb
+++ spec/requests/feeds_controller_spec.rb
@@ -4,22 +4,20 @@ describe FeedsController, type: :request do
   include HTMLCompareHelper
 
   describe '#mp-info' do
-    context "for all MPs" do
-      fixtures :all
+    before :each do
+      clear_db_of_fixture_data
 
+      create_divisions
+      create_people
+      create_members
+      create_member_infos
+    end
+
+    context "for all MPs" do
       it { compare_static '/feeds/mp-info.xml' }
     end
 
     context "for only the senate" do
-      before do
-        clear_db_of_fixture_data
-
-        create_divisions
-        create_people
-        create_members
-        create_member_infos
-      end
-
       it { compare_static '/feeds/mp-info.xml?house=senate' }
     end
   end
