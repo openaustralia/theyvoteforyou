82c6bf15bf7f2507c97fc76dcb1ed494ca1f74ff
Luke Bacon <luke@equivalentideas.com>
2017-04-24 17:33:05 +0200

    Make the catch-all method more complete

--- spec/support/static_regression_test_data_helpers.rb
+++ spec/support/static_regression_test_data_helpers.rb
@@ -2,12 +2,19 @@ Dir[Rails.root.join('spec/support/static_regression_test_data/*.rb')].each { |f|
 
 module StaticRegressionTestDataHelpers
   def create_data_for_static_regression_tests
+    create_offices
     create_users
     create_people
     create_members
+    create_member_infos
     create_policies
     create_divisions
     create_policy_divisions
+    # We want to feed specific PolicyPersonDistance records here,
+    # so we need to clear out the ones generated when the PolicyDivisions
+    # are created/the callback is triggered.
+    PolicyPersonDistance.delete_all
+    create_policy_person_distances
     create_whips
     create_votes
     create_wiki_motions
