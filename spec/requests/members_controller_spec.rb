819c79fd45e1d49df4d1d5c82e1bf1d45966e644
Luke Bacon <luke@equivalentideas.com>
2017-05-06 16:28:24 +0200

    Make tests more isolated

--- spec/requests/members_controller_spec.rb
+++ spec/requests/members_controller_spec.rb
@@ -67,52 +67,52 @@ describe MembersController, type: :request do
 
     # Test free teller under Interesting Votes
     it {compare_static("/mp.php?mpn=Roger_Price&mpc=Chifley&house=representatives")}
+  end
 
-    context "Barnaby Joyce" do
-      before :each do
-        clear_db_of_fixture_data
-        create_divisions
-        create_votes
-        create_whips
-        create_wiki_motions
+  describe "#show for Barnaby Joyce" do
+    before :each do
+      create_divisions
+      create_votes
+      create_whips
+      create_wiki_motions
 
-        Person.create(id: 10350, large_image_url: "http://www.openaustralia.org/images/mpsL/10350.jpg")
-        Member.create(id: 664, gid: "uk.org.publicwhip/member/664", source_gid: "",
-          first_name: "Barnaby", last_name: "Joyce", title: "", person_id: 10350,
-          party: "National Party",
-          house: "representatives", constituency: "New England",
-          entered_house: "2013-09-07", left_house: "9999-12-31")
-        Member.create(id: 100114, gid: "uk.org.publicwhip/lord/100114", source_gid: "",
-          first_name: "Barnaby", last_name: "Joyce", title: "", person_id: 10350,
-          party: "National Party",
-          house: "senate", constituency: "Queensland",
-          entered_house: "2005-07-01", left_house: "2013-08-08")
+      Person.create(id: 10350, large_image_url: "http://www.openaustralia.org/images/mpsL/10350.jpg")
+      Member.create(id: 664, gid: "uk.org.publicwhip/member/664", source_gid: "",
+        first_name: "Barnaby", last_name: "Joyce", title: "", person_id: 10350,
+        party: "National Party",
+        house: "representatives", constituency: "New England",
+        entered_house: "2013-09-07", left_house: "9999-12-31")
+      Member.create(id: 100114, gid: "uk.org.publicwhip/lord/100114", source_gid: "",
+        first_name: "Barnaby", last_name: "Joyce", title: "", person_id: 10350,
+        party: "National Party",
+        house: "senate", constituency: "Queensland",
+        entered_house: "2005-07-01", left_house: "2013-08-08")
 
-        Electorate.create(id: 143, name: "New England", main_name: true)
-      end
-
-      it { compare_static("/mp.php?mpn=Barnaby_Joyce&mpc=New_England&house=representatives") }
-
-      # TODO: Should this be in spec/routing/redirects_spec.rb ?
-      it "should redirect to the senator's page even if the id param incorrectly identifies a member as a senator and vice versa" do
-        # Barnaby has been set up above as a member and a senator.
-        # Let's refer to his senator record but incorrectly using `member` instead of `lord`
-        get "/mp.php?id=uk.org.publicwhip/member/100114"
-        expect(response.status).to eq 302
-        expect(response.headers["location"]).to eq "/mp.php?house=senate&mpc=Queensland&mpn=Barnaby_Joyce"
-      end
+      Electorate.create(id: 143, name: "New England", main_name: true)
     end
 
-    # TODO: Should this be in spec/controllers/members_controller_spec.rb ?
-    it "should 404 with an unknown person" do
-      get "/mp.php?mpn=Foo_Bar"
-      expect(response.status).to eq 404
-    end
+    it { compare_static("/mp.php?mpn=Barnaby_Joyce&mpc=New_England&house=representatives") }
 
-    # TODO: Should this be in spec/controllers/members_controller_spec.rb ?
-    it "should 404 when the wrong name is given for a correct electorate" do
-      get "/people/representatives/warringah/foo_bar"
-      expect(response.status).to eq(404)
+    # TODO: Should this be in spec/routing/redirects_spec.rb ?
+    it "should redirect to the senator's page even if the id param incorrectly identifies a member as a senator and vice versa" do
+      # Barnaby has been set up above as a member and a senator.
+      # Let's refer to his senator record but incorrectly using `member` instead of `lord`
+      get "/mp.php?id=uk.org.publicwhip/member/100114"
+      expect(response.status).to eq 302
+      expect(response.headers["location"]).to eq "/mp.php?house=senate&mpc=Queensland&mpn=Barnaby_Joyce"
     end
   end
+
+  # TODO: Should this be in spec/controllers/members_controller_spec.rb ?
+  it "should 404 with an unknown person" do
+    get "/mp.php?mpn=Foo_Bar"
+    expect(response.status).to eq 404
+  end
+
+  # TODO: Should this be in spec/controllers/members_controller_spec.rb ?
+  it "should 404 when the wrong name is given for a correct electorate" do
+    get "/people/representatives/warringah/foo_bar"
+    expect(response.status).to eq(404)
+  end
+
 end
