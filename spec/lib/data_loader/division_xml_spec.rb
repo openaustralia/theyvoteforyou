f2a8482f5372930e9db2a39a85c9f4f437887c70
Luke Bacon <luke@equivalentideas.com>
2017-05-06 15:23:49 +0200

    Add database clearing to make sure test setup is isolated

--- spec/lib/data_loader/division_xml_spec.rb
+++ spec/lib/data_loader/division_xml_spec.rb
@@ -3,6 +3,11 @@ require 'nokogiri'
 
 describe DataLoader::DebatesXML do
   context 'actual division 1 from representatives on 2009-11-25' do
+    after(:each) do
+      clear_db_of_fixture_data
+      DatabaseCleaner.clean
+    end
+
     subject(:division) do
       xml_document = Nokogiri.parse(File.read(File.expand_path('../../../fixtures/2009-11-25.xml', __FILE__)))
       DataLoader::DebatesXML.new(xml_document, 'representatives').divisions.first
