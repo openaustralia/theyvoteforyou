282bbfb2246d53879f71e45408f3885ad677bd19
Mark Cottman-Fields <cofiem@gmail.com>
2017-11-18 19:34:09 +1000

    Modified policy list page to add subscriber count and last division date.
    Also added css media query for better layout on small screen.
    Closes #1111

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -33,6 +33,10 @@ class Policy < ActiveRecord::Base
     PaperTrail::Version.order(created_at: :desc).find_by(policy_id: id)
   end
 
+  def most_recent_division_date
+    divisions.maximum('date')
+  end
+
   def last_edited_at
     most_recent_version ? most_recent_version.created_at : updated_at
   end
