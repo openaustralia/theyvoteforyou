a2d0a476192d3ea64e9bd8f70e92ec00cbee26dc
James Polley <jp@jamezpolley.com>
2019-01-02 16:19:30 +1100

    Update dev hostname from .dev to .test

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,6 +1,6 @@
 set :branch, :master
 set :deploy_to, "/srv/www/production"
 
-role :app, %w{deploy@theyvoteforyou.org.au.dev}
-role :web, %w{deploy@theyvoteforyou.org.au.dev}
-role :db,  %w{deploy@theyvoteforyou.org.au.dev}
+role :app, %w{deploy@theyvoteforyou.org.au.test}
+role :web, %w{deploy@theyvoteforyou.org.au.test}
+role :db,  %w{deploy@theyvoteforyou.org.au.test}
