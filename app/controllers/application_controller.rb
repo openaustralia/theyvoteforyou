676062b13bd6bbf92c2958964a3710d28487d19b
Matthew Landauer <mlandauer@gmail.com>
2021-04-21 16:14:48 +1000

    Remove another deprecation warning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_filter :store_location
+  after_action :store_location
 
   protected
 
