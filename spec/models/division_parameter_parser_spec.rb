81497dd3601a59a574ec12bef9439f03ddc0a55e
Matthew Landauer <mlandauer@gmail.com>
2021-10-12 13:37:38 +1100

    For regular integers don't put 0 in front of them

--- spec/models/division_parameter_parser_spec.rb
+++ spec/models/division_parameter_parser_spec.rb
@@ -13,16 +13,16 @@ describe DivisionParameterParser, type: :model do
     it "gets range for year and month date's format" do
       date_start, date_end, date_range = DivisionParameterParser.get_date_range("2017-12")
 
-      expect(date_start).to eq(Date.new(2017, 12, 01))
-      expect(date_end).to eq(Date.new(2018, 01, 01))
+      expect(date_start).to eq(Date.new(2017, 12, 1))
+      expect(date_end).to eq(Date.new(2018, 1, 1))
       expect(date_range).to eq(:month)
     end
 
     it "gets range for year only date's format" do
       date_start, date_end, date_range = DivisionParameterParser.get_date_range("2017")
 
-      expect(date_start).to eq(Date.new(2017, 01, 01))
-      expect(date_end).to eq(Date.new(2018, 01, 01))
+      expect(date_start).to eq(Date.new(2017, 1, 1))
+      expect(date_end).to eq(Date.new(2018, 1, 1))
       expect(date_range).to eq(:year)
     end
 
