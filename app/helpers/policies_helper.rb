b4dbde0ae2a232cb2712ff258de3de3430d028b5
Matthew Landauer <mlandauer@gmail.com>
2021-10-14 12:09:08 +1100

    Extract helper methods

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -15,17 +15,24 @@ module PoliciesHelper
 
   # Returns things like "voted strongly against", "has never voted on", etc..
   def policy_agreement_summary(policy_member_distance)
-    if policy_member_distance.nil?
-      first_word = "voted"
-      text = "unknown about"
-    elsif policy_member_distance.number_of_votes.zero?
-      first_word = "has"
-      text = "never voted on"
-    else
-      first_word = "voted"
-      text = ranges.find { |r| r.first.include?(policy_member_distance.agreement_fraction) }.second
-    end
-    first_word.html_safe + " ".html_safe + content_tag(:strong, text)
+    policy_agreement_summary_first_word(policy_member_distance) +
+      " ".html_safe +
+      content_tag(:strong, policy_agreement_summary_short(policy_member_distance))
+  end
+
+  def policy_agreement_summary_first_word(policy_member_distance)
+    (policy_member_distance&.number_of_votes&.zero? ? "has" : "voted").html_safe
+  end
+
+  def policy_agreement_summary_short(policy_member_distance)
+    text = if policy_member_distance.nil?
+             "unknown about"
+           elsif policy_member_distance.number_of_votes.zero?
+             "never voted on"
+           else
+             ranges.find { |r| r.first.include?(policy_member_distance.agreement_fraction) }.second
+           end
+    text.html_safe
   end
 
   # TODO: This shouldn't really be in a helper should it? It smells a lot like "business" logic
