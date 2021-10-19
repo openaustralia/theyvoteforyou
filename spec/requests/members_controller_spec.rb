fdb717d756461e60a7521f54fa28bf29abaf3864
Matthew Landauer <mlandauer@gmail.com>
2021-10-19 12:00:34 +1100

    Move a redirect spec to where the others are

--- spec/requests/members_controller_spec.rb
+++ spec/requests/members_controller_spec.rb
@@ -59,14 +59,6 @@ describe MembersController, type: :request do
       end
 
       it { compare_static("/people/representatives/new_england/barnaby_joyce") }
-
-      it "should redirect to the senator's page even if the id param incorrectly identifies a member as a senator and vice versa" do
-        # Barnaby has been set up above as a member and a senator.
-        # Let's refer to his senator record but incorrectly using `member` instead of `lord`
-        get "/mp.php?id=uk.org.publicwhip/member/100114", params: {}
-        expect(response.status).to eq 302
-        expect(response.headers["location"]).to eq "/mp.php?house=senate&mpc=Queensland&mpn=Barnaby_Joyce"
-      end
     end
 
     it "should 404 with an unknown person" do
--- spec/routing/redirects_spec.rb
+++ spec/routing/redirects_spec.rb
@@ -373,4 +373,30 @@ describe "routing redirects", type: :request do
     get "/parties/liberal_party/divisions/representatives", params: {}
     expect(response).to redirect_to "/divisions/representatives"
   end
+
+  context "with Barnaby Joyce" do
+    before do
+      Person.create(id: 10350, large_image_url: "https://www.openaustralia.org.au/images/mpsL/10350.jpg")
+      Member.create(id: 664, gid: "uk.org.publicwhip/member/664", source_gid: "",
+                    first_name: "Barnaby", last_name: "Joyce", title: "", person_id: 10350,
+                    party: "National Party",
+                    house: "representatives", constituency: "New England",
+                    entered_house: "2013-09-07", left_house: "9999-12-31")
+      Member.create(id: 100114, gid: "uk.org.publicwhip/lord/100114", source_gid: "",
+                    first_name: "Barnaby", last_name: "Joyce", title: "", person_id: 10350,
+                    party: "National Party",
+                    house: "senate", constituency: "Queensland",
+                    entered_house: "2005-07-01", left_house: "2013-08-08")
+
+      Electorate.create(id: 143, name: "New England", main_name: true)
+    end
+
+    it "redirects to the senator's page even if the id param incorrectly identifies a member as a senator and vice versa" do
+      # Barnaby has been set up above as a member and a senator.
+      # Let's refer to his senator record but incorrectly using `member` instead of `lord`
+      get "/mp.php?id=uk.org.publicwhip/member/100114", params: {}
+      expect(response.status).to eq 302
+      expect(response.headers["location"]).to eq "/mp.php?house=senate&mpc=Queensland&mpn=Barnaby_Joyce"
+    end
+  end
 end
