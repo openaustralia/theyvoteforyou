9295b0ffda36bb8d10ac7ff980d9b93c528c8747
Matthew Landauer <mlandauer@gmail.com>
2021-10-19 13:59:50 +1100

    Keep rubocop happy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -97,7 +97,7 @@ module ApplicationHelper
     Date.parse("#{month}-01").strftime("%B %Y")
   end
 
-  def formatted_date(date, include_nbsp = false)
+  def formatted_date(date, include_nbsp: false)
     include_nbsp ? date.strftime("#{date.day.ordinalize}&nbsp;%b&nbsp;%Y").html_safe : date.strftime("#{date.day.ordinalize} %b %Y")
   end
 
