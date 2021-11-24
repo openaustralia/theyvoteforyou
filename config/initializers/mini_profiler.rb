6a59800adb171c0ad4e8fcda592c04810f2842df
Matthew Landauer <mlandauer@gmail.com>
2021-11-24 13:52:48 +1100

    Go back to always showing rack-mini-profiler in development

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -2,4 +2,4 @@
 
 # Config for mini profiler
 # https://github.com/MiniProfiler/rack-mini-profiler#configuration-options
-Rack::MiniProfiler.config.start_hidden = true if Rails.env.development?
+# Rack::MiniProfiler.config.start_hidden = true if Rails.env.development?
