3c367d40e13ed639e40af4b87eda58c04fa66b82
Matthew Landauer <mlandauer@gmail.com>
2021-12-13 13:39:57 +1100

    Remove unused methods

--- app/models/person.rb
+++ app/models/person.rb
@@ -65,17 +65,6 @@ class Person < ApplicationRecord
     members.last
   end
 
-  def member_who_voted_on_division(division)
-    # What we have now in @member is a member related to the person that voted in division but @member wasn't necessarily
-    # current when @division took place. So, let's fix this
-    # We're doing this the same way as the php which doesn't seem necessarily the best way
-    # TODO Figure what is the best way
-    new_member = members.find do |member|
-      member.vote_on_division_without_tell(division) != "absent"
-    end
-    new_member || latest_member
-  end
-
   # Find the member that relates to a given policy
   # Let's just step through the votes of the policy and find the first matching member
   def member_for_policy(policy)
@@ -92,11 +81,6 @@ class Person < ApplicationRecord
     pmd ? pmd.agreement_fraction : 0
   end
 
-  def number_of_votes_on_policy(policy)
-    pmd = policy_person_distances.find_by(policy: policy)
-    pmd ? pmd.number_of_votes : 0
-  end
-
   def current_offices
     # Checking for the to_date after the sql query to get the same result as php
     offices.order(from_date: :desc).select { |o| o.to_date == Date.new(9999, 12, 31) }
@@ -104,11 +88,5 @@ class Person < ApplicationRecord
 
   def offices_on_date(date)
     offices.where("? >= from_date AND ? <= to_date", date, date)
-  end
-
-  # TODO: This is wrong as parliamentary secretaries will be considered to be on the
-  # front bench which as far as I understand is not the case
-  def on_front_bench?(date)
-    !offices_on_date(date).empty?
   end
 end
