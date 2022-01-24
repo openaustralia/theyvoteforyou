94133664095657926f60dca82b60fdf74520e6ab
Matthew Landauer <mlandauer@gmail.com>
2022-01-24 16:45:41 +1100

    Fix markdown handler under rails 6.0

--- config/initializers/markdown_handler.rb
+++ config/initializers/markdown_handler.rb
@@ -13,8 +13,8 @@ module MarkdownHandler
     @erb ||= ActionView::Template.registered_template_handler(:erb)
   end
 
-  def self.call(template)
-    compiled_source = erb.call(template)
+  def self.call(template, source)
+    compiled_source = erb.call(template, source)
     "Redcarpet::Markdown.new(BootstrapTableRenderer, tables: true).render(begin;#{compiled_source};end).html_safe"
   end
 end
