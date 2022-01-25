748ca986ac11e7fedb4125908042f9e0028d7d9b
Matthew Landauer <mlandauer@gmail.com>
2022-01-25 11:36:32 +1100

    If person isn't found in loading office provide more helpful message

--- app/lib/data_loader/offices.rb
+++ app/lib/data_loader/offices.rb
@@ -11,13 +11,17 @@ module DataLoader
       ministers_xml = agent.get "#{Settings.xml_data_base_url}members/ministers.xml"
       Rails.logger.info "Deleted #{Office.delete_all} offices"
       ministers_xml.search(:moffice).each do |moffice|
-        person_id = People.member_to_person[moffice[:matchid]]
-        raise "MP #{moffice[:name]} has no person" unless person_id
+        person_id_long = People.member_to_person[moffice[:matchid]]
+        raise "MP #{moffice[:name]} has no person" unless person_id_long
+
+        person_id = person_id_long[%r{uk.org.publicwhip/person/(\d*)}, 1]
+        person = Person.find_by(id: person_id)
+        raise "Office #{moffice[:id]} points to a non existent person" if person.nil?
 
         responsibility = moffice[:responsibility] || ""
 
         Office.create!(id: moffice[:id][%r{uk.org.publicwhip/moffice/(\d*)}, 1],
-                       person_id: person_id[%r{uk.org.publicwhip/person/(\d*)}, 1],
+                       person: person,
                        dept: moffice[:dept],
                        position: moffice[:position],
                        responsibility: responsibility,
