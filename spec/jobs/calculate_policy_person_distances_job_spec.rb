351c90a099e678a76d5ccadbff48d00b4895257b
Matthew Landauer <mlandauer@gmail.com>
2022-01-25 15:25:01 +1100

    Remove empty specs

--- spec/jobs/calculate_policy_person_distances_job_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-RSpec.describe CalculatePolicyPersonDistancesJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
