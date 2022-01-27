1b28ac83d22dfd5f57b351cafb0bb02d105dfe9e
Matthew Landauer <mlandauer@gmail.com>
2022-01-28 09:01:13 +1100

    Move helpers to more sensible place

---
+++ app/helpers/policy_person_distances_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module PolicyPersonDistancesHelper
+  # Returns things like "voted strongly against", "has never voted on", etc..
+  def policy_agreement_summary(policy_person_distance)
+    category_words(policy_person_distance.category(current_user))
+  end
+
+  def category_words(category)
+    case category
+    when :for3 then "voted consistently for"
+    when :for2 then "voted almost always for"
+    when :for1 then "voted generally for"
+    when :mixture then "voted a mixture of for and against"
+    when :against1 then "voted generally against"
+    when :against2 then "voted almost always against"
+    when :against3 then "voted consistently against"
+    when :never then "has never voted on"
+    when :not_enough then "has not voted enough to determine a position on"
+    else
+      raise "Unsupported category #{category}"
+    end
+  end
+end
