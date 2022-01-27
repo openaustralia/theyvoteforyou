19ad2fce3831afcdea8cfc9aa214c5913df23324
Matthew Landauer <mlandauer@gmail.com>
2022-01-28 09:19:36 +1100

    Add super simple tests

--- spec/helpers/policy_person_distances_helper_spec.rb
+++ spec/helpers/policy_person_distances_helper_spec.rb
@@ -8,4 +8,28 @@ describe PolicyPersonDistancesHelper, type: :helper do
     it { expect(helper.category_words(:never)).to eq "has never voted on" }
     it { expect(helper.category_words(:not_enough)).to eq "has not voted enough to determine a position on" }
   end
+
+  describe ".policy_agreement_summary" do
+    context "when user does not see the new policy category yet" do
+      before do
+        allow(helper).to receive(:current_user).and_return(nil)
+      end
+
+      context "when never voted" do
+        let(:ppd) { create(:policy_person_distance) }
+
+        it do
+          expect(helper.policy_agreement_summary(ppd)).to eq "has never voted on"
+        end
+      end
+
+      context "when voted twice and distance is zero" do
+        let(:ppd) { create(:policy_person_distance, distance_a: 0, nvotessame: 2) }
+
+        it do
+          expect(helper.policy_agreement_summary(ppd)).to eq "voted consistently for"
+        end
+      end
+    end
+  end
 end
