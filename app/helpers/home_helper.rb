256c5b4c662423775e0bc20c4ffb641ee6fe8779
Taz17 <mutaz.ashhab@gmail.com>
2022-02-07 18:34:13 +1100

    Added helper to resolve query string for senator search

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,35 @@
 # frozen_string_literal: true
 
 module HomeHelper
+    def senator_search(query)
+        res = []
+        if query.downcase.include?("senator") || query.downcase.include?("senate")
+            res << "senate"
+        else
+            return res
+        end
+
+        query.downcase.split(" ").each do |string|
+            case string
+            when "new south wales", "nsw"
+                res << "NSW"
+            when "victoria", "vic"
+                res << "Victoria"
+            when "western australia", "wa"
+                res << "WA"
+            when "queensland", "qld"
+                res << "Queensland"
+            when "northern territory", "nt"
+                res << "NT"
+            when "south australia", "sa"
+                res << "SA"
+            when "tasmania", "tas"
+                res << "Tasmania"
+            when "canberra", "act"
+                res << "ACT"
+            end
+        end
+
+        return res
+    end
 end
