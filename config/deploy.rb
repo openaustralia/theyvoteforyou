2f58878d6ed1d87ea0661d706ff6b957d5987dc6
Matthew Landauer <mlandauer@gmail.com>
2022-02-17 09:09:31 +1100

    Don't link bin directory in deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :rvm_ruby_version, "3.0.0"
 set :linked_files, %w[config/database.yml config/settings.yml config/secrets.yml config/newrelic.yml]
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []) + %w[bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
+set :linked_dirs, fetch(:linked_dirs, []) + %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
