8454a64512d7903cf722e52b1b7aa5d9f45abfea
Matthew Landauer <mlandauer@gmail.com>
2022-02-21 12:04:34 +1100

    Disallow creating users in admin interface

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -44,5 +44,9 @@ module Admin
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
+
+    def valid_action?(name, resource = resource_class)
+      %w[new].exclude?(name.to_s) && super
+    end
   end
 end
