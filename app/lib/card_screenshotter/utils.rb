e3a1e5c56db5c0b52a87708727aec4fe22d0d4bd
Taz17 <mutaz.ashhab@gmail.com>
2022-02-24 12:40:43 +1100

    created CardScreenshotter module and added utils class

---
+++ app/lib/card_screenshotter/utils.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "selenium-webdriver"
+require "fileutils"
+
+module CardScreenshotter
+  class Utils
+    class << self
+      def open_headless_driver
+        options = Selenium::WebDriver::Chrome::Options.new
+        options.add_argument("--headless")
+        driver = Selenium::WebDriver.for :chrome, capabilities: [options]
+
+        driver
+      end
+
+      def close_driver(driver)
+        driver.quit
+      end
+
+      def screenshot(driver, url, width, height)
+        driver.get(url)
+        driver.manage.window.resize_to(width, height)
+        screenshot = driver.screenshot_as(:png)
+        screenshot
+      end
+
+      def save_image(image, save_path, file_name)
+        FileUtils.mkdir_p(save_path) unless File.directory?(save_path)
+
+        File.open("#{save_path}/#{file_name}", "wb+") do |f|
+          f.write image
+        end
+      end
+    end
+  end
+end
