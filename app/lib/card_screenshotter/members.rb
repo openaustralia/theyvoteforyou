eb838ff5dec40b1659a720c58f23949b490703b5
Taz17 <mutaz.ashhab@gmail.com>
2022-02-24 12:40:58 +1100

    Added members class to module

---
+++ app/lib/card_screenshotter/members.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+include PathHelper
+include Rails.application.routes.url_helpers
+
+module CardScreenshotter
+  class Members
+    def self.update_screenshots
+      driver = CardScreenshotter::Utils.open_headless_driver
+
+      card_width = 600
+      card_height = 350
+      save_path = Rails.root.join("public/cards/member_policy_vote")
+
+      PolicyPersonDistance.find_each do |ppd|
+        person = ppd.person
+        policy = ppd.policy
+        url = "http://#{ActionMailer::Base.default_url_options[:host]}#{person_policy_path_simple(person, policy)}?card=true"
+        file_name = "#{person.id}_#{policy.id}.png"
+        
+        image = CardScreenshotter::Utils.screenshot(driver, url, card_width, card_height)
+        CardScreenshotter::Utils.save_image(image, save_path, file_name)
+      end
+      CardScreenshotter::Utils.close_driver(driver)
+    end
+  end
+end
