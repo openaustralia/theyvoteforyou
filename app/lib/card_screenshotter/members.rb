2dc6556330f398deefbebabbc35cac82bcdc99f3
Matthew Landauer <mlandauer@gmail.com>
2022-03-07 16:30:38 +1100

    Extract method

--- app/lib/card_screenshotter/members.rb
+++ app/lib/card_screenshotter/members.rb
@@ -9,18 +9,19 @@ module CardScreenshotter
 
     def self.update_screenshots
       driver = CardScreenshotter::Utils.open_headless_driver
-
-      PolicyPersonDistance.find_each do |ppd|
-        person = ppd.person
-        policy = ppd.policy
-        url = "https://#{ActionMailer::Base.default_url_options[:host]}#{person_policy_path_simple(person, policy)}?card=true"
-        save_path = get_save_path(person)
-        file_name = "#{policy.id}.png"
-
-        image = CardScreenshotter::Utils.screenshot(driver, url, CARD_WIDTH, CARD_HEIGHT)
-        CardScreenshotter::Utils.save_image(image, save_path, file_name)
-      end
+      PolicyPersonDistance.find_each { |ppd| update_screenshot(driver, ppd) }
       CardScreenshotter::Utils.close_driver(driver)
+    end
+
+    def self.update_screenshot(driver, ppd)
+      person = ppd.person
+      policy = ppd.policy
+      url = "https://#{ActionMailer::Base.default_url_options[:host]}#{person_policy_path_simple(person, policy)}?card=true"
+      save_path = get_save_path(person)
+      file_name = "#{policy.id}.png"
+
+      image = CardScreenshotter::Utils.screenshot(driver, url, CARD_WIDTH, CARD_HEIGHT)
+      CardScreenshotter::Utils.save_image(image, save_path, file_name)
     end
 
     def self.get_save_path(person)
