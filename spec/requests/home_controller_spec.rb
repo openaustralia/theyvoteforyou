3063ea85111e4dc530f74f1ff47418be7622e274
Matthew Landauer <mlandauer@gmail.com>
2022-03-30 12:18:06 +1100

    Simplify fixtures

--- spec/requests/home_controller_spec.rb
+++ spec/requests/home_controller_spec.rb
@@ -7,15 +7,36 @@ describe HomeController, type: :request do
   include HTMLCompareHelper
   include_context "with fixtures"
 
-  before do
-    add_new_fixtures
-  end
-
   it "#index" do
+    member_tony_abbott
+    member_john_alexander
+    member_christopher_back
+    member_christine_milne
     compare_static("/")
   end
 
   it "#faq" do
+    member_tony_abbott
+    member_kevin_rudd
+    member_christine_milne
+    member_john_howard
+    member_maxine_mckew
+    member_john_alexander
+    member_christopher_back
+    member_judith_adams
+    member_paul_zammit
+    member_disagreeable_curmudgeon
+    member_surly_nihilist
+    member_roger_price
+    division_representatives_2013_03_14_1
+    division_senate_2013_03_14_1
+    division_representatives_2006_12_06_3
+    division_senate_2009_11_25_8
+    division_senate_2009_11_30_8
+    division_senate_2009_12_30_8
+    policy1
+    policy2
+    policy3
     compare_static("/help/faq")
   end
 
@@ -24,6 +45,7 @@ describe HomeController, type: :request do
 
     # Goes direct to MP page (only one MP covered by this postcode)
     it do
+      member_tony_abbott
       VCR.use_cassette("openaustralia_postcode_api") do
         get("/search?query=2088")
         expect(response).to redirect_to("/people/representatives/warringah/tony_abbott")
