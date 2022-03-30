d45f02c61d1a19d6be40f52479e8187fcfd0ae63
Matthew Landauer <mlandauer@gmail.com>
2022-03-30 14:00:13 +1100

    Simplify fixtures

--- spec/requests/policies_controller_spec.rb
+++ spec/requests/policies_controller_spec.rb
@@ -8,7 +8,14 @@ describe PoliciesController, type: :request do
   include_context "with fixtures"
 
   before do
-    add_new_fixtures
+    policy1
+    policy2
+    member_tony_abbott
+    member_kevin_rudd
+    member_john_alexander
+    policy1_tony_abbott
+    policy1_kevin_rudd
+    policy1_john_alexander
     # To workaround paper trail and fixtures problems we're deleting the static
     # fixtures data and recreating here in such a way that the versions in paper
     # trail are setup the way we want
