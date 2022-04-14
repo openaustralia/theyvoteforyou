5a3cad56f502a362db2144c8f14920cb3de9cb35
Matthew Landauer <mlandauer@gmail.com>
2022-04-14 15:46:38 +1000

    Show divisions in which they voted the same

--- app/models/people_distance.rb
+++ app/models/people_distance.rb
@@ -55,4 +55,16 @@ class PeopleDistance < ApplicationRecord
       .where(members2: { person_id: person2_id })
       .where("votes1.vote != votes2.vote")
   end
+
+  # Divisions where the two people voted the same
+  def divisions_same
+    Division
+      .joins("INNER JOIN votes AS votes1 on votes1.division_id = divisions.id")
+      .joins("INNER JOIN votes AS votes2 on votes2.division_id = divisions.id")
+      .joins("INNER JOIN members AS members1 on members1.id = votes1.member_id")
+      .joins("INNER JOIN members AS members2 on members2.id = votes2.member_id")
+      .where(members1: { person_id: person1_id })
+      .where(members2: { person_id: person2_id })
+      .where("votes1.vote = votes2.vote")
+  end
 end
