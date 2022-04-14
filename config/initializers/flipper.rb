e549c3eef823f6c635b257dc4f12c290fa2c8fa1
Matthew Landauer <mlandauer@gmail.com>
2022-04-14 11:25:51 +1000

    Add feature flag to swap out whole friends section

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -3,7 +3,7 @@
 Flipper::UI.configure do |config|
   config.descriptions_source = lambda do |_keys|
     # This should be a complete list of all features being currently used in the codebase
-    { }
+    { "people_distances" => "Show friends using people distances measure"}
   end
 
   # Defaults to false. Set to true to show feature descriptions on the list
