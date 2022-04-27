a0752fdd118c122df7251d90b1438872a39cb229
Matthew Landauer <mlandauer@gmail.com>
2022-04-27 15:52:49 +1000

    Remove unused class

--- spec/models/parliament_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-describe Parliament, type: :model do
-  describe "#latest" do
-    it { expect(described_class.latest).to eq "2013" }
-  end
-end
