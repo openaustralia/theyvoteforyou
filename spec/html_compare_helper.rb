e8e0e710c0edba03c485f64887e57fc78f50b8cb
Matthew Landauer <mlandauer@gmail.com>
2025-02-21 12:19:51 +1100

    Reverts three commits that were adding debugging code
    This reverts commit b5b92bef9a2a0a76d74f0a0a49971db770afa6fc
    This reverts commit c4d7975df66a6e6349ed5c6120967f535a1cf096
    This reverts commit ce26352a681be52ed5412d84d27248fcac4dbdd7

--- spec/html_compare_helper.rb
+++ spec/html_compare_helper.rb
@@ -38,7 +38,6 @@ module HTMLCompareHelper
 
     # Write it out to a file
     File.write("spec/fixtures/static_pages#{path.gsub '?', '__'}.#{format}", new_text)
-    puts new_text
     raise "Don't match. Writing over file in spec/fixtures/static_pages. Do a git diff."
   end
 
@@ -63,7 +62,6 @@ module HTMLCompareHelper
   end
 
   def tidy(text, format = :html)
-    puts `#{tidy_path}`
     File.write("temp", text)
     # Requires HTML Tidy (http://tidy.sourceforge.net/) version 14 June 2007 or later
     # Note the version installed with OS X by default is a version that's too old
