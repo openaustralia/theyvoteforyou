348bc368403a2c38b04b7e39957bdb705dd4ab81
Matthew Landauer <mlandauer@gmail.com>
2025-04-25 13:16:59 +1000

    Permanently switch over

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ end
 module Publicwhip
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
