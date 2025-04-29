b5e765d94ed90bbafe3def9209bc6045fe453c4b
Matthew Landauer <mlandauer@gmail.com>
2025-04-29 15:53:33 +1000

    Move over to 6.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ end
 module Publicwhip
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
