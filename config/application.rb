609e4ef12dceede7862e1e77e50663c337024450
Matthew Landauer <mlandauer@gmail.com>
2025-05-09 14:53:58 +1000

    All settings are now at the rails 7.0 default

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ end
 module Publicwhip
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.active_support.cache_format_version = 7.0
     config.active_support.disable_to_s_conversion = true
