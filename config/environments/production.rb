17c657f8343db68a77ea7b480066ef1901e0c3a8
Matthew Landauer <mlandauer@gmail.com>
2025-05-30 12:25:31 +1000

    Upgrade dali gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, nil, { namespace: "publicwhip" }
+  config.cache_store = :mem_cache_store, nil, { namespace: "publicwhip" }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :delayed_job
