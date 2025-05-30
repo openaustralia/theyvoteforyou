ff28d3f5e30cb989b02cc87b97b30ae877fb3d02
Matthew Landauer <mlandauer@gmail.com>
2025-05-30 10:02:19 +1000

    Switch over completely to rails 7.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ end
 module Publicwhip
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     config.active_support.cache_format_version = 7.1
     config.add_autoload_paths_to_load_path = false
