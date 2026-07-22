5f607988cdc070f74a9ad30f68043c15f07fc150
Ian Heggie (OAF) <ian@oaf.org.au>
2026-07-23 06:44:36 +1000

    Allow override of development email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.smtp_settings = { address: "mailpit", port: 1025 }
+  config.action_mailer.smtp_settings = { address: ENV.fetch("MAILPIT_HOST") { "mailpit" }, port: (ENV.fetch("MAILPIT_PORT") { 1025 }).to_i }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
