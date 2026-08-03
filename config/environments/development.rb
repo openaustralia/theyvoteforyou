fb8d8261407ea6810a8527d77cec7fa8e3829106
Ian Heggie (OAF) <ian@oaf.org.au>
2026-08-04 03:19:47 +1000

    Set PORT to 3088 and pay attention to it in development config like puma does

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: ENV.fetch("PORT", 3000).to_i }
   config.action_mailer.smtp_settings = { address: ENV.fetch("MAILPIT_HOST") { "mailpit" }, port: (ENV.fetch("MAILPIT_PORT") { 1025 }).to_i }
 
   # Print deprecation notices to the Rails logger.
